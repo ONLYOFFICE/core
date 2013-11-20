aa318b45d6db598f91b1c5cd25091da870d2b377
Ivan.Shulga <Ivan.Shulga@onlyoffice.com>
2013-11-20 11:52:32 +0000

    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@52284 954022d7-b5bf-4e40-9824-e11837661b57

--- ASCOfficeTxtFile/ASCOfficeTxtFile.cpp
+++ ASCOfficeTxtFile/ASCOfficeTxtFile.cpp
@@ -7,8 +7,8 @@
 
 // The module attribute causes DllMain, DllRegisterServer and DllUnregisterServer to be automatically implemented for you
 [ module(dll, uuid = "{30D961A6-9131-48E8-A7D9-3826E3D7818A}", 
-		 name = "AVSOfficeTxtFile", 
-		 helpstring = "AVSOfficeTxtFile 1.0 Type Library",
+		 name = "ASCOfficeTxtFile", 
+		 helpstring = "ASCOfficeTxtFile 1.0 Type Library",
 		 resource_name = "IDR_AVSTXTFILE") ]
 class CAVSTxtFileModule
 {

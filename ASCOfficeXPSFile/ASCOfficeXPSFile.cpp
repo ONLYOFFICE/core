4a4a739ed2a7d9f054d56ce2f041c8746ef82810
Ivan.Shulga <Ivan.Shulga@onlyoffice.com>
2013-11-21 15:07:36 +0000

    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@52346 954022d7-b5bf-4e40-9824-e11837661b57

--- ASCOfficeXPSFile/ASCOfficeXPSFile.cpp
+++ ASCOfficeXPSFile/ASCOfficeXPSFile.cpp
@@ -1,4 +1,4 @@
-// AVSOfficeXPSFile.cpp : Implementation of DLL Exports.
+// ASCOfficeXPSFile.cpp : Implementation of DLL Exports.
 
 
 #include "stdafx.h"
@@ -9,6 +9,6 @@
 
 // The module attribute causes DllMain, DllRegisterServer and DllUnregisterServer to be automatically implemented for you
 [ module(dll, uuid = "{15F1A39D-3759-4b5f-9AB0-521B2D556786}", 
-		 name = "AVSOfficeXPSFile", 
-		 helpstring = "AVSOfficeXPSFile 1.0 Type Library",
-		 resource_name = "IDR_AVSXPSFILE") ];
+		 name = "ASCOfficeXPSFile", 
+		 helpstring = "ASCOfficeXPSFile 1.0 Type Library",
+		 resource_name = "IDR_ASCXPSFILE") ];

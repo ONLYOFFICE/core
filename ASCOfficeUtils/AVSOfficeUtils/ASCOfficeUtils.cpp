b7bc2b2a848a54c82de6ff48119c3357f3413fc0
Ivan.Shulga <Ivan.Shulga@onlyoffice.com>
2013-11-21 10:56:34 +0000

    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@52313 954022d7-b5bf-4e40-9824-e11837661b57

--- ASCOfficeUtils/AVSOfficeUtils/ASCOfficeUtils.cpp
+++ ASCOfficeUtils/AVSOfficeUtils/ASCOfficeUtils.cpp
@@ -1,4 +1,4 @@
-// AVSOfficeUtils.cpp : Implementation of DLL Exports.
+// ASCOfficeUtils.cpp : Implementation of DLL Exports.
 
 
 #include "stdafx.h"
@@ -7,9 +7,9 @@
 
 // The module attribute causes DllMain, DllRegisterServer and DllUnregisterServer to be automatically implemented for you
 [ module(dll, uuid = "{92F87FA9-D3C2-4820-82F6-DEAEC0A3539D}", 
-		 name = "AVSOfficeUtils", 
-		 helpstring = "AVSOfficeUtils 1.0 Type Library",
-		 resource_name = "IDR_AVSOFFICEUTILS") ]
+		 name = "ASCOfficeUtils", 
+		 helpstring = "ASCOfficeUtils 1.0 Type Library",
+		 resource_name = "IDR_ASCOFFICEUTILS") ]
 class CAVSOfficeUtilsModule
 {
 public:

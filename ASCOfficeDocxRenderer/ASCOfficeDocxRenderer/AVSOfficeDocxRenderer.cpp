4d8d02ff81f12d97a8ece4d9cc67bc7f90903352
Ivan.Shulga <Ivan.Shulga@onlyoffice.com>
2013-11-26 08:56:42 +0000

    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@52542 954022d7-b5bf-4e40-9824-e11837661b57

--- ASCOfficeDocxRenderer/ASCOfficeDocxRenderer/AVSOfficeDocxRenderer.cpp
+++ ASCOfficeDocxRenderer/ASCOfficeDocxRenderer/AVSOfficeDocxRenderer.cpp
@@ -1,4 +1,4 @@
-// AVSOfficeDocxRenderer.cpp : Implementation of DLL Exports.
+// ASCOfficeDocxRenderer.cpp : Implementation of DLL Exports.
 
 
 #include "stdafx.h"
@@ -7,9 +7,9 @@
 
 // The module attribute causes DllMain, DllRegisterServer and DllUnregisterServer to be automatically implemented for you
 [ module(dll, uuid = "{F522F14B-4A53-4DC8-960B-C5A46B447BEC}", 
-		 name = "AVSOfficeDocxRenderer", 
-		 helpstring = "AVSOfficeDocxRenderer 1.0 Type Library",
-		 resource_name = "IDR_AVSOFFICEDOCXRENDERER") ]
+		 name = "ASCOfficeDocxRenderer", 
+		 helpstring = "ASCOfficeDocxRenderer 1.0 Type Library",
+		 resource_name = "IDR_ASCOFFICEDOCXRENDERER") ]
 class CAVSOfficeDocxRendererModule
 {
 public:

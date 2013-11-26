5eebdda782bfc3564d7d2378ecf7ae6ccf85bb64
Oleg.Korshul <Oleg.Korshul@onlyoffice.com>
2013-11-26 10:04:40 +0000

    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@52558 954022d7-b5bf-4e40-9824-e11837661b57

--- Common/Config.h
+++ Common/Config.h
@@ -2,3 +2,16 @@
 
 //#define BUILD_CONFIG_OPENSOURCE_VERSION
 #define BUILD_CONFIG_FULL_VERSION
+
+#ifdef BUILD_CONFIG_OPENSOURCE_VERSION
+
+class CRegistratorClient
+{
+public:
+	static BOOL IsRegistered()
+	{
+		return true;
+	}
+};
+
+#endif

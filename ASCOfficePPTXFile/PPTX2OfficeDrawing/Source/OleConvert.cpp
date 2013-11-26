9eb5b1febd4a2e0a039738a935b471ac2cfc0466
Oleg.Korshul <Oleg.Korshul@onlyoffice.com>
2013-11-26 14:45:18 +0000

    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@52601 954022d7-b5bf-4e40-9824-e11837661b57

--- ASCOfficePPTXFile/PPTX2OfficeDrawing/Source/OleConvert.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "./../Converter.h"
-
-namespace PPTX2OfficeDrawing
-{
-	void Converter::OleConvert(const PPTX::Logic::GraphicFrame& pOle, CAtlArray<IElement*>& pElements)
-	{
-	}
-}

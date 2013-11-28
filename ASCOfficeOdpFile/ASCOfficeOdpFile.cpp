6b16e7ecf7210f68240e4451de2da8daba70047b
Oleg.Korshul <Oleg.Korshul@onlyoffice.com>
2013-11-28 10:17:58 +0000

    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@52680 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ ASCOfficeOdpFile/ASCOfficeOdpFile.cpp
@@ -0,0 +1,12 @@
+// ASCOfficeOdpFile.cpp : Implementation of DLL Exports.
+#include "stdafx.h"
+#include "resource.h"
+
+#include "OfficeODPFile.h"
+// The module attribute causes DllMain, DllRegisterServer and DllUnregisterServer to be automatically implemented for you
+[ module(dll, uuid = "{FA56A4F4-FCEA-47f8-BE80-B37C7FA95BBF}", 
+		 name = "ASCOfficeOdpFile", 
+		 helpstring = "ASCOfficeOdpFile 1.0 Type Library",
+		 resource_name = "IDR_ASCOFFICEODPFILE") ];
+
+		 

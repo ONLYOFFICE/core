568ea824e131ba469ff554d6419988a85b32a826
Oleg.Korshul <Oleg.Korshul@onlyoffice.com>
2013-12-20 09:53:19 +0000

    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@53245 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ DoctRenderer/COMMON/native_ppt/corrector.js
@@ -0,0 +1,36 @@
+CFontFileLoader.prototype.LoadFontNative = function()
+{
+    var __font_data_idx = g_fonts_streams.length;
+    var _data = window.native.GetFontBinary(this.Id);
+    g_fonts_streams[__font_data_idx] = new FT_Stream(_data, _data.length);
+    this.SetStreamIndex(__font_data_idx);
+    this.Status = 0;
+};
+
+BinaryFileReader.prototype.ReadNative = function(data)
+{
+	try
+	{
+		this.stream = new FT_Stream2(data, data.length);
+		this.PreLoadPrepare();
+		this.ReadMainTable();
+		this.PostLoadPrepare();
+	}
+	catch(e)
+	{
+		if(e.message == g_sErrorCharCountMessage)
+			return false;
+		else
+			throw e;
+	}
+	return true;
+};
+
+CChartAsGroup.prototype.draw = function(graphics, pageIndex)
+{
+    return;
+};
+
+function CCacheManager()
+{
+}

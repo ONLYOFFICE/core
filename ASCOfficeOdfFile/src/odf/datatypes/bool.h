437ff29de131d6ea8f4211967edd52d0a9c424c7
Elen.Subbotina <Elen.Subbotina@onlyoffice.com>
2014-03-23 10:57:18 +0000

    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@55078 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ ASCOfficeOdfFile/src/odf/datatypes/bool.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <iosfwd>
+#include <string>
+#include "odfattributes.h"
+
+
+namespace cpdoccore { namespace odf { 
+
+class Bool
+{
+public:
+	Bool() {bool_ = false;};
+    
+    Bool(bool Val) : bool_(Val) { bool_str_ = bool_ ? L"true" : L"false"; };
+    
+    const bool & get() const 
+    { 
+        return bool_; 
+    };
+	
+	void operator=( bool b ) {bool_ = b; bool_str_ = bool_ ? L"true" : L"false"; }
+	
+	const std::wstring &string() const 
+	{
+		return bool_str_ ;
+	}
+	static Bool parse(const std::wstring & Str);
+
+
+private:
+    bool bool_;
+	std::wstring bool_str_;
+};
+
+std::wostream & operator << (std::wostream & _Wostream, const Bool & _val);
+
+} 
+
+APPLY_PARSE_XML_ATTRIBUTES(odf::Bool);
+
+}

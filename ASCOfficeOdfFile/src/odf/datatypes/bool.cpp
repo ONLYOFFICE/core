edc1315503edc835c3f384a5c2b70e5daedfd958
Elen.Subbotina <Elen.Subbotina@onlyoffice.com>
2014-03-23 10:57:18 +0000

    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@55078 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ ASCOfficeOdfFile/src/odf/datatypes/bool.cpp
@@ -0,0 +1,23 @@
+#include "precompiled_cpodf.h"
+#include "bool.h"
+
+#include <ostream>
+#include <sstream>
+
+namespace cpdoccore { namespace odf { 
+
+std::wostream & operator << (std::wostream & _Wostream, const Bool & _Val)
+{
+	_Wostream << _Val.string();
+    return _Wostream;
+}
+Bool Bool::parse(const std::wstring & Str)
+{
+    std::wstring tmp = Str;
+    boost::algorithm::to_lower(tmp);
+
+	if (tmp == L"0" || tmp == L"false") return Bool(false);
+	else return Bool(true);
+}
+} 
+}

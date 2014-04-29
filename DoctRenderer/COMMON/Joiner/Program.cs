5cc1b06102e8fa7f87d705fb66724f50ef57c377
Oleg.Korshul <Oleg.Korshul@onlyoffice.com>
2014-04-29 13:50:35 +0000

    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@55799 954022d7-b5bf-4e40-9824-e11837661b57

--- DoctRenderer/COMMON/Joiner/Program.cs
+++ DoctRenderer/COMMON/Joiner/Program.cs
@@ -21,9 +21,6 @@ namespace Joiner
             string strRoot = strApplication + "\\OfficeWeb\\";
             List<string> files = new List<string>();
 
-            //files.Add("../ServerComponents/DoctRenderer/COMMON/native.js");
-            //files.Add("Common/3rdparty/XRegExp/xregexp-all.js");
-
             string[] arrFilesConfig = {
                 "Common/docscoapisettings.js",
 	            "Common/browser.js",
@@ -176,120 +173,6 @@ namespace Joiner
                 "Word/Math/borderBox.js",
                 "Word/Math/test_composition.js"
             };
-
-
-            files.Add("Common/browser.js");
-            files.Add("Common/editorscommon.js");
-            
-            files.Add("Common/downloaderfiles.js");
-            files.Add("Common/docscoapicommon.js");
-            files.Add("Common/docscoapi.js");
-            files.Add("Common/spellcheckapi.js");
-
-            files.Add("Common/wordcopypaste.js");
-    		
-            files.Add("Common/apiCommon.js");
-
-            files.Add("Excel/apiDefines.js");
-            files.Add("Common/Charts/DrawingObjects.js");
-            files.Add("Common/Charts/charts.js");
-            files.Add("Common/commonDefines.js");
-            files.Add("Common/SerializeCommonWordExcel.js");
-            files.Add("Common/Charts/libraries/OfficeExcel.common.core.js");
-            files.Add("Common/Charts/libraries/OfficeExcel.common.key.js");
-            files.Add("Common/Charts/libraries/OfficeExcel.bar.js");
-            files.Add("Common/Charts/libraries/OfficeExcel.hbar.js");
-            files.Add("Common/Charts/libraries/OfficeExcel.line.js");
-            files.Add("Common/Charts/libraries/OfficeExcel.pie.js");
-            files.Add("Common/Charts/libraries/OfficeExcel.scatter.js");
-            files.Add("Common/Charts/libraries/OfficeExcel.chartProperties.js");
-
-            files.Add("Common/FontsFreeType/font_engine.js");
-
-            files.Add("Common/FontsFreeType/FontFile.js");
-            files.Add("Common/FontsFreeType/FontManager.js");
-            files.Add("Word/Drawing/Externals.js");
-            files.Add("Word/Drawing/GlobalLoaders.js");
-
-            files.Add("Word/Drawing/translations.js");
-
-            files.Add("Word/Drawing/Controls.js");
-            files.Add("Word/Drawing/WorkEvents.js");
-
-            files.Add("Word/Editor/GraphicObjects/GraphicObjects.js");
-            files.Add("Word/Editor/GraphicObjects/GraphicPage.js");
-            files.Add("Word/Editor/GraphicObjects/States.js");
-            files.Add("Word/Editor/GraphicObjects/Math.js");
-            files.Add("Word/Editor/GraphicObjects/TrackGraphicObjects.js");
-            files.Add("Word/Editor/GraphicObjects/WrapManager.js");
-            files.Add("Word/Editor/GraphicObjects/Spline.js");
-            files.Add("Word/Editor/GraphicObjects/PolyLine.js");
-
-
-            files.Add("Word/Editor/GraphicObjects/ObjectTypes/WordShape.js");
-            files.Add("Word/Editor/GraphicObjects/ObjectTypes/WordImage.js");
-            files.Add("Word/Editor/GraphicObjects/ObjectTypes/WordGroupShapes.js");
-            files.Add("Word/Editor/GraphicObjects/ObjectTypes/Format.js");
-            files.Add("Word/Editor/GraphicObjects/ObjectTypes/Geometry.js");
-            files.Add("Word/Editor/GraphicObjects/ObjectTypes/ShapeTracks.js");
-            files.Add("Word/Editor/GraphicObjects/ObjectTypes/GroupTracks.js");
-            files.Add("Word/Editor/GraphicObjects/ObjectTypes/CreateGeometry.js");
-            files.Add("Word/Editor/GraphicObjects/ObjectTypes/Path.js");
-
-            files.Add("Word/Editor/GraphicObjects/ObjectTypes/Chart.js");
-            files.Add("Word/Editor/GraphicObjects/ObjectTypes/ChartLayout.js");
-            files.Add("Word/Editor/GraphicObjects/ObjectTypes/ChartLegend.js");
-            files.Add("Word/Editor/GraphicObjects/ObjectTypes/ChartTitle.js");
-            files.Add("Word/Editor/GraphicObjects/ObjectTypes/TextBody.js");
-
-            files.Add("Word/Drawing/Metafile.js");
-            files.Add("Word/Editor/CollaborativeEditing.js");
-            files.Add("Word/Editor/Comments.js");
-            files.Add("Word/Editor/History.js");
-            files.Add("Word/Editor/Styles.js");
-            files.Add("Word/Editor/DrawingObjects.js");
-            files.Add("Word/Editor/FlowObjects.js");
-            files.Add("Word/Editor/ParagraphContent.js");
-            files.Add("Word/Editor/Paragraph.js");
-            files.Add("Word/Editor/Sections.js");
-            files.Add("Word/Editor/Numbering.js");
-            files.Add("Word/Editor/HeaderFooter.js");
-            files.Add("Word/Editor/Document.js");
-            files.Add("Word/Editor/Common.js");
-            files.Add("Word/Editor/Numbering.js");
-            files.Add("Word/Editor/DocumentContent.js");
-            files.Add("Word/Editor/Table.js");
-            files.Add("Word/Editor/Serialize2.js");
-            files.Add("Word/Editor/Search.js");
-            files.Add("Word/Editor/FontClassification.js");
-            files.Add("Word/Editor/Spelling.js");
-
-            files.Add("Word/Drawing/HatchPattern.js");
-            files.Add("Word/Drawing/Graphics.js");
-            files.Add("Word/Drawing/ArcTo.js");
-            files.Add("Word/Drawing/Hit.js");
-            files.Add("Word/Drawing/ColorArray.js");
-            files.Add("Word/Drawing/Overlay.js");
-            files.Add("Word/Drawing/ShapeDrawer.js");
-            files.Add("Word/Drawing/DrawingDocument.js");
-            files.Add("Word/Drawing/GraphicsEvents.js");
-            files.Add("Word/Drawing/HtmlPage.js");
-            files.Add("Word/Drawing/Rulers.js");
-            files.Add("Word/apiDefines.js");
-            files.Add("Word/apiCommon.js");
-            files.Add("Word/api.js");
-
-            files.Add("Common/Shapes/EditorSettings.js");
-            files.Add("Common/Shapes/Serialize.js");
-            files.Add("Common/Shapes/SerializeWriter.js");
-            files.Add("Word/Editor/SerializeCommon.js");
-
-            files.Add("Excel/graphics/DrawingContextWord.js");
-
-            //files.Add("../ServerComponents/DoctRenderer/COMMON/native_word/DrawingDocument.js");
-            //files.Add("../ServerComponents/DoctRenderer/COMMON/native_word/HtmlPage.js");
-            //files.Add("../ServerComponents/DoctRenderer/COMMON/native_word/api.js");
-            //files.Add("../ServerComponents/DoctRenderer/COMMON/native_word/corrector.js");
 
             if (true)
             {

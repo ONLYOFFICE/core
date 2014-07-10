846d19acfba54e01c6cd6beccba853efcc575426
Oleg.Korshul <Oleg.Korshul@onlyoffice.com>
2014-07-10 07:31:27 +0000

    graphics qt_build windows
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@57263 954022d7-b5bf-4e40-9824-e11837661b57

--- DesktopEditor/cximage/CxImage/stdint.h
+++ DesktopEditor/cximage/CxImage/stdint.h
@@ -104,6 +104,19 @@ typedef uint64_t  uint_least64_t;
 
 // 7.18.1.3 Fastest minimum-width integer types
 typedef int8_t    int_fast8_t;
+#ifdef _QT
+#ifndef  WIN32
+typedef int16_t   int_fast16_t;
+#endif
+typedef int32_t   int_fast32_t;
+typedef int64_t   int_fast64_t;
+typedef uint8_t   uint_fast8_t;
+#ifndef WIN32
+typedef uint16_t  uint_fast16_t;
+#endif
+typedef uint32_t  uint_fast32_t;
+typedef uint64_t  uint_fast64_t;
+#else
 typedef int16_t   int_fast16_t;
 typedef int32_t   int_fast32_t;
 typedef int64_t   int_fast64_t;
@@ -111,6 +124,7 @@ typedef uint8_t   uint_fast8_t;
 typedef uint16_t  uint_fast16_t;
 typedef uint32_t  uint_fast32_t;
 typedef uint64_t  uint_fast64_t;
+#endif
 
 // 7.18.1.4 Integer types capable of holding object pointers
 #ifdef _WIN64 // [

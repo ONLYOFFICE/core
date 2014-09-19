f28484a708323ea6fa184fb47152d76863b1bafd
Oleg.Korshul <Oleg.Korshul@onlyoffice.com>
2014-09-19 15:37:25 +0000

    windows version
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@58375 954022d7-b5bf-4e40-9824-e11837661b57

--- DesktopEditor/Qt_build/test/control/mainwindow.cpp
+++ DesktopEditor/Qt_build/test/control/mainwindow.cpp
@@ -15,10 +15,30 @@ MainWindow::~MainWindow()
 
 void MainWindow::resizeEvent(QResizeEvent* e)
 {
-    ui->widget->setGeometry(10, 10, this->width() - 20, this->height() - 60);
+    ui->widget->setGeometry(10, 10, this->width() - 20, this->height() - ui->horizontalSlider->height() - 10);
+
+    ui->horizontalSlider->setGeometry(10,
+                                      this->height() - ui->horizontalSlider->height(),
+                                      500,
+                                      ui->horizontalSlider->height());
+
+    ui->pushButton->setGeometry(520,
+                                this->height() - ui->horizontalSlider->height(),
+                                60,
+                                ui->horizontalSlider->height());
 }
 
 void MainWindow::closeEvent(QCloseEvent* e)
 {
     ui->widget->closeEvent(e);
+}
+
+void MainWindow::on_horizontalSlider_valueChanged(int value)
+{
+    ui->widget->SetZoom((double)value / 100.0);
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->widget->ChangeCountPagesInBlock();
 }
--- DesktopEditor/Qt_build/test/control/mainwindow.h
+++ DesktopEditor/Qt_build/test/control/mainwindow.h
@@ -21,6 +21,9 @@ private:
 protected:
     virtual void resizeEvent(QResizeEvent* e);
     virtual void closeEvent(QCloseEvent* e);
+private slots:
+    void on_horizontalSlider_valueChanged(int value);
+    void on_pushButton_clicked();
 };
 
 #endif // MAINWINDOW_H

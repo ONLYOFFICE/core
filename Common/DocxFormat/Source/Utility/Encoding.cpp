b4c6725e0d67e6949581f430f4a3901642df7803
Ivan.Shulga <Ivan.Shulga@onlyoffice.com>
2014-09-25 15:46:05 +0000

    linux build
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@58499 954022d7-b5bf-4e40-9824-e11837661b57

--- Common/DocxFormat/Source/Utility/Encoding.cpp
+++ Common/DocxFormat/Source/Utility/Encoding.cpp
@@ -1,45 +1,45 @@
 #include "Encoding.h"
 #include "Utility.h"
-#include "X:\AVS\Sources\AVSOfficeStudio\Common\DocxFormat\Source\Common\SimpleTypes_Word.h"
+#include "../Common/SimpleTypes_Word.h"
 
 
-__forceinline const CStringA Encoding::ansi2utf8     (const CStringA &sLine)
+AVSINLINE const CStringA Encoding::ansi2utf8     (const CStringA &sLine)
 {
 	return wstring2string( string2wstring( sLine, CP_ACP ), CP_UTF8 );
 }
 
 
-__forceinline const CStringW Encoding::ansi2unicode  (const CStringA &sLine)
+AVSINLINE const CStringW Encoding::ansi2unicode  (const CStringA &sLine)
 {
 	return string2wstring( sLine, CP_ACP );
 }
 
 
-__forceinline const CStringA Encoding::utf82ansi     (const CStringA &sLine)
+AVSINLINE const CStringA Encoding::utf82ansi     (const CStringA &sLine)
 {
 	return wstring2string( string2wstring( sLine, CP_UTF8 ), CP_ACP );
 }
 
 
-__forceinline const CStringW Encoding::utf82unicode  (const CStringA &sLine)
+AVSINLINE const CStringW Encoding::utf82unicode  (const CStringA &sLine)
 {
 	return string2wstring( sLine, CP_UTF8 );
 }
 
 
-__forceinline const CStringA Encoding::unicode2ansi  (const CStringW &sLine)
+AVSINLINE const CStringA Encoding::unicode2ansi  (const CStringW &sLine)
 {
 	return wstring2string( sLine, CP_ACP );
 }
 
 
-__forceinline const CStringA Encoding::unicode2utf8  (const CStringW &sLine)
+AVSINLINE const CStringA Encoding::unicode2utf8  (const CStringW &sLine)
 {
 	return wstring2string( sLine, CP_UTF8 );
 }
 
 
-__forceinline const CStringA Encoding::wstring2string(const CStringW &sLine, const unsigned int unCodePage)
+AVSINLINE const CStringA Encoding::wstring2string(const CStringW &sLine, const unsigned int unCodePage)
 {
 	const int nSize = WideCharToMultiByte( unCodePage, 0, sLine.GetString(), sLine.GetLength(), NULL, 0, NULL, NULL );
 	char *sTemp = new char[nSize];
@@ -55,7 +55,7 @@ __forceinline const CStringA Encoding::wstring2string(const CStringW &sLine, con
 }
 
 
-__forceinline const CStringW Encoding::string2wstring(const CStringA &sLine, const unsigned int unCodePage)
+AVSINLINE const CStringW Encoding::string2wstring(const CStringA &sLine, const unsigned int unCodePage)
 {
 	const int nSize = MultiByteToWideChar( unCodePage, 0, sLine.GetString(), sLine.GetLength(), NULL, 0 );
 

f9733a391233ef8eadc3e422dfd7ed692c25e9ce
Elen.Subbotina <Elen.Subbotina@onlyoffice.com>
2014-10-07 13:03:51 +0000

    необходимо...
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@58696 954022d7-b5bf-4e40-9824-e11837661b57

--- Common/DocxFormat/Source/Common/SimpleTypes_Word.h
+++ Common/DocxFormat/Source/Common/SimpleTypes_Word.h
@@ -2571,41 +2571,7 @@ namespace SimpleTypes
 
         void SetColorByString ();
 
-        virtual EHexColor FromString(CString &sValue)
-		{
-			if ( _T("auto") == sValue || _T("none") == sValue )
-				this->m_eValue = hexcolorAuto;
-			else
-			{
-				//¬ документации не написано, что цвет может приходить строкой, но в реальных докуентах встречаетс€ и word это разруливает.
-				//CHighlightColor<highlightcolorNone> oHighlightColor(sValue);
-				CHighlightColor<> oHighlightColor(sValue);
-				if(SimpleTypes::highlightcolorNone != oHighlightColor.GetValue())
-				{
-					this->m_eValue = hexcolorRGB;
-					m_unR = oHighlightColor.Get_R();
-					m_unG = oHighlightColor.Get_G();
-					m_unB = oHighlightColor.Get_B();
-				}
-				else if ( 6 <= sValue.GetLength() )
-				{
-					this->m_eValue = hexcolorRGB;
-					m_sValue = sValue.Mid( 0, 6 );
-					Parse();
-				}
-				else if ( 3 == sValue.GetLength() )// a la #339 (Compo 3AP.docx)
-				{
-					this->m_eValue = hexcolorRGB;
-					m_sValue = sValue;
-					Parse3();
-				}
-				else   this->m_eValue = hexcolorAuto;//eDefValue;
-
-			}
-
-			return this->m_eValue;
-
-		}
+        virtual EHexColor FromString(CString &sValue);
 
 		virtual CString   ToString  () const 
 		{

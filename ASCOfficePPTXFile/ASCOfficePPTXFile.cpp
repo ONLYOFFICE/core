e953637d218f9945c8f3a008ea4ee0d355246594
Elen.Subbotina <Elen.Subbotina@onlyoffice.com>
2014-10-21 12:17:34 +0000

    случайно потерся
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@59075 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ ASCOfficePPTXFile/ASCOfficePPTXFile.cpp
@@ -0,0 +1,13 @@
+//ASCOfficePPTXFile.cpp : Implementation of DLL Exports.
+
+#include "stdafx.h"
+#include "resource.h"
+
+#include "ASCOfficePPTXFileComInterface.h"
+#include "ASCOfficeDrawingConverterComInterface.h"
+
+// The module attribute causes DllMain, DllRegisterServer and DllUnregisterServer to be automatically implemented for you
+[ module(dll, uuid = "{95DEE027-7707-4078-A314-33D35AE842FD}", 
+		 name = "ASCOfficePPTXFile", 
+		 helpstring = "ASCOfficePPTXFile 1.0 Type Library",
+		 resource_name = "IDR_ASCOFFICEPPTXFILE") ];

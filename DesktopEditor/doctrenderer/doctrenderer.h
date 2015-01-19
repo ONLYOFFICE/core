8ab93ee3d945a813f7b6dc1b66bdb08bcadfc916
Oleg.Korshul <Oleg.Korshul@onlyoffice.com>
2015-01-19 11:10:31 +0000

    doctrenderer
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@60549 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ DesktopEditor/doctrenderer/doctrenderer.h
@@ -0,0 +1,81 @@
+#ifndef DOCTRENDERER_H
+#define DOCTRENDERER_H
+
+#include <string>
+#include "../common/Types.h"
+#include "../common/Array.h"
+
+namespace NSDoctRenderer
+{
+    namespace DoctRendererFormat
+    {
+        enum FormatFile
+        {
+            DOCT	= 0,
+            XLST	= 1,
+            PPTT	= 2,
+            PDF		= 3,
+
+            INVALID = 255
+        };
+    }
+
+    class CExecuteParams
+    {
+    public:
+        DoctRendererFormat::FormatFile m_eSrcFormat;
+        DoctRendererFormat::FormatFile m_eDstFormat;
+
+        std::wstring m_strFontsDirectory;
+        std::wstring m_strImagesDirectory;
+        std::wstring m_strThemesDirectory;
+
+        std::wstring m_strSrcFilePath;
+        std::wstring m_strDstFilePath;
+
+        CArray<std::wstring> m_arChanges;
+        int m_nCountChangesItems;
+
+    public:
+        CExecuteParams();
+        ~CExecuteParams();
+
+    public:
+        bool FromXml(const std::wstring& sXml);
+    };
+}
+
+namespace NSDoctRenderer
+{
+    class CDoctrenderer
+    {
+    private:
+        CExecuteParams m_oParams;
+
+        std::wstring m_strConfigDir;
+        std::wstring m_strConfigPath;
+        CArray<std::wstring> m_arrFiles;
+
+        std::wstring m_strDoctSDK;
+        std::wstring m_strPpttSDK;
+        std::wstring m_strXlstSDK;
+
+        std::wstring m_strEditorType;
+        std::wstring m_strFilePath;
+
+        bool m_bIsInitTypedArrays;
+
+    public:
+        CDoctrenderer();
+        ~CDoctrenderer();
+
+    public:
+        bool Execute(const std::wstring& strXml, std::wstring& strError);
+
+    private:
+        bool ExecuteScript(const std::string& strScript, std::wstring& strError);
+        std::string ReadScriptFile(const std::wstring& strFile);
+    };
+}
+
+#endif // DOCTRENDERER_H

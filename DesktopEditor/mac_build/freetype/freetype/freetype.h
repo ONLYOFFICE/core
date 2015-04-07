05da13a97bbd7c97f81ab75dc05968065fdad03f
Elen.Subbotina <Elen.Subbotina@onlyoffice.com>
2015-04-07 14:51:46 +0000

    .....
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@61941 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ DesktopEditor/mac_build/freetype/freetype/freetype.h
@@ -0,0 +1,23 @@
+/*
+ *  freetype.h
+ *  freetype
+ *
+ *  Created by alexey.musinov on 27.03.15.
+ *  Copyright (c) 2015 Ascensio System SIA. All rights reserved.
+ *
+ */
+
+#ifndef freetype_
+#define freetype_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class freetype
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ DesktopEditor/mac_build/raster/raster/rasterPriv.h
@@ -0,0 +1,19 @@
+/*
+ *  rasterPriv.h
+ *  raster
+ *
+ *  Created by alexey.musinov on 27.03.15.
+ *  Copyright (c) 2015 Ascensio System SIA. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class rasterPriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop

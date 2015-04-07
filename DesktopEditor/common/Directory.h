62c0e1122d07f6ca5b46813d1f732e1d9a953007
Elen.Subbotina <Elen.Subbotina@onlyoffice.com>
2015-04-07 14:44:24 +0000

    .....
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@61938 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ DesktopEditor/common/Directory.h
@@ -0,0 +1,293 @@
+#ifndef _BUILD_DIRECTORY_CROSSPLATFORM_H_
+#define _BUILD_DIRECTORY_CROSSPLATFORM_H_
+
+#include <stdio.h>
+#include <string>
+#include "Array.h"
+#include "File.h"
+
+#if defined(_WIN32) || defined (_WIN64)
+    #include "windows.h"
+    #include "windef.h"
+#elif __linux__
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <unistd.h>
+    #include <dirent.h>
+#elif MAC
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <unistd.h>
+    #include <dirent.h>
+#endif
+
+namespace NSDirectory
+{
+#ifdef _IOS
+    void GetFiles2_ios(std::wstring strDirectory, CArray<std::wstring>& oArray, bool bIsRecursion);
+#endif
+	static void GetFiles2(std::wstring strDirectory, CArray<std::wstring>& oArray, bool bIsRecursion = false)
+    {
+#if defined(_WIN32) || defined (_WIN64)
+        WIN32_FIND_DATAW oFD;
+
+        std::wstring sSpec = strDirectory + L"\\*.*";
+        HANDLE hRes = FindFirstFileW( sSpec.c_str(), &oFD );
+        if( INVALID_HANDLE_VALUE == hRes )
+            return;
+        do
+        {
+            sSpec = oFD.cFileName;
+            if (sSpec != L"." && sSpec != L"..")
+            {
+                sSpec = strDirectory + L"\\" + sSpec;
+                if( !( oFD.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ) )
+                {
+                    oArray.Add(sSpec);
+                }
+                else if (bIsRecursion)
+                {
+                    GetFiles2(sSpec, oArray, bIsRecursion);
+                }
+            }
+        } while( FindNextFileW( hRes, &oFD ) );
+        FindClose( hRes );
+#endif
+
+#ifdef __linux__
+        BYTE* pUtf8 = NULL;
+        LONG lLen = 0;
+        NSFile::CUtf8Converter::GetUtf8StringFromUnicode(strDirectory.c_str(), strDirectory.length(), pUtf8, lLen, false);
+        DIR *dp;
+        struct dirent *dirp;
+        if((dp  = opendir((char*)pUtf8)) != NULL)
+        {
+            while ((dirp = readdir(dp)) != NULL)
+            {
+                if(DT_REG == dirp->d_type)
+                {
+                    std::wstring sName = NSFile::CUtf8Converter::GetUnicodeStringFromUTF8((BYTE*)dirp->d_name, strlen(dirp->d_name));
+                    oArray.Add(strDirectory + L"/" + sName);
+                }
+
+                if (bIsRecursion && DT_DIR == dirp->d_type)
+                {
+                    if(dirp->d_name[0] != '.')
+                    {
+                        std::wstring sName = NSFile::CUtf8Converter::GetUnicodeStringFromUTF8((BYTE*)dirp->d_name, strlen(dirp->d_name));
+                        GetFiles2(strDirectory + L"/" + sName, oArray, bIsRecursion);
+                    }
+                }
+            }
+            closedir(dp);
+        }
+        delete [] pUtf8;
+#endif
+        
+#ifdef MAC
+        BYTE* pUtf8 = NULL;
+        LONG lLen = 0;
+        NSFile::CUtf8Converter::GetUtf8StringFromUnicode(strDirectory.c_str(), strDirectory.length(), pUtf8, lLen, false);
+        DIR *dp;
+        struct dirent *dirp;
+        if((dp  = opendir((char*)pUtf8)) != NULL)
+        {
+            while ((dirp = readdir(dp)) != NULL)
+            {
+                if(DT_REG == dirp->d_type)
+                {
+                    std::wstring sName = NSFile::CUtf8Converter::GetUnicodeStringFromUTF8((BYTE*)dirp->d_name, strlen(dirp->d_name));
+                    oArray.Add(strDirectory + L"/" + sName);
+                }
+                
+                if (bIsRecursion && DT_DIR == dirp->d_type)
+                {
+                    if(dirp->d_name[0] != '.')
+                    {
+                        std::wstring sName = NSFile::CUtf8Converter::GetUnicodeStringFromUTF8((BYTE*)dirp->d_name, strlen(dirp->d_name));
+                        GetFiles2(strDirectory + L"/" + sName, oArray, bIsRecursion);
+                    }
+                }
+            }
+            closedir(dp);
+        }
+        delete [] pUtf8;
+        return;
+#endif
+
+#if 0
+        // нормально работает и линукс версия
+#ifdef _IOS
+        return GetFiles2_ios(strDirectory, oArray, bIsRecursion);
+#endif
+#endif
+    }
+
+    static CArray<std::wstring> GetFiles(std::wstring strDirectory, bool bIsRecursion = false)
+	{
+		CArray<std::wstring> oArray;
+        GetFiles2(strDirectory, oArray, bIsRecursion);
+		return oArray;
+	}
+
+	static CArray<std::wstring> GetDirectories(std::wstring strDirectory)
+	{
+		CArray<std::wstring> oArray;
+
+#if defined(_WIN32) || defined (_WIN64)
+		WIN32_FIND_DATAW oFD; 
+		
+		std::wstring sSpec = strDirectory + L"\\*";
+		HANDLE hRes = FindFirstFileW( sSpec.c_str(), &oFD );
+		if( INVALID_HANDLE_VALUE == hRes )
+			return oArray;
+		do 
+		{
+			sSpec = oFD.cFileName;
+			if (sSpec != L"." && sSpec != L"..")
+			{
+				sSpec = strDirectory + L"\\" + sSpec;
+				if( oFD.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ) 
+				{
+					oArray.Add(sSpec);
+				}
+			}
+		} while( FindNextFileW( hRes, &oFD ) );		
+		FindClose( hRes );
+#elif __linux__
+		BYTE* pUtf8 = NULL;
+		LONG lLen = 0;
+        NSFile::CUtf8Converter::GetUtf8StringFromUnicode(strDirectory.c_str(), strDirectory.length(), pUtf8, lLen, false);
+		DIR *dp;
+		struct dirent *dirp;
+		if((dp  = opendir((char*)pUtf8)) != NULL)
+		{
+			while ((dirp = readdir(dp)) != NULL)
+			{
+				if(DT_DIR == dirp->d_type)
+				{
+					if(dirp->d_name[0] != '.')
+					{
+						std::wstring sName = NSFile::CUtf8Converter::GetUnicodeStringFromUTF8((BYTE*)dirp->d_name, strlen(dirp->d_name));
+						oArray.Add(strDirectory + L"/" + sName);
+					}
+				}
+			}
+			closedir(dp);
+		}
+		delete [] pUtf8;
+#elif MAC
+        BYTE* pUtf8 = NULL;
+        LONG lLen = 0;
+        NSFile::CUtf8Converter::GetUtf8StringFromUnicode(strDirectory.c_str(), strDirectory.length(), pUtf8, lLen, false);
+        DIR *dp;
+        struct dirent *dirp;
+        if((dp  = opendir((char*)pUtf8)) != NULL)
+        {
+            while ((dirp = readdir(dp)) != NULL)
+            {
+                if(DT_DIR == dirp->d_type)
+                {
+                    if(dirp->d_name[0] != '.')
+                    {
+                        std::wstring sName = NSFile::CUtf8Converter::GetUnicodeStringFromUTF8((BYTE*)dirp->d_name, strlen(dirp->d_name));
+                        oArray.Add(strDirectory + L"/" + sName);
+                    }
+                }
+            }
+            closedir(dp);
+        }
+        delete [] pUtf8;
+#endif
+		return oArray;
+	}
+	static bool Exists(const std::wstring& strDirectory)
+	{
+#if defined(_WIN32) || defined (_WIN64)
+		DWORD dwAttrib = ::GetFileAttributesW(strDirectory.c_str());
+		return (dwAttrib != INVALID_FILE_ATTRIBUTES && 0 != (dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+#elif __linux__
+        BYTE* pUtf8 = NULL;
+		LONG lLen = 0;
+        NSFile::CUtf8Converter::GetUtf8StringFromUnicode(strDirectory.c_str(), strDirectory.length(), pUtf8, lLen, false);
+		struct stat st;
+		bool bRes = (0 == stat((char*)pUtf8, &st)) && S_ISDIR(st.st_mode);
+		delete [] pUtf8;
+		return bRes;
+#elif MAC
+        BYTE* pUtf8 = NULL;
+        LONG lLen = 0;
+        NSFile::CUtf8Converter::GetUtf8StringFromUnicode(strDirectory.c_str(), strDirectory.length(), pUtf8, lLen, false);
+        struct stat st;
+        bool bRes = (0 == stat((char*)pUtf8, &st)) && S_ISDIR(st.st_mode);
+        delete [] pUtf8;
+        return bRes;
+#endif
+        return false;
+	}
+	static bool CreateDirectory(const std::wstring& strDirectory)
+	{
+		if (Exists(strDirectory) == true)  return true;
+
+#if defined(_WIN32) || defined (_WIN64)
+		return FALSE != ::CreateDirectoryW(strDirectory.c_str(), NULL);
+#elif __linux__
+		BYTE* pUtf8 = NULL;
+		LONG lLen = 0;
+        NSFile::CUtf8Converter::GetUtf8StringFromUnicode(strDirectory.c_str(), strDirectory.length(), pUtf8, lLen, false);
+		struct stat st;
+		int nRes = 0;
+		if (stat((char*)pUtf8, &st) == -1) {
+			nRes = mkdir((char*)pUtf8, S_IRWXU | S_IRWXG | S_IRWXO);
+		}
+		delete [] pUtf8;
+		return 0 == nRes;
+#elif MAC
+        BYTE* pUtf8 = NULL;
+        LONG lLen = 0;
+        NSFile::CUtf8Converter::GetUtf8StringFromUnicode(strDirectory.c_str(), strDirectory.length(), pUtf8, lLen, false);
+        struct stat st;
+        int nRes = 0;
+        if (stat((char*)pUtf8, &st) == -1) {
+            nRes = mkdir((char*)pUtf8, S_IRWXU | S_IRWXG | S_IRWXO);
+        }
+        delete [] pUtf8;
+        return 0 == nRes;
+#endif
+        return false;
+	}
+	static void DeleteDirectory(const std::wstring& strDirectory, bool deleteRoot = true)
+	{
+		CArray<std::wstring> aFiles = GetFiles(strDirectory);
+		for(int i = 0; i < aFiles.GetCount(); ++i)
+		{
+			NSFile::CFileBinary::Remove(aFiles[i]);
+		}
+		CArray<std::wstring> aDirectories = GetDirectories(strDirectory);
+		for(int i = 0; i < aDirectories.GetCount(); ++i)
+		{
+			DeleteDirectory(aDirectories[i]);
+		}
+#if defined(_WIN32) || defined (_WIN64)
+		if (deleteRoot) RemoveDirectoryW(strDirectory.c_str());
+#elif __linux__
+		BYTE* pUtf8 = NULL;
+		LONG lLen = 0;
+        NSFile::CUtf8Converter::GetUtf8StringFromUnicode(strDirectory.c_str(), strDirectory.length(), pUtf8, lLen, false);
+		rmdir((char*)pUtf8);
+		delete [] pUtf8;
+
+		if (deleteRoot = false)CreateDirectory(strDirectory);
+#elif MAC
+        BYTE* pUtf8 = NULL;
+        LONG lLen = 0;
+        NSFile::CUtf8Converter::GetUtf8StringFromUnicode(strDirectory.c_str(), strDirectory.length(), pUtf8, lLen, false);
+        rmdir((char*)pUtf8);
+        delete [] pUtf8;
+        
+        if (deleteRoot = false)CreateDirectory(strDirectory);
+#endif
+	}
+}
+
+#endif //_BUILD_DIRECTORY_CROSSPLATFORM_H_
---
+++ DesktopEditor/common/Types.h
@@ -0,0 +1,164 @@
+#ifndef _BUILD_TYPES_CROSSPLATFORM_H_
+#define _BUILD_TYPES_CROSSPLATFORM_H_
+
+#ifndef NULL
+#ifdef __cplusplus
+#define NULL    0
+#else
+#define NULL    ((void *)0)
+#endif
+#endif
+
+#ifndef FALSE
+#define FALSE               0
+#endif
+
+#ifndef TRUE
+#define TRUE                1
+#endif
+
+typedef unsigned long       DWORD;
+
+#if defined (_WIN32) || defined (_WIN64)
+	typedef int                 BOOL;
+#else
+
+	#ifndef _IOS
+	//	typedef int                 BOOL;
+	#else
+		#ifndef _XCODE
+			#define BOOL int
+		#endif
+	#endif
+
+#endif
+typedef unsigned char       BYTE;
+typedef unsigned short      WORD;
+typedef float               REAL, FLOAT, *PFLOAT;
+
+typedef char			CHAR;
+typedef short			SHORT;
+typedef unsigned short	USHORT;
+typedef long			LONG;
+typedef int				INT;
+typedef unsigned int	UINT, *PUINT;
+typedef wchar_t			WCHAR;
+
+#ifdef __linux__
+#include <inttypes.h>
+typedef int64_t     T_LONG64;
+typedef uint64_t    T_ULONG64;
+typedef T_LONG64    __int64;
+
+typedef T_ULONG64   ULONG64;
+typedef T_LONG64    LONG64;
+typedef T_ULONG64     UINT64;
+#else
+
+#ifdef __APPLE__
+#include <inttypes.h>
+typedef int64_t		T_LONG64;
+typedef uint64_t	T_ULONG64;
+typedef T_LONG64    __int64;
+
+typedef T_ULONG64   ULONG64;
+typedef T_LONG64    LONG64;
+typedef T_ULONG64   UINT64;
+
+#define BOOL int
+#else
+
+#if (!defined (_MAC) && (!defined(MIDL_PASS) || defined(__midl)) && (!defined(_M_IX86) || (defined(_INTEGRAL_MAX_BITS) && _INTEGRAL_MAX_BITS >= 64)))
+typedef __int64				T_LONG64;
+typedef unsigned __int64	T_ULONG64;
+#else
+#if defined(_MAC) && defined(_MAC_INT_64)
+typedef __int64				T_LONG64;
+typedef unsigned __int64	T_ULONG64;
+#else
+typedef double T_LONG64;
+typedef double T_ULONG64;
+#endif //_MAC and int64
+#endif
+
+#endif
+
+#endif
+
+#ifndef VOID
+typedef void			VOID, *LPVOID;
+#endif
+
+#ifndef FLT_EPSILON
+#define FLT_EPSILON     1.192092896e-07F        /* smallest such that 1.0+FLT_EPSILON != 1.0 */
+#endif
+
+typedef unsigned long		ULONG, ARGB;
+
+typedef long HRESULT;
+
+#if defined(_WIN32) || defined (_WIN64)
+    #include "winerror.h"
+    #ifndef max
+        #define max(a,b)            (((a) > (b)) ? (a) : (b))
+    #endif
+
+    #ifndef min
+        #define min(a,b)            (((a) < (b)) ? (a) : (b))
+    #endif
+#else
+    #ifndef S_OK
+        #define S_OK                                   ((HRESULT)0x00000000L)
+        #define S_FALSE                                ((HRESULT)0x00000001L)
+    #endif
+#endif
+
+
+#ifndef RGB
+    typedef int RGB;
+    #define RGB(r,g,b) ((r)<<16|(g)<<8|(b))
+#endif
+
+
+#define RELEASEMEM(pobject)\
+{\
+	if (pobject!=NULL)\
+	{\
+		free(pobject);\
+		pobject=NULL;\
+	}\
+}
+#define RELEASEOBJECT(pobject)\
+{\
+	if (pobject!=NULL)\
+	{\
+		delete pobject;\
+		pobject=NULL;\
+	}\
+}
+#define RELEASEARRAYOBJECTS(pobject)\
+{\
+	if (pobject!=NULL)\
+	{\
+		delete []pobject;\
+		pobject=NULL;\
+	}\
+}
+
+#define ADDREFINTERFACE(pinterface)\
+{\
+    if (pinterface!=NULL)\
+    {\
+        pinterface->AddRef();\
+    }\
+}
+#define RELEASEINTERFACE(pinterface)\
+{\
+    if (pinterface!=NULL)\
+    {\
+        pinterface->Release();\
+        pinterface=NULL;\
+    }\
+}
+
+#endif //_BUILD_TYPES_CROSSPLATFORM_H_

72245d4aec0fd8156822bd3ea3b98734a2a3337f
Oleg.Korshul <Oleg.Korshul@onlyoffice.com>
2015-04-30 15:13:01 +0000

    нигде не используется clipboard
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@62378 954022d7-b5bf-4e40-9824-e11837661b57

--- DesktopEditor/clipboard/src/base/string_piece.h
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2013 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// TODO(brettw) move callers to using the new location
-#include "base/strings/string_piece.h"

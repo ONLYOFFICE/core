360230eb81994863b8ce397cfcd5907cd3e88b53
Oleg.Korshul <Oleg.Korshul@onlyoffice.com>
2015-04-30 15:13:01 +0000

    нигде не используется clipboard
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@62378 954022d7-b5bf-4e40-9824-e11837661b57

--- DesktopEditor/clipboard/src/base/message_loop_proxy.h
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2013 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// TODO(brettw) remove this forwarding file when all users reference the new
-// location.
-#include "base/message_loop/message_loop_proxy.h"
--- DesktopEditor/clipboard/src/base/string_number_conversions.h
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// This file has moved, please use the new location.
-// TODO(brettw) remove this file when all users have been updated.
-#include "base/strings/string_number_conversions.h"

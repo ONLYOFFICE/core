7ead87acdef889c22e08cac53ddbdb1f5bbeebe0
Alexey.Musinov <Alexey.Musinov@onlyoffice.com>
2015-07-08 15:55:50 +0000

    [ios] обновлены проекты для сборки конвертера
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@63405 954022d7-b5bf-4e40-9824-e11837661b57

--- XlsxSerializerCom/Reader/BinaryWriter.h
+++ XlsxSerializerCom/Reader/BinaryWriter.h
@@ -3354,7 +3354,11 @@ namespace BinXlsxRW {
 				int nBinBufferLen = oBufferedStream.GetPosition();
 				int nBase64BufferLen = Base64::Base64EncodeGetRequiredLength(nBinBufferLen, Base64::B64_BASE64_FLAG_NOCRLF);
 				BYTE* pbBase64Buffer = new BYTE[nBase64BufferLen];
+#ifdef _IOS
+                if(true == Base64_1::Base64Encode(pbBinBuffer, nBinBufferLen, pbBase64Buffer, &nBase64BufferLen))
+#else
                 if(true == Base64::Base64Encode(pbBinBuffer, nBinBufferLen, (LPSTR)pbBase64Buffer, &nBase64BufferLen, Base64::B64_BASE64_FLAG_NOCRLF))
+#endif
 				{
 					CFile oFile;
 					oFile.CreateFile(sFileDst);
@@ -3503,7 +3507,11 @@ namespace BinXlsxRW {
 		{
 			int nBase64BufferLen = Base64::Base64EncodeGetRequiredLength(nLength, Base64::B64_BASE64_FLAG_NOCRLF);
 			BYTE* pbBase64Buffer = new BYTE[nBase64BufferLen];
+#ifdef _IOS
+            if(true == Base64_1::Base64Encode(pData, nLength, pbBase64Buffer, &nBase64BufferLen))
+#else
             if(true == Base64::Base64Encode(pData, nLength, (LPSTR)pbBase64Buffer, &nBase64BufferLen, Base64::B64_BASE64_FLAG_NOCRLF))
+#endif
 			{
 				CFile oFile;
 				oFile.CreateFile(sFileOutput);
@@ -3688,7 +3696,11 @@ namespace BinXlsxRW {
 			int nBinBufferLen = oBufferedStream.GetPosition();
 			int nBase64BufferLen = Base64::Base64EncodeGetRequiredLength(nBinBufferLen, Base64::B64_BASE64_FLAG_NOCRLF);
 			BYTE* pbBase64Buffer = new BYTE[nBase64BufferLen];
-            if (true == Base64::Base64Encode(pbBinBuffer, nBinBufferLen, (LPSTR)pbBase64Buffer, &nBase64BufferLen, Base64::B64_BASE64_FLAG_NOCRLF))
+#ifdef _IOS
+            if(true == Base64_1::Base64Encode(pbBinBuffer, nBinBufferLen, pbBase64Buffer, &nBase64BufferLen))
+#else
+            if(true == Base64::Base64Encode(pbBinBuffer, nBinBufferLen, (LPSTR)pbBase64Buffer, &nBase64BufferLen, Base64::B64_BASE64_FLAG_NOCRLF))
+#endif
 			{
 				CFile oFile;
 				oFile.CreateFile(sFileOutput);
@@ -3745,7 +3757,11 @@ namespace BinXlsxRW {
 			int nBinBufferLen = oBufferedStream.GetPosition();
 			int nBase64BufferLen = Base64::Base64EncodeGetRequiredLength(nBinBufferLen, Base64::B64_BASE64_FLAG_NOCRLF);
 			BYTE* pbBase64Buffer = new BYTE[nBase64BufferLen];
+#ifdef _IOS
+            if(true == Base64_1::Base64Encode(pbBinBuffer, nBinBufferLen, pbBase64Buffer, &nBase64BufferLen))
+#else
             if(true == Base64::Base64Encode(pbBinBuffer, nBinBufferLen, (LPSTR)pbBase64Buffer, &nBase64BufferLen, Base64::B64_BASE64_FLAG_NOCRLF))
+#endif
 			{
 				CFile oFile;
 				oFile.CreateFile(sFileOutput);

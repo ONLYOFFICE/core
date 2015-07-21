aa7545965498032d3a626783d5c4633113acf215
Oleg.Korshul <Oleg.Korshul@onlyoffice.com>
2015-07-21 15:58:37 +0000

    тестовый пример
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@63625 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ ASCHTMLRenderer2/lib/test/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <QCoreApplication>
+
+#include "../../../PdfReader/PdfReader.h"
+#include "../include/HTMLRenderer3.h"
+#include "../../../DesktopEditor/fontengine/ApplicationFonts.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    std::wstring sFile = L"\\\\KIRILLOV8\\_Office\\PDF\\Android intro(2p).pdf";
+
+    CApplicationFonts oFonts;
+    oFonts.Initialize();
+    PdfReader::CPdfReader oReader(&oFonts);
+    bool bResult = oReader.LoadFromFile(sFile.c_str());
+
+    NSHtmlRenderer::CASCHTMLRenderer3 oHtmlRenderer;
+    oHtmlRenderer.CreateOfficeFile(L"D:\\test\\Document");
+
+    int nPagesCount = oReader.GetPagesCount();
+    for (int i = 0; i < nPagesCount; ++i)
+    {
+        double dWidth = 0;
+        double dHeight = 0;
+
+        oReader.GetPageSize(i, &dWidth, &dHeight);
+
+        oHtmlRenderer.NewPage();
+        oHtmlRenderer.put_Width(dWidth);
+        oHtmlRenderer.put_Height(dHeight);
+
+        oReader.DrawPageOnRenderer(&oHtmlRenderer, i, NULL);
+    }
+
+    oHtmlRenderer.CloseFile();
+
+    return a.exec();
+}

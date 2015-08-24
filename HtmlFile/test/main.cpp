88ef7bcd9dc04e99913385afe8326b16f2d0e9b8
Oleg.Korshul <Oleg.Korshul@onlyoffice.com>
2015-08-24 13:30:59 +0000

    проблема с параллельным запуском под линукс
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@64248 954022d7-b5bf-4e40-9824-e11837661b57

--- HtmlFile/test/main.cpp
+++ HtmlFile/test/main.cpp
@@ -1,5 +1,76 @@
 ﻿#include "../HtmlFile.h"
 #include "../../DesktopEditor/common/File.h"
+#include "../../DesktopEditor/graphics/BaseThread.h"
+#include "../../DesktopEditor/graphics/TemporaryCS.h"
+
+class CCounter
+{
+private:
+    NSCriticalSection::CRITICAL_SECTION m_oCS;
+    int m_lCounter;
+
+public:
+    CCounter()
+    {
+        m_oCS.InitializeCriticalSection();
+        m_lCounter = 0;
+    }
+    ~CCounter()
+    {
+        m_oCS.DeleteCriticalSection();
+    }
+
+public:
+    void Increment(int nCount = 1)
+    {
+        CTemporaryCS oCS(&m_oCS);
+        m_lCounter += nCount;
+    }
+    void Decrement()
+    {
+        CTemporaryCS oCS(&m_oCS);
+        --m_lCounter;
+    }
+    int GetCounter()
+    {
+        CTemporaryCS oCS(&m_oCS);
+        return m_lCounter;
+    }
+};
+
+class CThreadConverter : public NSThreads::CBaseThread
+{
+public:
+    CCounter* m_pCounter;
+    int m_nDstId;
+
+public:
+    CThreadConverter() : NSThreads::CBaseThread()
+    {
+        m_pCounter = NULL;
+        m_nDstId = 1;
+    }
+    void SetCounter(CCounter* pCounter)
+    {
+        m_pCounter = pCounter;
+    }
+
+protected:
+    DWORD ThreadProc()
+    {
+        std::wstring sPath = NSFile::GetProcessDirectory() + L"/../../Internal/linux/Release/";
+        std::wstring sSrc = L"/home/oleg/activex/37898EB";
+        std::wstring sDstFolder = L"/home/oleg/activex/1/" + std::to_wstring(m_nDstId) + L"/";
+
+        CHtmlFile oFile;
+        std::wstring sMetaInfo;
+        int nResult = oFile.ConvertEpub(sSrc, sMetaInfo, sDstFolder, sPath);
+        nResult;
+
+        m_pCounter->Decrement();
+        return 0;
+    }
+};
 
 int main(int argc, char *argv[])
 {
@@ -36,10 +107,29 @@ int main(int argc, char *argv[])
     std::wstring sDstFolder = L"/home/oleg/activex/1/";
 #endif
 
+#if 1
     CHtmlFile oFile;
     std::wstring sMetaInfo;
     int nResult = oFile.ConvertEpub(sSrc, sMetaInfo, sDstFolder, sPath);
     nResult;
+#else
+
+    int nCountTC = 1;
+    CCounter oCounter;
+    oCounter.Increment(nCountTC);
+
+    for (int i = 0; i < nCountTC; i++)
+    {
+        CThreadConverter* pThread = new CThreadConverter();
+        pThread->SetCounter(&oCounter);
+        pThread->m_nDstId = i;
+        pThread->Start(0);
+    }
+
+    while (0 != oCounter.GetCounter())
+        NSThreads::Sleep(100);
+
+#endif
 
 #endif
 

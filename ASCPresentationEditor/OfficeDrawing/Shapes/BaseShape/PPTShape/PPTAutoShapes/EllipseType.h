984af9304263e4519b0f1abe3b84e4bbc11786e2
Sergey.Tsarkov <Sergey.Tsarkov@onlyoffice.com>
2015-08-25 12:07:50 +0000

    добавлен textrect, чтобы текст отображался в автофигуре
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@64296 954022d7-b5bf-4e40-9824-e11837661b57

--- ASCPresentationEditor/OfficeDrawing/Shapes/BaseShape/PPTShape/PPTAutoShapes/EllipseType.h
+++ ASCPresentationEditor/OfficeDrawing/Shapes/BaseShape/PPTShape/PPTAutoShapes/EllipseType.h
@@ -11,5 +11,6 @@ public:
         m_bConcentricFill = true;
 		m_eJoin = NSOfficeDrawing::lineJoinMiter;
         m_strPath = _T("m,10800qy10800,,21600,10800,10800,21600,,10800xe");
+		LoadTextRect(_T("3233,3233,18367,18367"));
     }
 };

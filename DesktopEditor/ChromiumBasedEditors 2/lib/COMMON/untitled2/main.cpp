74e8d2ee8e0a0a7f00403bac19ca70450cacd224
Oleg.Korshul <Oleg.Korshul@onlyoffice.com>
2015-08-29 13:51:12 +0000

    просто чтобы не потерять
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@64396 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ DesktopEditor/ChromiumBasedEditors 2/lib/COMMON/untitled2/main.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QPrintDialog>
+#include <QPrinter>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
---
+++ DesktopEditor/ChromiumBasedEditors 2/lib/COMMON/untitled2/mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+
+private:
+    std::string GetLanguage();
+};
+
+#endif // MAINWINDOW_H

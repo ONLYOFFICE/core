01b7e3c114c957cb1be615c0c7bec57d4686be26
Oleg.Korshul <Oleg.Korshul@onlyoffice.com>
2015-09-02 16:20:39 +0000

    mac common
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@64488 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ DesktopEditor/ChromiumBasedEditors 2/lib/src/mac_common.h
@@ -0,0 +1,8 @@
+#ifndef CEF_MAC_COMMON_H
+#define CEF_MAC_COMMON_H
+
+#include "../include/base.h"
+void MAC_COMMON_set_window_handle_sizes(WindowHandleId handle,
+                                               int x, int y, int w, int h);
+
+#endif // CEF_MAC_COMMON_H

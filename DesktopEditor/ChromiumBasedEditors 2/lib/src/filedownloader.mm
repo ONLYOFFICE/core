3f4cc2a6b03eec590074c2aa34f298c8b6dad0ab
Oleg.Korshul <Oleg.Korshul@onlyoffice.com>
2015-09-02 16:20:39 +0000

    mac common
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@64488 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ DesktopEditor/ChromiumBasedEditors 2/lib/src/filedownloader.mm
@@ -0,0 +1,28 @@
+﻿#include "./filedownloader.h"
+#include <Cocoa/Cocoa.h>
+
+static NSString* StringWToNSString ( const std::wstring& Str )
+{
+    NSString* pString = [ [ NSString alloc ]
+                         initWithBytes : (char*)Str.data()
+                         length : Str.size() * sizeof(wchar_t)
+                         encoding : CFStringConvertEncodingToNSStringEncoding ( kCFStringEncodingUTF32LE ) ];
+    return pString;
+}
+
+int CFileDownloader::DownloadFileAll(std::wstring sFileURL)
+{
+    NSString* stringURL = StringWToNSString(sFileURL);
+    NSURL  *url = [NSURL URLWithString:stringURL];
+    NSData *urlData = [NSData dataWithContentsOfURL:url];
+    if ( urlData )
+    {
+        NSArray       *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString  *documentsDirectory = [paths objectAtIndex:0];
+
+        NSString  *filePath = StringWToNSString ( m_sFilePath );
+        [urlData writeToFile:filePath atomically:YES];
+        return 1;
+    }
+    return 0;
+}

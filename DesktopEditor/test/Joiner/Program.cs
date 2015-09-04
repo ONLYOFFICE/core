9655c11faea4d70958a8d2a81ac764cb356a5e63
Oleg.Korshul <Oleg.Korshul@onlyoffice.com>
2015-09-04 15:53:51 +0000

    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@64530 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ DesktopEditor/test/Joiner/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Joiner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int nType = 0; nType < 3; nType++)
+            {
+                string sPathConfig = "";
+                string sDstFile = "";
+
+                string sPathBase = Directory.GetCurrentDirectory() + "/../../../";
+                if (true)
+                    sPathBase = Directory.GetCurrentDirectory() + "/";
+
+                if (nType == 0)
+                {
+                    sPathConfig = sPathBase + "sdk_configs/webword.json";
+                    sDstFile = sPathBase + "../../Word/sdk-all.js";
+                }
+                else if (nType == 1)
+                {
+                    sPathConfig = sPathBase + "sdk_configs/webexcel.json";
+                    sDstFile = sPathBase + "../../Excel/sdk-all.js";
+                }
+                else
+                {
+                    sPathConfig = sPathBase + "sdk_configs/webpowerpoint.json";
+                    sDstFile = sPathBase + "../../PowerPoint/sdk-all.js";
+                }
+
+                Dictionary<string, bool> map_files = new Dictionary<string, bool>();
+
+                StreamReader oReader = new StreamReader(sPathConfig);
+                string sConfigSource = oReader.ReadToEnd();
+
+                List<int> arCommon = new List<int>();
+                List<int> arWord = new List<int>();
+                List<int> arPowerPoint = new List<int>();
+                List<int> arExcel = new List<int>();
+
+                List<string> arFiles = new List<string>();
+
+                int nStart = 0;
+
+                nStart = 0;
+                while ((nStart = sConfigSource.IndexOf("/Common/", nStart + 1)) >= 0)
+                {
+                    int nFind = sConfigSource.IndexOf('\"', nStart);
+                    arCommon.Add(nStart);
+
+                    string sFile = sConfigSource.Substring(nStart, nFind - nStart);
+                    if (!map_files.ContainsKey(sFile))
+                    {
+                        arFiles.Add(sFile);
+                        map_files.Add(sFile, true);
+                    }
+                }
+
+                nStart = 0;
+                while ((nStart = sConfigSource.IndexOf("/Word/", nStart + 1)) >= 0)
+                {
+                    int nFind = sConfigSource.IndexOf('\"', nStart);
+                    arCommon.Add(nStart);
+
+                    string sFile = sConfigSource.Substring(nStart, nFind - nStart);
+                    if (!map_files.ContainsKey(sFile))
+                    {
+                        arFiles.Add(sFile);
+                        map_files.Add(sFile, true);
+                    }
+                }
+
+                nStart = 0;
+                while ((nStart = sConfigSource.IndexOf("/PowerPoint/", nStart + 1)) >= 0)
+                {
+                    int nFind = sConfigSource.IndexOf('\"', nStart);
+                    arWord.Add(nStart);
+
+                    string sFile = sConfigSource.Substring(nStart, nFind - nStart);
+                    if (!map_files.ContainsKey(sFile))
+                    {
+                        arFiles.Add(sFile);
+                        map_files.Add(sFile, true);
+                    }
+                }
+
+                nStart = 0;
+                while ((nStart = sConfigSource.IndexOf("/Excel/", nStart + 1)) >= 0)
+                {
+                    int nFind = sConfigSource.IndexOf('\"', nStart);
+                    arExcel.Add(nStart);
+
+                    string sFile = sConfigSource.Substring(nStart, nFind - nStart);
+                    if (!map_files.ContainsKey(sFile))
+                    {
+                        arFiles.Add(sFile);
+                        map_files.Add(sFile, true);
+                    }
+                }
+
+                StringBuilder oBuilder = new StringBuilder();
+
+                for (int i = 0; i < arFiles.Count; i++)
+                {
+                    string sFileCandidate = arFiles[i];
+                    if ((sFileCandidate.IndexOf("/Build/") >= 0) ||
+                        (sFileCandidate.IndexOf("3rdparty") >= 0) ||
+                        (sFileCandidate.IndexOf("-all.js") >= 0) ||
+                        (sFileCandidate.IndexOf("apiExport.js") >= 0) ||
+                        (sFileCandidate.LastIndexOf(".js") != (sFileCandidate.Length - 3)))
+                        continue;
+
+
+                    StreamReader oReader2 = new StreamReader(sPathBase + "../.." + arFiles[i]);
+                    oBuilder.Append(oReader2.ReadToEnd());
+
+                    oBuilder.Append("\n\n");
+                }
+
+                StreamWriter oWriter = new StreamWriter(sDstFile, false, Encoding.UTF8);
+
+                oWriter.Write(oBuilder.ToString());
+                oWriter.Close();
+            }
+        }
+    }
+}

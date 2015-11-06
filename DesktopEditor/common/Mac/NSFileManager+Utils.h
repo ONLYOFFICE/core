a8d5205c7989065281024457220436c1127b961d
Alexey.Musinov <Alexey.Musinov@onlyoffice.com>
2015-11-06 11:25:51 +0000

    [ios] update
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@65631 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ DesktopEditor/common/Mac/NSFileManager+Utils.h
@@ -0,0 +1,14 @@
+//
+//  NSFileManager+Utils.h
+//  UTILS
+//
+//  Created by alexey.musinov on 21.09.15.
+//  Copyright © 2015 Ascensio System SIA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSFileManager (Utils)
+- (BOOL)copyFolderAtPath:(NSString *)sourceFolder toDestinationFolderAtPath:(NSString*)destinationFolder;
+- (NSString*)createTemporaryDirectory;
+@end

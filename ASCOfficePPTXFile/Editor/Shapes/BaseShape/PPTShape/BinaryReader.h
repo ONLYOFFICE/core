73edb7c62314b0d40173b719873ba01a478d392d
Elen.Subbotina <Elen.Subbotina@onlyoffice.com>
2015-11-12 08:49:40 +0000

    перенос кода Shapes -> PPTX (OfficeDrawingConverter)
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@65741 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ ASCOfficePPTXFile/Editor/Shapes/BaseShape/PPTShape/BinaryReader.h
@@ -0,0 +1,82 @@
+#pragma once
+#include "../../../../../Common/DocxFormat/Source/Base/Types_32.h"
+
+namespace NSOfficeDrawing
+{
+	#define BINARY_READER_CHECK_OUT_RANGE(index, count)     \
+	if (index > count)										\
+	{														\
+		index = count;										\
+		return 0;											\
+	}
+
+	class CBinaryReader
+	{
+	private:
+		BYTE* m_pBuffer;
+		DWORD m_lCount;
+
+		DWORD m_lOrigin;
+
+	public:
+		CBinaryReader(BYTE* pBuffer, DWORD lCount)
+		{
+			m_pBuffer = pBuffer;
+			m_lCount = lCount;
+
+			m_lOrigin = 0;
+		}
+		~CBinaryReader()
+		{
+			m_pBuffer = NULL;
+			m_lCount = 0;
+		}
+
+	public:
+		LONG ReadLONG() // int32 подразумевается
+		{
+			DWORD lOldOrigin = m_lOrigin;
+			m_lOrigin += 4;
+
+			BINARY_READER_CHECK_OUT_RANGE(m_lOrigin, m_lCount)
+
+			return *(_INT32*)(m_pBuffer + lOldOrigin);
+		}
+		DWORD ReadDWORD()
+		{
+			DWORD lOldOrigin = m_lOrigin;
+			m_lOrigin += 4;
+
+			BINARY_READER_CHECK_OUT_RANGE(m_lOrigin, m_lCount)
+
+			return *(DWORD*)(m_pBuffer + lOldOrigin);
+		}
+		WORD ReadWORD()
+		{
+			DWORD lOldOrigin = m_lOrigin;
+			m_lOrigin += 2;
+
+			BINARY_READER_CHECK_OUT_RANGE(m_lOrigin, m_lCount)
+
+			return *(WORD*)(m_pBuffer + lOldOrigin);
+		}
+		SHORT ReadSHORT()
+		{
+			DWORD lOldOrigin = m_lOrigin;
+			m_lOrigin += 2;
+
+			BINARY_READER_CHECK_OUT_RANGE(m_lOrigin, m_lCount)
+
+			return *(short*)(m_pBuffer + lOldOrigin);
+		}
+		double ReadDOUBLE()
+		{
+			DWORD lOldOrigin = m_lOrigin;
+			m_lOrigin += sizeof(double);
+
+			BINARY_READER_CHECK_OUT_RANGE(m_lOrigin, m_lCount)
+
+			return *(SHORT*)(m_pBuffer + lOldOrigin);
+		}
+	};
+}
---
+++ ASCOfficePPTXFile/Editor/Shapes/BaseShape/PPTXShape/Formula.cpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "Formula.h"
+
+#ifndef min
+#define min(a,b) (((a)<(b))?(a):(b))
+#endif
+#ifndef max
+#define max(a,b) (((a)>(b))?(a):(b))
+#endif
+
+double NSGuidesOOXML::CFormula::Calculate(NSGuidesOOXML::CFormulaManager* pManager)
+{
+	if ((0 == m_lIndex) || (-m_lIndex > pManager->Guides->size()) || (m_lIndex > pManager->Adjustments->size()))
+		return 0.0;
+	if((m_lIndex < 0) && (dNonDefResult > (*pManager->Guides)[-m_lIndex-1]))
+		return (*pManager->Guides)[-m_lIndex-1];
+	if((m_lIndex > 0) && (NonDefResult != (*pManager->Adjustments)[m_lIndex-1]))
+		return (*pManager->Adjustments)[m_lIndex-1];
+	
+	double a = pManager->GetValue(m_lParam1);
+	double b = pManager->GetValue(m_lParam2);
+	double c = pManager->GetValue(m_lParam3);
+	//double c = (double)c1;
+
+	double dRes = 0.0;
+
+	try
+	{
+		// теперь нужно просто посчитать
+		switch (m_eFormulaType)
+		{
+		case ftOOXMLSum:			{ dRes = a + b - c;						break; }
+		case ftOOXMLProduct:		{/*if(0==c1)c=1.0;*/ dRes = (a * b)/c;	break; }
+		case ftOOXMLAddDivide:		{/*if(0==c1)c=1.0;*/ dRes = (a + b)/c;	break; }
+		case ftOOXMLAbsolute:		{ dRes = abs(a);						break; }
+		case ftOOXMLMin:			{ dRes = min(a, b);						break; }
+		case ftOOXMLMax:			{ dRes = max(a, b);						break; }
+		case ftOOXMLIf:				{ dRes = (a > 0) ? b : c;				break; }
+		case ftOOXMLSqrt:			{ dRes = sqrt(a);						break; }
+		case ftOOXMLMod:			{ dRes = sqrt(a*a + b*b + c*c);			break; }
+		case ftOOXMLSin:			{ dRes = a * sin(b * RadKoef);			break; }
+		case ftOOXMLCos:			{ dRes = a * cos(b * RadKoef);			break; }
+		case ftOOXMLTan:			{ dRes = a * tan(b * RadKoef);			break; }
+		case ftOOXMLAtan2:			{ dRes = atan2(b,a)/RadKoef;			break; }
+		case ftOOXMLSinatan2:		{ dRes = a * sin(atan2(c,b));			break; }
+		case ftOOXMLCosatan2:		{ dRes = a * cos(atan2(c,b));			break; }
+		case ftOOXMLPin:			{ dRes = (b < a) ? a :((b > c) ? c : b);break; }
+		case ftOOXMLVal:			{ dRes = a;								break; }
+		default: break;
+		};
+	}
+	catch (...)
+	{
+		dRes = 0.0;
+	}
+
+	//long lResult = (long)dRes;
+	if(m_lIndex < 0)
+	{
+		(*pManager->Guides)[-m_lIndex-1] = dRes;
+		// переопределим формулу
+		pManager->mapGuides.insert(std::pair<CString, long>(m_sName, -m_lIndex-1));
+	}
+	else
+		(*pManager->Adjustments)[m_lIndex-1] = (long)dRes;
+	return dRes;
+}

8440bc5ac57d1705e3c93138ba8c23df79d59e71
Elen.Subbotina <Elen.Subbotina@onlyoffice.com>
2015-11-12 08:54:08 +0000

    правка путей к Shapes
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@65743 954022d7-b5bf-4e40-9824-e11837661b57

--- ASCOfficePPTXFile/Editor/PresentationDrawingsDef.h
+++ ASCOfficePPTXFile/Editor/PresentationDrawingsDef.h
@@ -2,23 +2,23 @@
 #pragma once
 
 #ifdef PPT_DEF
-    #include "../../ASCPresentationEditor/OfficeDrawing/Shapes/BaseShape/PPTShape/Formula.cpp"
-    #include "../../ASCPresentationEditor/OfficeDrawing/Shapes/BaseShape/PPTShape/PPTShape.cpp"
+    #include "./Shapes/BaseShape/PPTShape/Formula.cpp"
+    #include "./Shapes/BaseShape/PPTShape/PPTShape.cpp"
 	#ifndef BASE_SHAPE
 		#define BASE_SHAPE
 	#endif
 #endif
 #ifdef PPTX_DEF
-    #include "../../ASCPresentationEditor/OfficeDrawing/Shapes/BaseShape/PPTXShape/Formula.cpp"
-    #include "../../ASCPresentationEditor/OfficeDrawing/Shapes/BaseShape/PPTXShape/PPTXShape.cpp"
+    #include "./Shapes/BaseShape/PPTXShape/Formula.cpp"
+    #include "./Shapes/BaseShape/PPTXShape/PPTXShape.cpp"
 	#ifndef BASE_SHAPE
 		#define BASE_SHAPE
 	#endif
 
-    #include "../../ASCPresentationEditor/OfficeDrawing/Shapes/BaseShape/PPTShape/ppt2pptxshapeconverter.h"
+    #include "./Shapes/BaseShape/PPTShape/ppt2pptxshapeconverter.h"
 
 #endif
 
 #ifdef BASE_SHAPE
-    #include "./OfficeDrawing/Shapes/BaseShape/BaseShape.cpp"
+    #include "./Shapes/BaseShape/BaseShape.cpp"
 #endif

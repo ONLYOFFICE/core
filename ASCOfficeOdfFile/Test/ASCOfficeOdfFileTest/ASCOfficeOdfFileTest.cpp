38b6a0e08941c3f422ec902562da69aa7b1ad8d6
Elen.Subbotina <Elen.Subbotina@onlyoffice.com>
2016-01-20 10:45:19 +0000

    Подчищены тестовые проекты для ms vs
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@67594 954022d7-b5bf-4e40-9824-e11837661b57

--- ASCOfficeOdfFile/Test/ASCOfficeOdfFileTest/ASCOfficeOdfFileTest.cpp
+++ ASCOfficeOdfFile/Test/ASCOfficeOdfFileTest/ASCOfficeOdfFileTest.cpp
@@ -7,7 +7,6 @@
 #include <string>
 
 #import "../../../Redist/ASCOfficeOdfFile.dll" rename_namespace("ASCOfficeOdfFile"), raw_interfaces_only
-#import "../../../Redist/ASCPresentationEditor.dll" rename_namespace("Editor"), raw_interfaces_only
 
 #define HR_RET(HR) if FAILED(hr = (HR)) { _ASSERTE(false); return -1; }
 
@@ -103,25 +102,11 @@ int ConvertSingle(int argc, _TCHAR* argv[])
     return 0;
 }
 
-int PresentationEditor(int argc, _TCHAR* argv[])
-{
-	ATL::CComPtr<Editor::IAVSPresentationEditor> officeEditor;
-    HRESULT hr;
-	HR_RET(officeEditor.CoCreateInstance(__uuidof(Editor::CAVSPresentationEditor)));
-
-    boost::timer t1;
-	//hr = officeEditor->ConvertToPPTY(ATL::CComBSTR(argv[1]),ATL::CComBSTR(argv[2]));
-    hr = officeEditor->OpenFile(ATL::CComBSTR(argv[1]));
-	officeEditor->Save(1,ATL::CComBSTR(argv[2]));
-    std::cout << "\n\nTime : " << t1.elapsed() << "\n";    
-    return 0;
-}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
     CoInitialize(NULL);
    
-	//return PresentationEditor(argc, argv);
 	return ConvertSingle(argc, argv);
 
 

19d0f147f4e052dc44167c7382dcaa8fa2947f5f
Alexey.Musinov <Alexey.Musinov@onlyoffice.com>
2016-03-22 08:52:45 +0000

    [ios] x2tConverter update
    git-svn-id: svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents@68997 954022d7-b5bf-4e40-9824-e11837661b57

---
+++ X2tConverter/test/iosTest/TestIOSX2tConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestIOSX2tConverter
+//
+//  Created by alexey.musinov on 01.04.15.
+//  Copyright (c) 2015 Ascensio System SIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

0ce73543b6486c1cd2e96cf34a3fb2250e11b194
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2016-05-31 14:27:56 +0300

    no message

--- DesktopEditor/doctrenderer/test_builder/docbuilder/doctrendererwrapper/doctrendererwrapper.cpp
+++ DesktopEditor/doctrenderer/test_builder/docbuilder/doctrendererwrapper/doctrendererwrapper.cpp
@@ -6,9 +6,9 @@
 #include "../../../docbuilder.h"
 
 #ifdef _WIN64
-#pragma comment(lib, "../../../../../SDK/lib/win_64/doctrenderer.lib")
+#pragma comment(lib, "../../../../../build/lib/win_64/doctrenderer.lib")
 #else
-#pragma comment(lib, "../../../../../SDK/lib/win_32/doctrenderer.lib")
+#pragma comment(lib, "../../../../../build/lib/win_32/doctrenderer.lib")
 #endif
 
 namespace doctrendererwrapper {

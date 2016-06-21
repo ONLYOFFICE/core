e12a903ad23cb539f6b1d98a17d5e6a606270d0b
konovalovsergey <Sergey.Konovalov@onlyoffice.com>
2016-06-21 18:15:12 +0300

    delete unused

--- ASCOfficeFile/stdafx.cpp
+++
@@ -1,5 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// ASCOfficeFile.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
--- ASCOfficeFile/version.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-//1
-//0
-//0
-//115
-#define INTVER 1,0,0,115
-#define STRVER "1,0,0,115\0"

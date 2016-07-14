93991cfa134c1ae768b4777900760cf7f450db78
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2016-07-14 18:05:14 +0300

    fetch & build boost mac

--- Common/3dParty/boost/fetch.sh
+++ Common/3dParty/boost/fetch.sh
@@ -3,6 +3,25 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
+os=$(uname -s)
+platform=""
+
+case "$os" in
+  Linux*)   platform="linux" ;;
+  Darwin*)  platform="mac" ;; 
+  *)        exit ;;
+esac
+
+if [[ "$platform" == *"mac"* ]]
+then
+if [[ -f "$SCRIPTPATH/7zX_1.7.1.dmg" ]]
+then
+echo "7z already downloaded"
+else
+wget http://static.updatestar.net/dl/7zX/7zX_1.7.1.dmg
+fi
+fi
+
 if [[ -f "$SCRIPTPATH/boost_1_58_0.7z" ]]
 then
 echo "boost already downloaded"
@@ -13,5 +32,12 @@ fi
 if [ -d "$SCRIPTPATH/boost" ]; then
 echo "boost already extracted"
 else
+if [[ "$platform" == *"linux"* ]]
+then
 7z x "$SCRIPTPATH/boost_1_58_0.7z" -o"$SCRIPTPATH/"
+else
+hdiutil mount "$SCRIPTPATH/7zX_1.7.1.dmg"
+/Volumes/7zX/7zX.app/Contents/Resources/7za x "$SCRIPTPATH/boost_1_58_0.7z" -o"$SCRIPTPATH/"
+hdiutil unmount /Volumes/7zX
+fi
 fi

fdbd735e2675f03abf297e5659bb9f9cab4c219f
konovalovsergey <Sergey.Konovalov@onlyoffice.com>
2016-10-28 16:55:30 +0300

    Add X2tConverterDll.pro to build x2t as dll

---
+++ X2tConverter/build/Qt/X2tSLNDll.pro
@@ -0,0 +1,4 @@
+
+include(X2tSLN.pri)
+
+X2tConverter.file = ./X2tConverterDll.pro
--- X2tConverter/src/ASCConverters.cpp
+++ X2tConverter/src/ASCConverters.cpp
@@ -50,6 +50,7 @@
 #include "../../ASCOfficeOdfFile/src/ConvertOO2OOX.h"
 #include "../../ASCOfficeOdfFileW/source/Oox2OdfConverter/Oox2OdfConverter.h"
 #include "../../DesktopEditor/doctrenderer/doctrenderer.h"
+#include "../../DesktopEditor/doctrenderer/docbuilder.h"
 #include "../../DesktopEditor/fontengine/ApplicationFonts.h"
 #include "../../DesktopEditor/graphics/MetafileToGraphicsRenderer.h"
 #include "../../PdfReader/PdfReader.h"
@@ -2436,5 +2437,334 @@ namespace NExtractTools
         }
         return AVS_FILEUTILS_ERROR_CONVERT;
    }
+
+	int fromInputParams(InputParams& oInputParams)
+	{
+		TConversionDirection conversion  = oInputParams.getConversionDirection();
+		std::wstring sFileFrom	= *oInputParams.m_sFileFrom;
+		std::wstring sFileTo	= *oInputParams.m_sFileTo;
+
+		int nFormatFrom = AVS_OFFICESTUDIO_FILE_UNKNOWN;
+		if(NULL != oInputParams.m_nFormatFrom)
+			nFormatFrom = *oInputParams.m_nFormatFrom;
+		int nFormatTo = AVS_OFFICESTUDIO_FILE_UNKNOWN;
+		if(NULL != oInputParams.m_nFormatTo)
+			nFormatTo = *oInputParams.m_nFormatTo;
+
+		if (TCD_ERROR == conversion)
+		{
+			if(AVS_OFFICESTUDIO_FILE_DOCUMENT_TXT == nFormatFrom || AVS_OFFICESTUDIO_FILE_SPREADSHEET_CSV == nFormatFrom)
+				return AVS_FILEUTILS_ERROR_CONVERT_NEED_PARAMS;
+			else{
+				// print out conversion direction error
+				std::cerr << "Couldn't recognize conversion direction from an argument" << std::endl;
+				return AVS_FILEUTILS_ERROR_CONVERT_PARAMS;
+			}
+		}
+
+		if (sFileFrom.empty() || sFileTo.empty())
+		{
+			std::cerr << "Empty sFileFrom or sFileTo" << std::endl;
+			return AVS_FILEUTILS_ERROR_CONVERT_PARAMS;
+		}
+
+		// get conversion direction from file extensions
+		if (TCD_AUTO == conversion)
+		{
+			conversion = getConversionDirectionFromExt (sFileFrom, sFileTo);
+		}
+
+		if (TCD_ERROR == conversion)
+		{
+			// print out conversion direction error
+			std::cerr << "Couldn't automatically recognize conversion direction from extensions" << std::endl;
+			return AVS_FILEUTILS_ERROR_CONVERT_PARAMS;
+		}
+		bool bFromChanges = false;
+		if(NULL != oInputParams.m_bFromChanges)
+			bFromChanges = *oInputParams.m_bFromChanges;
+		bool bPaid = true;
+		if(NULL != oInputParams.m_bPaid)
+			bPaid = *oInputParams.m_bPaid;
+		std::wstring sThemeDir;
+		if(NULL != oInputParams.m_sThemeDir)
+			sThemeDir = *oInputParams.m_sThemeDir;
+		InputParamsMailMerge* oMailMerge = NULL;
+		if(NULL != oInputParams.m_oMailMergeSend)
+			oMailMerge = oInputParams.m_oMailMergeSend;
+
+		std::wstring sTempDir = FileSystem::Directory::GetFolderPath(sFileFrom) + FILE_SEPARATOR_STR + _T("Temp");
+		NSDirectory::CreateDirectory(sTempDir);
+
+		if (sTempDir.empty())
+		{
+			std::cerr << "Couldn't create temp folder" << std::endl;
+			return AVS_FILEUTILS_ERROR_UNKNOWN;
+		}
+
+		int result = 0;
+		switch(conversion)
+		{
+			case TCD_DOCX2DOCT:
+			{
+				result = docx2doct (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_DOCT2DOCX:
+			{
+				result =  doct2docx (sFileFrom, sFileTo, sTempDir, bFromChanges, sThemeDir, oInputParams);
+			}break;
+			case TCD_XLSX2XLST:
+			{
+				result =  xlsx2xlst (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_XLST2XLSX:
+			{
+				result =  xlst2xlsx (sFileFrom, sFileTo, sTempDir, bFromChanges, sThemeDir, oInputParams);
+			}break;
+			case TCD_PPTX2PPTT:
+			{
+				result =  pptx2pptt (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_PPTT2PPTX:
+			{
+				result =  pptt2pptx (sFileFrom, sFileTo, sTempDir, bFromChanges, sThemeDir, oInputParams);
+			}break;
+			case TCD_ZIPDIR:
+			{
+				result =  dir2zip (sFileFrom, sFileTo);
+			}break;
+			case TCD_UNZIPDIR:
+			{
+				result =  zip2dir (sFileFrom, sFileTo);
+			}break;
+			case TCD_CSV2XLSX:
+			{
+				result =  csv2xlsx (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_CSV2XLST:
+			{
+				result =  csv2xlst (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_XLSX2CSV:
+			{
+				result =  xlsx2csv (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_XLST2CSV:
+			{
+				result =  xlst2csv (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_DOCX2DOCT_BIN:
+			{
+				result = docx2doct_bin (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_DOCT_BIN2DOCX:
+			{
+				result =  doct_bin2docx (sFileFrom, sFileTo, sTempDir, bFromChanges, sThemeDir, oInputParams);
+			}break;
+			case TCD_XLSX2XLST_BIN:
+			{
+				result =  xlsx2xlst_bin (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_XLST_BIN2XLSX:
+			{
+				result =  xlst_bin2xlsx (sFileFrom, sFileTo, sTempDir, bFromChanges, sThemeDir, oInputParams);
+			}break;
+			case TCD_PPTX2PPTT_BIN:
+			{
+				result =  pptx2pptt_bin (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_PPTT_BIN2PPTX:
+			{
+				result =  pptt_bin2pptx (sFileFrom, sFileTo, sTempDir, bFromChanges, sThemeDir, oInputParams);
+			}break;
+			case TCD_BIN2PDF:
+			{
+				result =  bin2pdf (sFileFrom, sFileTo, sTempDir, bPaid, sThemeDir, oInputParams);
+			}break;
+			case TCD_BIN2T:
+			{
+				result =  dir2zip (FileSystem::Directory::GetFolderPath(sFileFrom), sFileTo);
+			}break;
+			case TCD_T2BIN:
+			{
+				result =  zip2dir (sFileFrom, FileSystem::Directory::GetFolderPath(sFileTo));
+			}break;
+			case TCD_PPSX2PPTX:
+			{
+				result =  ppsx2pptx (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_PPT2PPTX:
+			{
+				result =  ppt2pptx (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}
+			case TCD_PPT2PPTT:
+			{
+				result =  ppt2pptt (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_PPT2PPTT_BIN:
+			{
+				result =  ppt2pptt_bin (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_RTF2DOCX:
+			{
+				result =  rtf2docx (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_RTF2DOCT:
+			{
+				result = rtf2doct (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_RTF2DOCT_BIN:
+			{
+				result = rtf2doct_bin (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_DOCX2RTF:
+			{
+				result =  docx2rtf (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_DOC2DOCX:
+			{
+				result =  doc2docx (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_DOCT2RTF:
+			{
+				result =  doct2rtf (sFileFrom, sFileTo, sTempDir, bFromChanges, sThemeDir, oInputParams);
+			}break;
+			case TCD_DOCT_BIN2RTF:
+			{
+				result =  doct_bin2rtf (sFileFrom, sFileTo, sTempDir, bFromChanges, sThemeDir, oInputParams);
+			}break;
+			case TCD_TXT2DOCX:
+			{
+				result =  txt2docx (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_TXT2DOCT:
+			{
+				result = txt2doct (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_TXT2DOCT_BIN:
+			{
+				result = txt2doct_bin (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_XLS2XLSX:
+			{
+				result =  xls2xlsx (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_XLS2XLST:
+			{
+				result = xls2xlst (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_XLS2XLST_BIN:
+			{
+				result = xls2xlst_bin (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_ODF2OOX:
+			{
+				result =  odf2oox (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_ODF2OOT:
+			{
+				result = odf2oot (sFileFrom, sFileTo,  sTempDir, oInputParams);
+			}break;
+			case TCD_ODF2OOT_BIN:
+			{
+				result = odf2oot_bin (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}
+			case TCD_DOCX2ODT:
+			{
+				result =  docx2odt (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_XLSX2ODS:
+			{
+				result =  xlsx2ods (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_MAILMERGE:
+			{
+				result = convertmailmerge(*oMailMerge, sFileFrom, sFileTo, sTempDir, bPaid, sThemeDir, oInputParams);
+			}break;
+			case TCD_DOCUMENT2:
+			{
+				result = fromDocument(sFileFrom, nFormatFrom, sTempDir, oInputParams);
+			}break;
+			case TCD_SPREADSHEET2:
+			{
+				result = fromSpreadsheet(sFileFrom, nFormatFrom, sTempDir, oInputParams);
+			}break;
+			case TCD_PRESENTATION2:
+			{
+				result = fromPresentation(sFileFrom, nFormatFrom, sTempDir, oInputParams);
+			}break;
+			case TCD_T2:
+			{
+				result = fromT(sFileFrom, nFormatFrom, sFileTo, nFormatTo, sTempDir, sThemeDir, bFromChanges, bPaid, oInputParams);
+			}break;
+			case TCD_DOCT_BIN2:
+			{
+				result = fromDoctBin(sFileFrom, sFileTo, nFormatTo, sTempDir, sThemeDir, bFromChanges, bPaid, oInputParams);
+			}break;
+			case TCD_XLST_BIN2:
+			{
+				result = fromXlstBin(sFileFrom, sFileTo, nFormatTo, sTempDir, sThemeDir, bFromChanges, bPaid, oInputParams);
+			}break;
+			case TCD_PPTT_BIN2:
+			{
+				result = fromPpttBin(sFileFrom, sFileTo, nFormatTo, sTempDir, sThemeDir, bFromChanges, bPaid, oInputParams);
+			}break;
+			case TCD_CROSSPLATFORM2:
+			{
+				result = fromCrossPlatform(sFileFrom, nFormatFrom, sFileTo, nFormatTo, sTempDir, sThemeDir, bFromChanges, bPaid, oInputParams);
+			}break;
+			case TCD_CANVAS_PDF2:
+			{
+				result = fromCanvasPdf(sFileFrom, nFormatFrom, sFileTo, nFormatTo, sTempDir, sThemeDir, bFromChanges, bPaid, oInputParams);
+			}break;
+			case TCD_MSCRYPT2:
+			{
+				result = fromMscrypt (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_MSCRYPT2DOCT:
+			case TCD_MSCRYPT2XLST:
+			case TCD_MSCRYPT2PPTT:
+			{
+				result = mscrypt2oot (sFileFrom, sFileTo, sTempDir, oInputParams);
+			}break;
+			case TCD_MSCRYPT2BIN:
+				result =  mscrypt2oot_bin (sFileFrom, sFileTo, sTempDir, oInputParams);
+			{
+			}break;
+		}
+
+		// delete temp dir
+		NSDirectory::DeleteDirectory(sTempDir);
+
+		//clean up v8
+		NSDoctRenderer::CDocBuilder::Dispose();
+		return result;
+	}
+
+	int FromFile(const std::wstring& file)
+	{
+		InputParams oInputParams;
+		if(oInputParams.FromXmlFile(file))
+		{
+			return fromInputParams(oInputParams);
+		}
+		else
+		{
+			return AVS_FILEUTILS_ERROR_CONVERT_PARAMS;
+		}
+	}
+
+	int FromXml(const std::wstring& xml)
+	{
+		InputParams oInputParams;
+		if(oInputParams.FromXml(xml))
+		{
+			return fromInputParams(oInputParams);
+		}
+		else
+		{
+			return AVS_FILEUTILS_ERROR_CONVERT_PARAMS;
+		}
+	}
 }
 

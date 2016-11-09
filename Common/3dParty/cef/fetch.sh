b7842e18b152d0da792e3a0118d964cfd404777f
Alexey Golubev <Alexey.Golubev@onlyoffice.com>
2016-11-09 18:59:07 +0300

    fixed fetch cef error

--- Common/3dParty/cef/fetch.sh
+++ Common/3dParty/cef/fetch.sh
@@ -38,18 +38,18 @@ fi
 
 if [[ "$platform" == *"linux"* ]]
 then
-if [[ -f "$SCRIPTPATH/cef_binary.7z" ]]
+if [[ -f "cef_binary.7z" ]]
 then 
 echo "cef_binary already downloaded"
 else
 wget http://d2ettrnqo7v976.cloudfront.net/cef/2454/$platform$arch/cef_binary.7z
 fi
-if [ -d "./$SCRIPTPATH/cef" ]
+if [ -d cef_binary ]
 then
 echo "cef_binary already extracted"
 else
 
-7z x -y $SCRIPTPATH/cef_binary.7z -o$SCRIPTPATH/
+7z x -y cef_binary.7z
 fi
-cp -t build ./Release/*  ./Resources/*
+cp -r -t build/ ./cef_binary/Release/*  ./cef_binary/Resources/*
 fi

cb798f59369585f2306c4b17ad68a60b7ac12063
Alexey Golubev <Alexey.Golubev@onlyoffice.com>
2016-11-15 16:54:57 +0300

    Skip fetch and build boost if it already done

--- Common/3dParty/boost/build.sh
+++ Common/3dParty/boost/build.sh
@@ -24,32 +24,30 @@ outputdir="$SCRIPTPATH/boost_1_58_0/build/$platform$arch"
 echo $outputdir
 
 cd "$SCRIPTPATH/boost_1_58_0"
-./bootstrap.sh --with-libraries=filesystem,system,date_time,regex
 
 folder="build/$platform$arch"
-if [ -d $folder ]; then
-  rm -R $folder
+if [ ! -d $folder ]; then
+  ./bootstrap.sh --with-libraries=filesystem,system,date_time,regex
+
+   stage="stage"
+   if [ -d $stage ]; then
+     rm -R $stage
+   fi
+
+  mkdir -p "$folder"
+  mkdir -p "$folder/static"
+  mkdir -p "$folder/static_fpic"
+  mkdir -p "$folder/shared"
+
+  ./b2 --clean
+  ./bjam link=static
+  cp stage/lib/* "$folder/static/"
+
+  ./b2 --clean
+  ./bjam link=static cxxflags=-fPIC
+  cp stage/lib/* "$folder/static_fpic/"
+
+  ./b2 --clean
+  ./bjam link=shared
+  cp stage/lib/* "$folder/shared/"
 fi
-
-stage="stage"
-if [ -d $stage ]; then
-  rm -R $stage
-fi
-
-mkdir -p "$folder"
-mkdir -p "$folder/static"
-mkdir -p "$folder/static_fpic"
-mkdir -p "$folder/shared"
-
-./b2 --clean
-./bjam link=static
-cp stage/lib/* "$folder/static/"
-
-./b2 --clean
-./bjam link=static cxxflags=-fPIC
-cp stage/lib/* "$folder/static_fpic/"
-
-./b2 --clean
-./bjam link=shared
-cp stage/lib/* "$folder/shared/"
-

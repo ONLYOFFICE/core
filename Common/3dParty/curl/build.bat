3c9a91af452105657b40a09f12df479435cb9199
Alexey.Golubev <alexey.golubev@onlyoffice.com>
2017-07-03 14:46:56 +0300

    Added scripts for fetching and building curl

---
+++ Common/3dParty/curl/build.bat
@@ -0,0 +1,39 @@
+SET SCRIPTPATH=%~dp0
+CD /D %~dp0
+SET PLATFORM=win_32
+if defined ProgramFiles(x86) (
+	SET PLATFORM=win_64
+)
+
+if defined TARGET (
+	SET PLATFORM=%TARGET%
+)
+
+if "%PLATFORM%" == "win_32" (
+  SET MACHINE=x86
+)
+
+if "%PLATFORM%" == "win_64" (
+  SET MACHINE=x64
+)
+
+SET VC="%ProgramFiles%\Microsoft Visual Studio 12.0\VC"
+SET VC64="%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\VC"
+if exist %VC64% (
+	SET VC=%VC64%
+)
+
+call %VC%\vcvarsall.bat %MACHINE%
+
+cd curl
+call buildconf.bat
+cd winbuild
+nmake /f Makefile.vc mode=static ENABLE_WINSSL=yes MACHINE=%MACHINE% VC=12
+
+if not exist "%SCRIPTPATH%%PLATFORM%" (
+   md %SCRIPTPATH%%PLATFORM%\build
+   xcopy /Y /S ..\builds\libcurl-vc12-%MACHINE%-release-static-ipv6-sspi-winssl\lib\libcurl_a.lib %SCRIPTPATH%%PLATFORM%\build\
+   mv %SCRIPTPATH%%PLATFORM%\build\libcurl_a.lib %SCRIPTPATH%%PLATFORM%\build\curl.lib
+)
+
+cd ..\..

22f986295f5d753af5f0d00a89753ae50de7f331
ElenaSubbotina <Elena.Subbotina@onlyoffice.com>
2017-10-12 18:17:16 +0300

    .

--- OfficeCryptReader/Test/Test.cpp
+++ OfficeCryptReader/Test/Test.cpp
@@ -5,19 +5,22 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	std::wstring srcFileName	= L"D:\\test\\_crypted\\test-password-2016.docx";
-	std::wstring dstFileName	= srcFileName + L".oox";
-	std::wstring dstFileName2	= dstFileName + L"-mycrypt.docx";
+	//std::wstring srcFileName	= L"D:\\test\\_crypted\\test-password-2016.docx";
+	//std::wstring dstFileName	= srcFileName + L".oox";
+	//std::wstring dstFileName2	= dstFileName + L"-mycrypt.docx";
+	
+	std::wstring srcFileName	= L"D:\\test\\_crypted\\test.docx";
+	std::wstring dstFileName2	= srcFileName + L"-mycrypt.docx";
 
-	std::wstring password = L"password";
+	std::wstring password = L"password132eqdqdwewedwdwwskskms09elzwewedskjsdnkjsdnjksjsnkcsdncskjdnss";
 
 	ECMACryptFile crypt_file;
 	bool result, bDataIntegrity;
 		
-	result = crypt_file.DecryptOfficeFile(srcFileName, dstFileName, password, bDataIntegrity);
+	//result = crypt_file.DecryptOfficeFile(srcFileName, dstFileName, password, bDataIntegrity);
 
-	result = crypt_file.EncryptOfficeFile(dstFileName, dstFileName2, password);
-
+	//result = crypt_file.EncryptOfficeFile(dstFileName, dstFileName2, password);
+	result = crypt_file.EncryptOfficeFile(srcFileName, dstFileName2, password);
 	return 0;
 }
 

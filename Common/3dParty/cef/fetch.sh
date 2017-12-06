fb0dbfc2317433bc964f0847c451c67b87d4bc4b
Alexey Golubev <Alexey.Golubev@onlyoffice.com>
2017-12-06 17:19:46 +0300

    Cherrypick from develop

--- Common/3dParty/cef/fetch.sh
+++ Common/3dParty/cef/fetch.sh
@@ -8,7 +8,7 @@ platform=""
 
 case "$os" in
   Linux*)   platform="linux" ;;
-  Darwin*)  platform="mac" ;; 
+  Darwin*)  platform="mac" ;;
   *)        exit ;;
 esac
 
@@ -35,22 +35,38 @@ echo ""
 else
 mkdir "build"
 fi
+cef_binary=cef_binary
+cef_arch=$cef_binary.7z
+cef_url=http://d2ettrnqo7v976.cloudfront.net/cef/3163/$platform$arch/$cef_arch
 
 if [[ "$platform" == *"linux"* ]]
 then
-if [[ -f "cef_binary.7z" ]]
-then 
-echo "cef_binary already downloaded"
-else
-wget http://d2ettrnqo7v976.cloudfront.net/cef/2454/$platform$arch/cef_binary.7z
-fi
-if [ -d cef_binary ]
-then
-echo "cef_binary already extracted"
-else
 
-7z x -y cef_binary.7z
-fi
-cp -r -t build/ ./cef_binary/Release/*  ./cef_binary/Resources/*
-chmod a+xr build/locales
+  if [[ -f $cef_arch ]]
+  then
+    cef_mod_time=$(date -d"$(curl -sI $cef_url | awk '/Last-Modified/ {print ($3, $4, $5, $6, $7, $8)}')" +"%s")
+    local_mod_time=$(stat -c %Y $cef_arch)
+    echo $cef_mod_time
+    echo $local_mod_time
+    if [[ $cef_mod_time -eq $local_mod_time ]]
+    then
+      echo "cef_binary already downloaded"
+    else
+      wget $cef_url -O $cef_arch
+      rm -fr $cef_binary/
+    fi
+  else
+    wget $cef_url
+    rm -fr $cef_binary/
+  fi
+
+  if [ -d $cef_binary ]
+  then
+    echo "cef_binary already extracted"
+  else
+    7z x -y $cef_arch
+  fi
+
+  cp -r -t build/ ./$cef_binary/Release/*  ./$cef_binary/Resources/*
+  chmod a+xr build/locales
 fi

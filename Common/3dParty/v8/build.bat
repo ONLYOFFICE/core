d44ef86319abcc13d28d0119aa2832b418adae3d
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2018-01-11 11:35:45 +0300

    .

--- Common/3dParty/v8/build.bat
+++ Common/3dParty/v8/build.bat
@@ -5,11 +5,13 @@ SET PATH=%SCRIPTPATH%depot_tools;%PATH%
 SET DEPOT_TOOLS_WIN_TOOLCHAIN=0
 SET GYP_MSVS_VERSION=2015
 
-cd v8
-call gn gen out.gn/win_64 --args="is_debug=false target_cpu=\"x64\" v8_target_cpu=\"x64\" v8_static_library=true is_component_build=false v8_use_snapshot=false is_clang=false"
-
-cd ../
 call powershell -File .\fix-static_crt.ps1
 cd v8
 
-call ninja -C out.gn/win_64
+call gn gen out.gn/win_64/release --args="is_debug=false target_cpu=\"x64\" v8_target_cpu=\"x64\" v8_static_library=true is_component_build=false v8_use_snapshot=false is_clang=false"
+call ninja -C out.gn/win_64/release
+
+call gn gen out.gn/win_64/debug --args="is_debug=true target_cpu=\"x64\" v8_target_cpu=\"x64\" v8_static_library=true is_component_build=false v8_use_snapshot=false is_clang=false"
+call ninja -C out.gn/win_64/debug
+
+rem v8_use_snapshot=true v8_use_external_startup_data=true
--- Common/3dParty/v8/v8.pri
+++ Common/3dParty/v8/v8.pri
@@ -6,7 +6,7 @@ INCLUDEPATH += \
     $$CORE_V8_PATH_INCLUDE/include
 
 core_windows {
-    #CORE_V8_PATH_LIBS = $$CORE_V8_PATH_INCLUDE/out.gn/$$CORE_BUILDS_PLATFORM_PREFIX_$$CORE_BUILDS_CONFIGURATION_PREFIX/obj
+    CORE_V8_PATH_LIBS = $$CORE_V8_PATH_INCLUDE/out.gn/$$CORE_BUILDS_PLATFORM_PREFIX/$$CORE_BUILDS_CONFIGURATION_PREFIX/obj
 
     LIBS += -L$$CORE_V8_PATH_LIBS -lv8_base -lv8_libplatform -lv8_libbase -lv8_snapshot  -lv8_libsampler
     LIBS += -L$$CORE_V8_PATH_LIBS/third_party/icu -licui18n -licuuc

62ba68aaa8a419bd63877a6ea7e7bff79883b5db
Alexey.Golubev <alexey.golubev@onlyoffice.com>
2018-04-03 18:25:01 +0300

    Add icu building from source code on windows

--- Common/3dParty/icu/fetch.bat
+++ Common/3dParty/icu/fetch.bat
@@ -10,6 +10,14 @@ if defined TARGET (
 	SET platform=%TARGET%
 )
 
+if "%TARGET%" == "win_32" (
+  SET MACHINE=x86
+)
+
+if "%TARGET%" == "win_64" (
+  SET MACHINE=x64
+)
+
 if not exist "%platform%" (
 	md "%platform%"
 )
@@ -18,38 +26,36 @@ if not exist "%platform%\build" (
 	md "%platform%\build"
 )
 
-if "%platform%" == "win_64" (
-	SET URL=http://download.icu-project.org/files/icu4c/55.1/icu4c-55_1-Win64-msvc10.zip
-) else (
-	SET URL=http://download.icu-project.org/files/icu4c/55.1/icu4c-55_1-Win32-msvc10.zip
-)
-
-if exist "%SCRIPTPATH%%platform%\icu.zip" (
-    echo "icu already downloaded"
-) else (
-    Powershell.exe Invoke-WebRequest -OutFile %platform%\icu.zip -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox "%URL%"
-)
-
-SET UNSIP_PROGRAMM="C:\Program Files\7-Zip\7z.exe"
-SET UNSIP_PROGRAMM2="C:\Program Files (x86)\7-Zip\7z.exe"
-if exist %UNSIP_PROGRAMM2% (
-	SET UNSIP_PROGRAMM=%UNSIP_PROGRAMM2%
-)
+cd "%SCRIPTPATH%%platform%"
 
 if exist "%SCRIPTPATH%%platform%\icu\" (
-	echo "icu already extracted"
+	echo "icu already exported"
 ) else (
-    call %UNSIP_PROGRAMM% x "%SCRIPTPATH%%platform%\icu.zip" -o"%SCRIPTPATH%%platform%\"
+	svn export http://source.icu-project.org/repos/icu/tags/release-60-2/icu4c ./icu
+)
+
+SET VC=%ProgramFiles%\Microsoft Visual Studio 14.0\VC
+SET VC64=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC
+if exist %VC64% (
+	SET VC=%VC64%
+)
+
+call "%VC%\vcvarsall.bat" %MACHINE%
+
+if "%platform%" == "win_64" (
+	devenv.com icu\source\allinone\allinone.sln /build "Release|x64"
+) else (
+	devenv.com icu\source\allinone\allinone.sln /build "Release|Win32"
 )
 
 if "%platform%" == "win_64" (
-XCOPY /Y "%SCRIPTPATH%%platform%\icu\bin64\icudt55.dll" "%SCRIPTPATH%%platform%\build\"
-XCOPY /Y "%SCRIPTPATH%%platform%\icu\bin64\icuuc55.dll" "%SCRIPTPATH%%platform%\build\"
+XCOPY /Y "%SCRIPTPATH%%platform%\icu\bin64\icudt60.dll" "%SCRIPTPATH%%platform%\build\"
+XCOPY /Y "%SCRIPTPATH%%platform%\icu\bin64\icuuc60.dll" "%SCRIPTPATH%%platform%\build\"
 XCOPY /Y "%SCRIPTPATH%%platform%\icu\lib64\icudt.lib" "%SCRIPTPATH%%platform%\build\"
 XCOPY /Y "%SCRIPTPATH%%platform%\icu\lib64\icuuc.lib" "%SCRIPTPATH%%platform%\build\"
 ) else (
-XCOPY /Y "%SCRIPTPATH%%platform%\icu\bin\icudt55.dll" "%SCRIPTPATH%%platform%\build\"
-XCOPY /Y "%SCRIPTPATH%%platform%\icu\bin\icuuc55.dll" "%SCRIPTPATH%%platform%\build\"
+XCOPY /Y "%SCRIPTPATH%%platform%\icu\bin\icudt60.dll" "%SCRIPTPATH%%platform%\build\"
+XCOPY /Y "%SCRIPTPATH%%platform%\icu\bin\icuuc60.dll" "%SCRIPTPATH%%platform%\build\"
 XCOPY /Y "%SCRIPTPATH%%platform%\icu\lib\icudt.lib" "%SCRIPTPATH%%platform%\build\"
 XCOPY /Y "%SCRIPTPATH%%platform%\icu\lib\icuuc.lib" "%SCRIPTPATH%%platform%\build\"
 )

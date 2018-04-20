23ca735f231937ab3095dd492fa784c50c00edfc
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2018-04-20 11:19:07 +0300

    Add not used String::format

--- DesktopEditor/common/String.h
+++ DesktopEditor/common/String.h
@@ -40,6 +40,66 @@
 
 #include "../../Common/kernel_config.h"
 
+#ifdef USE_STRING_FORMAT
+#include <stdarg.h>
+namespace NSStrings
+{
+    static std::string format(const char *fmt, ...)
+    {
+        va_list args;
+        va_start(args, fmt);
+        std::vector<char> v(1024);
+        while (true)
+        {
+            va_list args2;
+            va_copy(args2, args);
+            int res = vsnprintf(v.data(), v.size(), fmt, args2);
+            if ((res >= 0) && (res < static_cast<int>(v.size())))
+            {
+                va_end(args);
+                va_end(args2);
+                return std::string(v.data());
+            }
+            size_t size;
+            if (res < 0)
+                size = v.size() * 2;
+            else
+                size = static_cast<size_t>(res) + 1;
+            v.clear();
+            v.resize(size);
+            va_end(args2);
+        }
+    }
+
+    static std::wstring format(const wchar_t *fmt, ...)
+    {
+        va_list args;
+        va_start(args, fmt);
+        std::vector<wchar_t> v(1024);
+        while (true)
+        {
+            va_list args2;
+            va_copy(args2, args);
+            int res = vswprintf(v.data(), v.size(), fmt, args2);
+            if ((res >= 0) && (res < static_cast<int>(v.size())))
+            {
+                va_end(args);
+                va_end(args2);
+                return std::wstring(v.data());
+            }
+            size_t size;
+            if (res < 0)
+                size = v.size() * 2;
+            else
+                size = static_cast<size_t>(res) + 1;
+            v.clear();
+            v.resize(size);
+            va_end(args2);
+        }
+    }
+}
+#endif
+
 namespace NSStringExt
 {
     class KERNEL_DECL CConverter

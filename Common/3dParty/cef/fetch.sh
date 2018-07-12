ONLYOFFICE/core

commit 51569b626fc38c66d4dbc98c60d8f21d88d2332b

Fix build on macOS

Common/3dParty/cef/fetch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT=$(readlink -f "$0" || grealpath "$0")
+SCRIPT=$(readlink -n "$0" || grealpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
 os=$(uname -s)

ASCOfficeOdfFile/linux/OdfFileReaderLib.pro
@@ -21,14 +21,9 @@ include($$PWD/../../Common/3dParty/boost/boost.pri)
 
 DEFINES +=  UNICODE \
             _UNICODE \
-            _USE_LIBXML2_READER_ \
-            LIBXML_READER_ENABLED \
             DONT_WRITE_EMBEDDED_FONTS
 
 INCLUDEPATH += ../include
-INCLUDEPATH += ../../DesktopEditor/freetype-2.5.2/include
-INCLUDEPATH += ../../DesktopEditor/xml/libxml2/include
-INCLUDEPATH += ../../DesktopEditor/xml/build/qt
 
 CONFIG(debug, debug|release){
 DEFINES +=  _DEBUG

ASCOfficeOdfFile/src/xml/sax_xmllite.cpp
@@ -34,7 +34,6 @@
 #include "../../../Common/DocxFormat/Source/Base/Nullable.h"
 
 #include "../../../DesktopEditor/xml/include/xmlutils.h"
-#include "../../../DesktopEditor/xml/src/xmllight_private.h"
 
 #include "../../include/cpdoccore/xml/sax.h"
 
@@ -73,7 +72,7 @@ public:
     virtual NodeType next(int Depth);
     
 private:
-    smart_ptr< XmlUtils::CXmlLiteReader_Private > xml_;
+    smart_ptr< XmlUtils::CXmlLiteReader > xml_;
 
 };
 
@@ -113,7 +112,7 @@ namespace
 
 saxXmlLiteReader::saxXmlLiteReader(const wchar_t * FileName)
 {
-    xml_ = new XmlUtils::CXmlLiteReader_Private();
+    xml_ = new XmlUtils::CXmlLiteReader();
 	
 	xml_->FromFile(FileName);
 
@@ -157,20 +156,14 @@ std::wstring saxXmlLiteReader::namespacePrefix()
 {
 	if (xml_.IsInit() == false) return _T("");
 
-    xmlChar* pName = xmlTextReaderPrefix(xml_->getNativeReader());
-	if (NULL == pName)
-		return L"";
-
-	std::wstring sTemp = NSFile::CUtf8Converter::GetUnicodeStringFromUTF8((BYTE*)pName, (LONG)strlen((const char*)pName));
-	free(pName);
-	return sTemp;
+    return xml_->GetNamespacePrefix();
 }
 
 NodeType saxXmlLiteReader::nodeType()
 {
 	if (xml_.IsInit() == false) return typeNone;	
 
-	XmlUtils::XmlNodeType nTempType = (XmlUtils::XmlNodeType)xmlTextReaderNodeType(xml_->getNativeReader());
+    XmlUtils::XmlNodeType nTempType = xml_->GetNodeType();
 	return NodeTypeConvert(nTempType); 
 }
 
@@ -214,14 +207,14 @@ bool saxXmlLiteReader::attrDefault()
 {
 	if (xml_.IsInit() == false) return false;
 	
-    return (FALSE != xmlTextReaderIsDefault(xml_->getNativeReader()));
+    return xml_->IsDefaultAttribute();
 }
 
 NodeType saxXmlLiteReader::next(int Depth)
 {
 	if (xml_->ReadNextSiblingNode2(Depth))
 	{
-		XmlUtils::XmlNodeType nTempType = (XmlUtils::XmlNodeType)xmlTextReaderNodeType(xml_->getNativeReader());
+        XmlUtils::XmlNodeType nTempType = xml_->GetNodeType();
 		return NodeTypeConvert(nTempType);
 	}
 	else 

ASCOfficeOdfFileW/source/OdfFormat/ods_table_state.cpp
@@ -254,7 +254,7 @@ void ods_table_state::add_column(office_element_ptr & elm, unsigned int repeated
 	odf_writer::style* style = dynamic_cast<odf_writer::style*>(style_elm.get());
 	if (style)style_name = style->style_name_;
 
-	ods_element_state state = {elm, repeated, style_name, style_elm, defaut_column_width_ , current_level_.size()};
+    ods_element_state state = {elm, repeated, style_name, style_elm, defaut_column_width_ , (unsigned int)current_level_.size()};
   
 	if (repeated > 10000)repeated = 1024;//????
 
@@ -343,7 +343,7 @@ void ods_table_state::add_row(office_element_ptr & elm, unsigned int repeated, o
 	odf_writer::style* style = dynamic_cast<odf_writer::style*>(style_elm.get());
 	if (style)style_name = style->style_name_;
 
-	ods_element_state state = {elm, repeated, style_name, style_elm, defaut_row_height_ , current_level_.size()};
+    ods_element_state state = {elm, repeated, style_name, style_elm, defaut_row_height_ , (unsigned int)current_level_.size()};
   
     rows_.push_back(state);
 

Common/kernel.pro
@@ -23,6 +23,7 @@ include(../Common/base.pri)
 CONFIG += build_all_zlib build_zlib_as_sources
 include(../OfficeUtils/OfficeUtils.pri)
 
+CONFIG += core_static_link_xml_full
 include(../DesktopEditor/xml/build/qt/libxml2.pri)
 
 # DOWNLOADER

DesktopEditor/xml/include/xmlutils.h
@@ -92,15 +92,15 @@ namespace XmlUtils
 		bool ReadTillEnd(int nDepth = -2);
 		
 		std::wstring	GetName();
-        std::string     GetNameA();
-        int             GetDepth();
-        bool            IsEmptyNode();
+                std::string     GetNameA();
+                int             GetDepth();
+                bool            IsEmptyNode();
 
 		std::wstring	GetText();
-        std::string     GetTextA();
+                std::string     GetTextA();
 
 		std::wstring	GetText2();
-        std::string     GetText2A();
+                std::string     GetText2A();
 		
 		std::wstring	GetText3();
 
@@ -112,7 +112,11 @@ namespace XmlUtils
 		bool MoveToNextAttribute();
 		bool MoveToElement();
 
-        bool IsEmptyElement();
+                bool IsEmptyElement();
+
+                std::wstring GetNamespacePrefix();
+                XmlNodeType GetNodeType();
+                bool IsDefaultAttribute();
 	};
 
 	class KERNEL_DECL IXmlDOMDocument

DesktopEditor/xml/src/xmllight.cpp
@@ -187,4 +187,17 @@ namespace XmlUtils
 	{
 		return m_pInternal->IsEmptyElement();
 	}
+
+    std::wstring CXmlLiteReader::GetNamespacePrefix()
+    {
+        return m_pInternal->GetNamespacePrefix();
+    }
+    XmlNodeType CXmlLiteReader::GetNodeType()
+    {
+        return m_pInternal->GetNodeType();
+    }
+    bool CXmlLiteReader::IsDefaultAttribute()
+    {
+        return m_pInternal->IsDefaultAttribute();
+    }
 }

DesktopEditor/xml/src/xmllight_private.h
@@ -36,6 +36,7 @@
 
 #include "../libxml2/libxml.h"
 #include "../libxml2/include/libxml/xmlreader.h"
+#include "../include/xmlutils.h"
 
 namespace XmlUtils
 {
@@ -427,6 +428,25 @@ namespace XmlUtils
             if (!IsValid())
                 return false;
             return 1 == xmlTextReaderIsEmptyElement(reader) ? true : false;
+        }
+
+        std::wstring GetNamespacePrefix()
+        {
+            xmlChar* pName = xmlTextReaderPrefix(reader);
+            if (NULL == pName)
+                return L"";
+
+            std::wstring sTemp = NSFile::CUtf8Converter::GetUnicodeStringFromUTF8((BYTE*)pName, (LONG)strlen((const char*)pName));
+            free(pName);
+            return sTemp;
+        }
+        XmlNodeType GetNodeType()
+        {
+            return (XmlUtils::XmlNodeType)xmlTextReaderNodeType(reader);
+        }
+        bool IsDefaultAttribute()
+        {
+            return (0 != xmlTextReaderIsDefault(reader)) ? true : false;
         }
 
     private:

commit 6588a3fa0a257670d97218e5c4e3f5ebf2ff536f

Fix possible typo
`realink -n` return empty string
Introduced in
https://github.com/ONLYOFFICE/core/commit/51569b626fc38c66d4dbc98c60d8f21d88d2332b
Seems have nothing to do with mac, since all other file has `readlink
-f`

Common/3dParty/cef/fetch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT=$(readlink -n "$0" || grealpath "$0")
+SCRIPT=$(readlink -f "$0" || grealpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
 os=$(uname -s)
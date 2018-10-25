e436c13fcc488a5f6764cca04885e0998ced7829
Sergey Konovalov <Sergey.Konovalov@onlyoffice.com>
2018-10-25 17:37:15 +0300

    [x2t] Fix bug 39456

--- ASCOfficePPTXFile/PPTXFormat/Logic/Effects/FillOverlay.h
+++ ASCOfficePPTXFile/PPTXFormat/Logic/Effects/FillOverlay.h
@@ -126,6 +126,8 @@ namespace PPTX
 			}
 			virtual void fromPPTY(NSBinPptxRW::CBinaryFileReader* pReader)
 			{
+				pReader->Skip(4); // len
+				BYTE _type = pReader->GetUChar();
 				LONG _end_rec = pReader->GetPos() + pReader->GetLong() + 4;
 
 				pReader->Skip(1); // start attributes

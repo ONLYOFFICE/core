792a0b35e1e31de89ff927b5c36fdb0f0d2d5fca
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2019-01-17 17:03:31 +0300

    [copyright] For bug 39644
    Change address Latvian office.

--- ASCOfficeOdfFileW/source/OdfFormat/calcext_elements.cpp
+++ ASCOfficeOdfFileW/source/OdfFormat/calcext_elements.cpp
@@ -12,8 +12,8 @@
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For
  * details, see the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
  *
- * You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia,
- * EU, LV-1021.
+ * You can contact Ascensio System SIA at 20A-12 Ernesta Birznieka-Upisha
+ * street, Riga, Latvia, EU, LV-1050.
  *
  * The  interactive user interfaces in modified source and object code versions
  * of the Program must display Appropriate Legal Notices, as required under

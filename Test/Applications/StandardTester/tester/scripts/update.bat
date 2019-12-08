ca348a9ea9e4c5f0dd5aedae1273c6f3ad38c030
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2019-12-08 13:50:25 +0300

    Fix update script

--- Test/Applications/StandardTester/tester/scripts/update.bat
+++ Test/Applications/StandardTester/tester/scripts/update.bat
@@ -6,9 +6,9 @@ RMDIR "%RUN_DIR%\sdkjs" /S /Q
 
 xcopy /s/e/k/c/y/q "%GIT_DIR%\sdkjs\deploy\sdkjs" "%RUN_DIR%\sdkjs\"
 md %RUN_DIR%\sdkjs\vendor\jquery
-copy "%GIT_DIR%\web-apps-pro\vendor\jquery\jquery.min.js"       "%RUN_DIR%\sdkjs\vendor\jquery\jquery.min.js"
+copy "%GIT_DIR%\web-apps\vendor\jquery\jquery.min.js"       "%RUN_DIR%\sdkjs\vendor\jquery\jquery.min.js"
 md %RUN_DIR%\sdkjs\vendor\xregexp
-copy "%GIT_DIR%\web-apps-pro\vendor\xregexp\xregexp-all-min.js" "%RUN_DIR%\sdkjs\vendor\xregexp\xregexp-all-min.js"
+copy "%GIT_DIR%\web-apps\vendor\xregexp\xregexp-all-min.js" "%RUN_DIR%\sdkjs\vendor\xregexp\xregexp-all-min.js"
 
 del /s /q "%RUN_DIR%\sdkjs\*.cache"
 

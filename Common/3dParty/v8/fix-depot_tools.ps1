a7c868d3d1296d007aeba128223b48e61a385352
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2020-03-18 09:55:30 +0300

    Remove unused files

--- Common/3dParty/v8/fix-depot_tools.ps1
+++
@@ -1,4 +0,0 @@
-$gclient_path = "depot_tools\gclient.bat"
-$fetch_path = "depot_tools\fetch.bat"
-(Get-Content $gclient_path) | ForEach-Object { $_ -replace "^(`"%~dp0python`")", "call $&" } | Set-Content $gclient_path
-(Get-Content $fetch_path) | ForEach-Object { $_ -replace "^(%~dp0python)", "call $&" } | Set-Content $fetch_path

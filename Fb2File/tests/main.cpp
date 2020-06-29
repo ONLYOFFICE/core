e8920f3cedf6524d373920fcbbf70c23e734b399
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2020-06-29 16:49:17 +0300

    Add test for xml education

---
+++ Fb2File/tests/main.cpp
@@ -0,0 +1,24 @@
+﻿#include "../../DesktopEditor/xml/include/xmlutils.h"
+
+int main(int argc, char *argv[])
+{
+    // dom - model test ---------------------------------------
+    XmlUtils::CXmlNode oNode;
+    oNode.FromXmlFile(L"pathToXmlFile");
+
+    // просто посмотри, как читается хмл. повызывай методы. прочитай вложенную ноду, ее атрибуты
+
+    // примеры методов
+    oNode.GetNode(L"rootNodeName");
+    oNode.GetAttribute(L"attributeName");
+
+    // sax reader ---------------------------------------------
+    XmlUtils::CXmlLiteReader oLightReader;
+
+    // просто посмотри, как читается хмл. повызывай методы. прочитай вложенную ноду, ее атрибуты
+    // примеры методов
+    oLightReader.ReadNextNode();
+
+
+    return 0;
+}

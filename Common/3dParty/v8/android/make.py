a098837f71e5a4da381423e8d63293f160f219fd
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2020-08-03 19:59:59 +0300

    Add path to 7z

--- Common/3dParty/v8/android/make.py
+++ Common/3dParty/v8/android/make.py
@@ -11,4 +11,5 @@ if ("mac" == base.host_platform()):
   if not base.is_file("./build.zip"):
     base.bash("./run_docker")
   if not base.is_dir("./build"):
+    base.configure_common_apps("./../../../../../build_tools/scripts/base.py")
     base.extract("./build.zip", "./build")

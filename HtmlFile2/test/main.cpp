c6e465d595e1a90ac8fa26bdc630d8f8fecdc36a
Kulikova Svetlana <Svetlana.Kulikova@onlyoffice.com>
2020-08-14 14:18:45 +0300

    refactoring

--- HtmlFile2/test/main.cpp
+++ HtmlFile2/test/main.cpp
@@ -5,50 +5,104 @@
 #include "../../DesktopEditor/common/Directory.h"
 #include "../../OfficeUtils/src/OfficeUtils.h"
 
+void getDirectories(std::wstring sDirectory, std::vector<std::wstring>& arrDirectory)
+{
+    arrDirectory.push_back(sDirectory);
+    for(std::wstring sT : NSDirectory::GetDirectories(sDirectory))
+        getDirectories(sT, arrDirectory);
+}
+
 int main()
 {
+    setlocale(LC_ALL, "Russian");
     bool bBatchMode = false;
-    CHtmlFile2 oFile;
-
-    // Директория, где будем создавать docx
-    std::wstring sOutputDirectory = NSFile::GetProcessDirectory() + L"/res";
-    NSDirectory::DeleteDirectory(sOutputDirectory);
-    NSDirectory::CreateDirectory(sOutputDirectory);
-
-    oFile.SetTmpDirectory(sOutputDirectory);
-    HRESULT nResConvert = S_FALSE;
-
-    CHtmlParams oParams;
-    oParams.SetAuthors(L"last first middle;last2 first2 middle2");
-    oParams.SetGenres(L"fantazy, drama");
-    oParams.SetTitle(L"The Last Wish");
-    oParams.SetDate(L"2010-06-03T04:00:00+00:00");
-    oParams.SetDescription(L"Description");
-
     if(bBatchMode)
     {
-        std::vector<std::wstring> arrFiles{NSFile::GetProcessDirectory() + L"/../../../examples/test1.html",
-                                           NSFile::GetProcessDirectory() + L"/../../../examples/test2.xhtml",
-                                           NSFile::GetProcessDirectory() + L"/../../../examples/test3.xhtml",
-                                           NSFile::GetProcessDirectory() + L"/../../../examples/test4.xhtml"};
-        nResConvert = oFile.OpenBatch(arrFiles, sOutputDirectory, &oParams);
+        // Директория файлов
+        // std::wstring sDirectory = NSFile::GetProcessDirectory() + L"/../../../examples/fb2";
+        std::wstring sDirectory = L"C:/Users/Svetlana.Kulikova.AVSMEDIA/Downloads/html";
+        // Вложенные директории
+        std::vector<std::wstring> arrDirectory;
+        getDirectories(sDirectory, arrDirectory);
+        COfficeUtils oZip;
+        // Выставляем временную директорию
+        std::wstring sTmp = NSFile::GetProcessDirectory() + L"/tmp";
+        NSDirectory::DeleteDirectory(sTmp);
+        NSDirectory::CreateDirectory(sTmp);
+
+        int nErrorCol = 0;
+        std::vector<std::wstring> arrError;
+
+        for(std::wstring sD : arrDirectory)
+        {
+            std::vector<std::wstring> arrFiles = NSDirectory::GetFiles(sD);
+
+            // Директория, где будем создавать docx
+            size_t nPos = sD.find(L"/html");
+            std::wstring sOutputDirectory = sD.insert(nPos + 4, L"-res");
+            NSDirectory::CreateDirectory(sOutputDirectory);
+
+            for(std::wstring sFile : arrFiles)
+            {
+                CHtmlFile2 oFile;
+                oFile.SetTmpDirectory(sTmp);
+                std::wstring sFileName = NSFile::GetFileName(sFile);
+                std::wcout << sFileName << std::endl;
+                if(!oFile.IsHtmlFile(sFile))
+                {
+                    nErrorCol++;
+                    arrError.push_back(sFileName);
+                    std::cout << "This isn't a html file" << std::endl;
+                    continue;
+                }
+
+                HRESULT nResConvert = oFile.Open(sFile, sTmp);
+                if(nResConvert == S_OK)
+                {
+                    std::cout << "Success" << std::endl;
+                    oZip.CompressFileOrDirectory(sTmp, sOutputDirectory + L"/" + sFileName + L".docx");
+                    NSDirectory::DeleteDirectory(sTmp + L"/word/media");
+                }
+                else
+                {
+                    nErrorCol++;
+                    arrError.push_back(sFileName);
+                    std::cout << "Failure" << std::endl;
+                }
+            }
+        }
     }
     else
     {
+        HRESULT nResConvert = S_FALSE;
+        // Директория, где будем создавать docx
+        std::wstring sOutputDirectory = NSFile::GetProcessDirectory() + L"/res";
+        NSDirectory::DeleteDirectory(sOutputDirectory);
+        NSDirectory::CreateDirectory(sOutputDirectory);
+
+        CHtmlFile2 oFile;
+        oFile.SetTmpDirectory(sOutputDirectory);
+
+        CHtmlParams oParams;
+        oParams.SetAuthors(L"last first middle;last2 first2 middle2");
+        oParams.SetGenres(L"fantazy, drama");
+        oParams.SetTitle(L"The Last Wish");
+        oParams.SetDate(L"2010-06-03T04:00:00+00:00");
+        oParams.SetDescription(L"Description");
+
         // Файл, который открываем
-        std::wstring sFile = NSFile::GetProcessDirectory() + L"/../../../examples/test4.xhtml";
+        std::wstring sFile = NSFile::GetProcessDirectory() + L"/../../../examples/test.html";
         nResConvert = oFile.Open(sFile, sOutputDirectory, &oParams);
 
+        if(nResConvert == S_OK)
+        {
+            std::cout << "Success" << std::endl;
+            COfficeUtils oZip;
+            oZip.CompressFileOrDirectory(sOutputDirectory, sOutputDirectory + L"/Aggregate.docx");
+        }
+        else
+            std::cout << "Failure" << std::endl;
     }
-    if(nResConvert == S_OK)
-    {
-        std::cout << "Success" << std::endl;
-        COfficeUtils oZip;
-        oZip.CompressFileOrDirectory(sOutputDirectory, sOutputDirectory + L"/Aggregate.docx");
-    }
-    else
-        std::cout << "Failure" << std::endl;
-
     std::cout << "THE END" << std::endl;
     return 0;
 }

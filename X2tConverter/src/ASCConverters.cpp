3219051e93703aea71015f4295e0353b1162ea06
Teetoow <titou.github@ap2c.com>
2020-08-25 13:49:00 +0200

    Add FreeBSD to the preproc conditions where Linux is also set. Correct syntax error in the code which leads to an error with clang compiler: returning NULL (void*) to a variable which is unsigned int

--- X2tConverter/src/ASCConverters.cpp
+++ X2tConverter/src/ASCConverters.cpp
@@ -4044,7 +4044,7 @@ namespace NExtractTools
 	{
 		bool bMacros = true;
 		
-		_UINT32 nRes = ConvertXls2Xlsx( sFrom, sTo, params.getPassword(), params.getFontPath(), sTemp, NULL, bMacros);
+		_UINT32 nRes = ConvertXls2Xlsx( sFrom, sTo, params.getPassword(), params.getFontPath(), sTemp, 0, bMacros);
 
 		nRes = processEncryptionError(nRes, sFrom, params);
 		return nRes;
@@ -4077,7 +4077,7 @@ namespace NExtractTools
         NSDirectory::CreateDirectory(sResultXlsxDir);
 
 		bool bMacros = true;
-		_UINT32 nRes = ConvertXls2Xlsx( sFrom, sResultXlsxDir, params.getPassword(), params.getFontPath(), sTemp, NULL, bMacros);
+		_UINT32 nRes = ConvertXls2Xlsx( sFrom, sResultXlsxDir, params.getPassword(), params.getFontPath(), sTemp, 0, bMacros);
 
 		nRes = processEncryptionError(nRes, sFrom, params);
 		if (SUCCEEDED_X2T(nRes))

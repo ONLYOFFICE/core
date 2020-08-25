2ea350d88dcd96ec329d63fef397be30edad4ec8
Teetoow <titou.github@ap2c.com>
2020-08-25 13:45:32 +0200

    Add FreeBSD to the preproc conditions where Linux is also set. Correct a syntax error in the code which leads to an error during compilation with clang. Add _gcvt function which does not exists under FreeBSD.

--- Common/DocxFormat/Source/DocxFormat/Docx.cpp
+++ Common/DocxFormat/Source/DocxFormat/Docx.cpp
@@ -67,7 +67,7 @@ namespace OOX {
 	void CDocx::FixAfterRead()
 	{
 		//solve id conflict between comments and documentComments
-		if(NULL != m_pComments && m_pComments->m_arrComments.size() > 0 && NULL != m_pDocumentComments && NULL != m_pDocumentComments->m_arrComments.size() > 0)
+		if(NULL != m_pComments && m_pComments->m_arrComments.size() > 0 && NULL != m_pDocumentComments && m_pDocumentComments->m_arrComments.size() > 0)
 		{
 			int maxId = INT_MIN;
 			for (size_t i = 0; i < m_pComments->m_arrComments.size(); ++i)

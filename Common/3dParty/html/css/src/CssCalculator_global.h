a462e4ee5e086c5f6717c5872191e4d050a81823
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2020-09-04 11:54:39 +0300

    Remove css exports

--- Common/3dParty/html/css/src/CssCalculator_global.h
+++ Common/3dParty/html/css/src/CssCalculator_global.h
@@ -9,10 +9,14 @@
 #  define Q_DECL_IMPORT     __attribute__((visibility("default")))
 #endif
 
+#ifndef CSSCALCULATOR_LIBRARY_STATIC
 #if defined(CSSCALCULATOR_LIBRARY)
 #  define CSSCALCULATOR_EXPORT Q_DECL_EXPORT
 #else
 #  define CSSCALCULATOR_EXPORT Q_DECL_IMPORT
+#endif
+#else
+#  define CSSCALCULATOR_EXPORT
 #endif
 
 #endif // CSSCALCULATOR_GLOBAL_H

faa6786712b83249af94c1f5aa76b45c8cabcf25
oleg.korshul@onlyoffice.com <oleg.korshul@onlyoffice.com>
2020-09-04 10:27:46 +0300

    Fix katana fetching

--- Common/3dParty/html/fetch.py
+++ Common/3dParty/html/fetch.py
@@ -7,8 +7,16 @@ import base
 import os
 import build
 
+base_directory = os.getcwd()
+
 if not base.is_dir("gumbo-parser"):
   base.cmd("git", ["clone", "https://github.com/google/gumbo-parser.git"])
 
 if not base.is_dir("katana-parser"):
   base.cmd("git", ["clone", "https://github.com/hackers-painters/katana-parser.git"])
+
+  # fix katana
+  base.replaceInFile(base_directory + "/katana-parser/src/tokenizer.c", "static inline bool katana_is_html_space(char c);", "static inline bool2 katana_is_html_space(char c);")
+  base.replaceInFile(base_directory + "/katana-parser/src/tokenizer.c", "inline bool katana_is_html_space(char c)", "static inline bool katana_is_html_space(char c)")
+  base.replaceInFile(base_directory + "/katana-parser/src/tokenizer.c", "static inline bool2 katana_is_html_space(char c);", "static inline bool katana_is_html_space(char c);")
+

4d3718e1ffab32cc8e77fe955fd5b6c5f063cfab
ElenaSubbotina <Elena.Subbotina@onlyoffice.com>
2020-10-05 11:17:31 +0300

    Revert "TestMetafile - vc2015"
    This reverts commit e26946a57925bd6336044d6e2d77f150a979fbf8.

--- DesktopEditor/raster/Metafile/stdafx.h
+++ DesktopEditor/raster/Metafile/stdafx.h
@@ -30,3 +30,11 @@
  *
  */
 #pragma once
+
+#ifdef _DEBUG
+#pragma comment(lib, "../../../SDK/lib/win_64/DEBUG/graphics.lib")
+#pragma comment(lib, "../../../SDK/lib/win_64/DEBUG/ASCOfficeUtilsLib.lib")
+#else
+#pragma comment(lib, "../../../SDK/lib/win_64/graphics.lib")
+#pragma comment(lib, "../../../SDK/lib/win_64/ASCOfficeUtilsLib.lib")
+#endif

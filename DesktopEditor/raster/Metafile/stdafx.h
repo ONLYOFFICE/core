e26946a57925bd6336044d6e2d77f150a979fbf8
ElenaSubbotina <Elena.Subbotina@onlyoffice.com>
2020-10-05 11:15:11 +0300

    TestMetafile - vc2015

--- DesktopEditor/raster/Metafile/stdafx.h
+++ DesktopEditor/raster/Metafile/stdafx.h
@@ -30,11 +30,3 @@
  *
  */
 #pragma once
-
-#ifdef _DEBUG
-#pragma comment(lib, "../../../SDK/lib/win_64/DEBUG/graphics.lib")
-#pragma comment(lib, "../../../SDK/lib/win_64/DEBUG/ASCOfficeUtilsLib.lib")
-#else
-#pragma comment(lib, "../../../SDK/lib/win_64/graphics.lib")
-#pragma comment(lib, "../../../SDK/lib/win_64/ASCOfficeUtilsLib.lib")
-#endif

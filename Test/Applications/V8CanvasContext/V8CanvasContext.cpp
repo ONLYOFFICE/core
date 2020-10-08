71fd5b74f6951fab1d00858240a2d3d7838868cc
Kulikova Svetlana <Svetlana.Kulikova@onlyoffice.com>
2020-10-08 14:16:25 +0300

    property set

--- Test/Applications/V8CanvasContext/V8CanvasContext.cpp
+++ Test/Applications/V8CanvasContext/V8CanvasContext.cpp
@@ -50,10 +50,17 @@ void SetName(const v8::FunctionCallbackInfo<v8::Value>& args)
     pUser->SetName(to_string(args[0]));
 }
 
-void name(v8::Local<v8::String> _name, const v8::PropertyCallbackInfo<v8::Value>& info)
+void nameget(v8::Local<v8::String> _name, const v8::PropertyCallbackInfo<v8::Value>& info)
 {
     CUser* pUser = unwrap_User(info.Holder());
     info.GetReturnValue().Set(v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), pUser->SayHi().data()));
+}
+
+void nameset(v8::Local<v8::String> _name, v8::Local<v8::Value> value, const v8::PropertyCallbackInfo<void>& info)
+{
+    CUser* pUser = unwrap_User(info.Holder());
+    pUser->SetName(to_string(value));
+    info.GetReturnValue().Set(value);
 }
 
 v8::Handle<v8::ObjectTemplate> CreateUserTemplate(v8::Isolate* isolate)
@@ -65,7 +72,7 @@ v8::Handle<v8::ObjectTemplate> CreateUserTemplate(v8::Isolate* isolate)
     v8::Isolate* current = v8::Isolate::GetCurrent();
 
     // свойства
-    result->SetAccessor(v8::String::NewFromUtf8(current, "name"), name);
+    result->SetAccessor(v8::String::NewFromUtf8(current, "name"), nameget, nameset);
 
     // методы
     result->Set(current, "SayHi", v8::FunctionTemplate::New(current, SayHi));

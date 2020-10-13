a4b163889be8d08a221ebd6be0587f3b2a2b46e7
Kulikova Svetlana <Svetlana.Kulikova@onlyoffice.com>
2020-10-13 11:33:23 +0300

    update fetch.py

--- Common/3dParty/html/fetch.py
+++ Common/3dParty/html/fetch.py
@@ -11,9 +11,11 @@ base_directory = os.getcwd()
 
 if not base.is_dir("gumbo-parser"):
   base.cmd("git", ["clone", "https://github.com/google/gumbo-parser.git"])
+  base.cmd_in_dir("gumbo-parser", "git", ["checkout", "aa91b27b02c0c80c482e24348a457ed7c3c088e0"])
 
 if not base.is_dir("katana-parser"):
   base.cmd("git", ["clone", "https://github.com/hackers-painters/katana-parser.git"])
+  base.cmd_in_dir("katana-parser", "git", ["checkout", "499118d32c387a893fdc9dda2cb95eee524bdb9b"])
 
   # fix katana
   base.replaceInFile(base_directory + "/katana-parser/src/tokenizer.c", "static inline bool katana_is_html_space(char c);", "static inline bool2 katana_is_html_space(char c);")

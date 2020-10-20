dc2236c6640abe1d9fed0d7acfd4385d54e16801
maxim-polyakov <maxim7012@gmail.com>
2020-10-20 14:59:56 +0300

    _

--- Test/Applications/ParseExcelNumberFormatLocales/test/lcids.ps1
+++ Test/Applications/ParseExcelNumberFormatLocales/test/lcids.ps1
@@ -9,6 +9,6 @@ foreach($culture in $CultureInfos) {
  $p1 = $culture.Name
 $p2 = $culture.NumberFormat.NumberDecimalSeparator
 $p3 = $culture.NumberFormat.NumberGroupSeparator
-Set-Content "C:\Users\maxim\Downloads\Telegram Desktop\DecSep\$p1.txt" $p2
-Set-Content -Path "C:\Users\maxim\Downloads\Telegram Desktop\GrSep\$p1.txt" -Value "$p3" -Encoding "utf8"
+Set-Content  $pathMacro = $(pwd).Path + "\$p2.txt" $p2
+Set-Content -Path $pathoutput = $(pwd).Path + "\$p1.txt" -Value "$p3" -Encoding "utf8"
 }

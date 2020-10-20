112e2a866aad0e49ebcb21c0d08f7abad8ef404d
maxim-polyakov <maxim7012@gmail.com>
2020-10-20 14:55:39 +0300

    fix

--- Test/Applications/ParseExcelNumberFormatLocales/test/test2.ps1
+++ Test/Applications/ParseExcelNumberFormatLocales/test/test2.ps1
@@ -6,7 +6,7 @@ echo Set-Culture $i
  Set-Culture $i
   Start-Sleep -Seconds 1
 
-  $pathMacro = $(pwd).Path + "\macros-locale (1).xlsm"
+  $pathMacro = $(pwd).Path + "\macros-locale-data.xlsm"
   $pathOutput = $(pwd).Path + "\" + $i + ".txt"
  echo .\run2.vbs $i
  

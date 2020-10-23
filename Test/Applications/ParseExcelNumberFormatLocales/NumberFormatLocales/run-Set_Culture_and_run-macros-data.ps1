40ef42f18d4214cb800b313ae2d1e285f87d6d15
maxim-polyakov <maxim7012@gmail.com>
2020-10-23 13:43:20 +0300

    [se] view

--- Test/Applications/ParseExcelNumberFormatLocales/NumberFormatLocales/run-Set_Culture_and_run-macros-data.ps1
+++ Test/Applications/ParseExcelNumberFormatLocales/NumberFormatLocales/run-Set_Culture_and_run-macros-data.ps1
@@ -1,17 +1,17 @@
 $Lcids = ([System.Globalization.CultureInfo]::GetCultures( [System.Globalization.CultureTypes]::AllCultures ) | Select-Object -ExpandProperty Name)
 
- foreach($i in $Lcids) {
+foreach($i in $Lcids) {
 
-echo Set-Culture $i
- Set-Culture $i
-  Start-Sleep -Seconds 1
+	echo Set-Culture $i
+	Set-Culture $i
+	Start-Sleep -Seconds 1
 
-  $pathMacro = $(pwd).Path + "\macros-locale-data.xlsm"
-  $pathOutput = $(pwd).Path + "\" + $i + ".txt"
- echo .\run-macros-data.vbs $i
+	$pathMacro = $(pwd).Path + "\macros-locale-data.xlsm"
+	$pathOutput = $(pwd).Path + "\" + $i + ".txt"
+	echo .\run-macros-data.vbs $i
  
-  .\run-macros-data.vbs $i
-  Start-Sleep -Seconds 7
+	.\run-macros-data.vbs $i
+	Start-Sleep -Seconds 7
  
 }
 

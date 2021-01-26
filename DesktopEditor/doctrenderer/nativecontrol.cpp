183a9219ee9c58707acaca0cc72192b7ecba784a
sankayop <23059127+sankayop@users.noreply.github.com>
2021-01-26 12:19:01 +0100

    [FIX] Replaced depracated v8::String::kNormalString instances

--- DesktopEditor/doctrenderer/nativecontrol.cpp
+++ DesktopEditor/doctrenderer/nativecontrol.cpp
@@ -133,7 +133,8 @@ void _GetFilePath(const v8::FunctionCallbackInfo<v8::Value>& args)
 {
     CNativeControl* pNative = unwrap_nativeobject(args.This());
     std::string sReturn = NSFile::CUtf8Converter::GetUtf8StringFromUnicode(pNative->GetFilePath());
-    args.GetReturnValue().Set(v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), sReturn.c_str()));
+    v8::MaybeLocal<v8::String> stringOut = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(),sReturn.c_str());
+    args.GetReturnValue().Set(CV8Convert::unwrap<v8::String>(stringOut));
 }
 void _SetFilePath(const v8::FunctionCallbackInfo<v8::Value>& args)
 {
@@ -149,21 +150,24 @@ void _GetImagesPath(const v8::FunctionCallbackInfo<v8::Value>& args)
 {
     CNativeControl* pNative = unwrap_nativeobject(args.This());
     std::string sReturn = NSFile::CUtf8Converter::GetUtf8StringFromUnicode(pNative->m_strImagesDirectory);
-    args.GetReturnValue().Set(v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), sReturn.c_str()));
+    v8::MaybeLocal<v8::String> stringOut = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(),sReturn.c_str());
+    args.GetReturnValue().Set(CV8Convert::unwrap<v8::String>(stringOut));
 }
 
 void _GetFontsDirectory(const v8::FunctionCallbackInfo<v8::Value>& args)
 {
     CNativeControl* pNative = unwrap_nativeobject(args.This());
     std::string sReturn = NSFile::CUtf8Converter::GetUtf8StringFromUnicode(pNative->m_strFontsDirectory);
-    args.GetReturnValue().Set(v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), sReturn.c_str()));
+    v8::MaybeLocal<v8::String> stringOut = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(),sReturn.c_str());
+    args.GetReturnValue().Set(CV8Convert::unwrap<v8::String>(stringOut));
 }
 
 void _GetEditorType(const v8::FunctionCallbackInfo<v8::Value>& args)
 {
     CNativeControl* pNative = unwrap_nativeobject(args.This());
     std::string sReturn = NSFile::CUtf8Converter::GetUtf8StringFromUnicode(pNative->m_strEditorType);
-    args.GetReturnValue().Set(v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), sReturn.c_str()));
+    v8::MaybeLocal<v8::String> stringOut = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(),sReturn.c_str());
+    args.GetReturnValue().Set(CV8Convert::unwrap<v8::String>(stringOut));
 }
 
 void _GetChangesCount(const v8::FunctionCallbackInfo<v8::Value>& args)
@@ -188,14 +192,16 @@ void _GetChangesFile(const v8::FunctionCallbackInfo<v8::Value>& args)
     if (pNative->m_pChanges != NULL)
         strFile = NSFile::CUtf8Converter::GetUtf8StringFromUnicode(pNative->m_pChanges->operator []((int)nIndex));
 
-    args.GetReturnValue().Set(v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), strFile.c_str()));
+    v8::MaybeLocal<v8::String> stringOut = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(),strFile.c_str());
+    args.GetReturnValue().Set(CV8Convert::unwrap<v8::String>(stringOut));
 }
 
 void _GetFileId(const v8::FunctionCallbackInfo<v8::Value>& args)
 {
     CNativeControl* pNative = unwrap_nativeobject(args.This());
     std::string sReturn = NSFile::CUtf8Converter::GetUtf8StringFromUnicode(pNative->GetFileId());
-    args.GetReturnValue().Set(v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), sReturn.c_str()));
+    v8::MaybeLocal<v8::String> stringOut = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(),sReturn.c_str());
+    args.GetReturnValue().Set(CV8Convert::unwrap<v8::String>(stringOut));
 }
 void _SetFileId(const v8::FunctionCallbackInfo<v8::Value>& args)
 {
@@ -313,7 +319,8 @@ void _GetFileString(const v8::FunctionCallbackInfo<v8::Value>& args)
     DWORD len = 0;
     pNative->getFileData(CV8Convert::ToString(args[0]), pData, len);
 
-    args.GetReturnValue().Set(v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), (char*)pData, v8::String::kNormalString, len));
+    v8::MaybeLocal<v8::String> stringOut = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), (char*)pData, v8::NewStringType::kNormal, len);
+    args.GetReturnValue().Set(CV8Convert::unwrap<v8::String>(stringOut));
 }
 
 void _Save_AllocNative(const v8::FunctionCallbackInfo<v8::Value>& args)
@@ -404,10 +411,10 @@ void _zipOpenFile(const v8::FunctionCallbackInfo<v8::Value>& args)
     {
         std::string sFile = NSFile::CUtf8Converter::GetUtf8StringFromUnicode(*i);
 
-        v8::Local<v8::String> _k = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), sFile.c_str(), v8::String::kNormalString, -1);
-        v8::Local<v8::String> _v = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), sFile.c_str(), v8::String::kNormalString, -1);
+        v8::MaybeLocal<v8::String> _maybe_k = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), sFile.c_str(), v8::NewStringType::kNormal, -1);
+        v8::MaybeLocal<v8::String> _maybe_v = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), sFile.c_str(), v8::NewStringType::kNormal, -1);
 
-        obj->Set(_k, _v);
+        obj->Set(v8::Isolate::GetCurrent()->GetCurrentContext(), CV8Convert::unwrap<v8::String>(_maybe_k), CV8Convert::unwrap<v8::String>(_maybe_v));
     }
 
     args.GetReturnValue().Set(obj);
@@ -434,10 +441,10 @@ void _zipOpenFileBase64(const v8::FunctionCallbackInfo<v8::Value>& args)
     {
         std::string sFile = NSFile::CUtf8Converter::GetUtf8StringFromUnicode(*i);
 
-        v8::Local<v8::String> _k = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), sFile.c_str(), v8::String::kNormalString, -1);
-        v8::Local<v8::String> _v = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), sFile.c_str(), v8::String::kNormalString, -1);
+        v8::MaybeLocal<v8::String> _maybe_k = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), sFile.c_str(), v8::NewStringType::kNormal, -1);
+        v8::MaybeLocal<v8::String> _maybe_v = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), sFile.c_str(), v8::NewStringType::kNormal, -1);
 
-        obj->Set(_k, _v);
+        obj->Set(v8::Isolate::GetCurrent()->GetCurrentContext(), CV8Convert::unwrap<v8::String>(_maybe_k), CV8Convert::unwrap<v8::String>(_maybe_v));
     }
 
     args.GetReturnValue().Set(obj);
@@ -457,7 +464,8 @@ void _zipGetFileAsString(const v8::FunctionCallbackInfo<v8::Value>& args)
     DWORD len = 0;
     pNative->m_oZipWorker.GetFileData(CV8Convert::ToString(args[0]), pData, len);
 
-    args.GetReturnValue().Set(v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), (char*)pData, v8::String::kNormalString, len));
+    v8::MaybeLocal<v8::String> stringOut = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), (char*)pData, v8::NewStringType::kNormal, len);
+    args.GetReturnValue().Set(CV8Convert::unwrap<v8::String>(stringOut));
 }
 
 void _zipGetFileAsBinary(const v8::FunctionCallbackInfo<v8::Value>& args)
@@ -517,7 +525,8 @@ void _GetImageUrl(const v8::FunctionCallbackInfo<v8::Value>& args)
     std::wstring sRet = pNative->m_pWorker->GetImage(sUrl);
     std::string sRetA = U_TO_UTF8(sRet);
 
-    args.GetReturnValue().Set(v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), (char*)sRetA.c_str(), v8::String::kNormalString, (int)sRetA.length()));
+    v8::MaybeLocal<v8::String> stringOut = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), (char*)sRetA.c_str(), v8::NewStringType::kNormal, (int)sRetA.length());
+    args.GetReturnValue().Set(CV8Convert::unwrap<v8::String>(stringOut));
 }
 
 v8::Handle<v8::ObjectTemplate> CreateNativeControlTemplate(v8::Isolate* isolate)

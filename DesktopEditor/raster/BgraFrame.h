01bf276697e02230d788eeb0e34f615c3338abb4
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2021-02-02 21:23:02 +0300

    Fix bug with hatch brush (support saving RGBA data in CBgraFrame)

--- DesktopEditor/raster/BgraFrame.h
+++ DesktopEditor/raster/BgraFrame.h
@@ -55,6 +55,8 @@ private:
     bool m_bIsGrayScale;
     double m_dJpegSaveQuality;
 
+    bool m_bIsRGBA;
+
 public:
     CBgraFrame();
     ~CBgraFrame();
@@ -80,10 +82,13 @@ public:
 
     bool IsGrayScale();
 
+    void put_IsRGBA(const bool& bIsRGBA);
+    bool get_IsRGBA();
+
     void SetJpegQuality(const double& value);
 
 	bool OpenFile(const std::wstring& strFileName, unsigned int nFileType = 0);	//0 - detect
-	bool SaveFile(const std::wstring& strFileName, unsigned int nFileType);
+    bool SaveFile(const std::wstring& strFileName, unsigned int nFileType);
     bool Encode(BYTE*& pBuffer, int& nSize, unsigned int nFileType);
     
 	bool Resize(const long& nNewWidth, const long& nNewHeight, bool bDestroyData = true);

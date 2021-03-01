35547e8b8c62d7f5efea6efe0424066406fd3a25
SalnikovDE <danila.salnikov@onlyoffice.com>
2021-03-01 11:40:55 +0300

    Hunpell version file

--- Common/3dParty/hunspell/make.py
+++ Common/3dParty/hunspell/make.py
@@ -3,6 +3,17 @@ sys.path.append('../../../../build_tools/scripts')
 import base
 import os
 
+def get_hunspell(last_stable_commit):
+    repo_path = "https://github.com/hunspell/hunspell.git"
+    base.cmd("git", ["clone", repo_path])
+    os.chdir("hunspell")
+    base.cmd("git", ["checkout", last_stable_commit])
+    base.replaceInFile("./src/hunspell/filemgr.hxx", "FileMgr& operator=(const FileMgr&);", "FileMgr& operator=(const FileMgr&); \n" 
+         +" #ifdef HUNSPELL_WASM_MODULE \n std::istrstream* memin; \n #endif") #custon filemgr support watch filemgr_wrapper_new.cxx
+    base.replaceInFile("./src/hunspell/filemgr.hxx", "#include <fstream>", "#include <fstream> \n #include <strstream>\n")
+    os.chdir("../")
+
+
 base.configure_common_apps()
 
 # remove previous version
@@ -22,18 +33,22 @@ if not base.is_dir("emsdk"):
 
 
 #fetch libhunspell
+HEAD = open("HEAD", "r")
+last_stable_commit = HEAD.read().split('\n')[0] # workaround to delete \n in the end of the line
+HEAD.close()
 
 if not base.is_dir("hunspell"):
-    last_stable_commit = "4ddd8ed5ca6484b930b111aec50c2750a6119a0f"
-    repo_path = "https://github.com/hunspell/hunspell.git"
-    base.cmd("git", ["clone", repo_path])
-    os.chdir("hunspell")
-    base.cmd("git", ["checkout", last_stable_commit])
-    base.replaceInFile("./src/hunspell/filemgr.hxx", "FileMgr& operator=(const FileMgr&);", "FileMgr& operator=(const FileMgr&); \n" 
-         +" #ifdef HUNSPELL_WASM_MODULE \n std::istrstream* memin; \n #endif") #custon filemgr support watch filemgr_wrapper_new.cxx
-    base.replaceInFile("./src/hunspell/filemgr.hxx", "#include <fstream>", "#include <fstream> \n #include <strstream>\n")
-    os.chdir("../")
+    get_hunspell(last_stable_commit)
 
+# version check
+
+git_head = open("hunspell/.git/HEAD", "r")
+current_commit = git_head.read().split('\n')[0]
+git_head.close()
+
+if current_commit != last_stable_commit:
+    base.delete_dir("hunspell")
+    get_hunspell(last_stable_commit)
 
 # compile
 compiler_flags = ["-o spell.js",
--- Common/3dParty/hunspell/wasm/src/filemgr_wrapper_new.cxx
+++ Common/3dParty/hunspell/wasm/src/filemgr_wrapper_new.cxx
@@ -6,46 +6,52 @@
 #include <stdio.h>
 
 #include "./filemgr.hxx"
- #include "csutil.hxx"
+#include "csutil.hxx"
 
- #include "./engine.h"
+#include "./engine.h"
 
-int FileMgr::fail(const char * err, const char * par) {
+int FileMgr::fail(const char *err, const char *par)
+{
     fprintf(stderr, err, par);
     return -1;
 }
 
-FileMgr::FileMgr(const char * file, const char * key)
+FileMgr::FileMgr(const char *file, const char *key)
     : hin(NULL), linenum(0), memin(NULL)
 {
     in[0] = '\0';
 
-    CFileMemory* file_memory = CSpellchecker::Get(file);
+    CFileMemory *file_memory = CSpellchecker::Get(file);
     if (!file_memory)
     {
         fail(MSG_OPEN, file);
     }
     else
     {
-		memin = new std::istrstream((char*)file_memory->data, file_memory->len);
+        memin = new std::istrstream((char *)file_memory->data, file_memory->len);
     }
 }
 
 FileMgr::~FileMgr()
 {
-    if (hin) delete hin;
-	if (memin) delete memin;
+    if (hin)
+        delete hin;
+    if (memin)
+        delete memin;
 }
 
-bool FileMgr::getline(std::string& dest) {
-  ++linenum;
-  bool ret = static_cast<bool>(std::getline(*memin, dest));
-  if (!ret) {
-    --linenum;
-  }
-  return ret;
+bool FileMgr::getline(std::string &dest)
+{
+    ++linenum;
+    bool ret = static_cast<bool>(std::getline(*memin, dest));
+    if (!ret)
+    {
+        --linenum;
+    }
+    return ret;
 }
 
-int FileMgr::getlinenum() {
+int FileMgr::getlinenum()
+{
     return linenum;
 }

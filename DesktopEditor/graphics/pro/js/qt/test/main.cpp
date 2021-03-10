784c7186a8c360f9eb911081d48f4d1dfda3e150
Kulikova Svetlana <Svetlana.Kulikova@onlyoffice.com>
2021-03-10 17:31:11 +0300

    try to understand

--- DesktopEditor/graphics/pro/js/qt/test/main.cpp
+++ DesktopEditor/graphics/pro/js/qt/test/main.cpp
@@ -1,4 +1,29 @@
-int main()
+#include "../../../../../common/Types.h"
+#include "../../../../../common/File.h"
+
+#include <string>
+
+BYTE* JPGToPixels(BYTE* pData, int nWidth, int nHeight)
 {
+    BYTE* pRes = new BYTE[4 * nWidth * nHeight];
+    if (!pRes)
+        return NULL;
+}
+
+int main()
+{    
+    DWORD nBytesCount = 0;
+    BYTE* pData = NULL;
+    NSFile::CFileBinary oFileBinary;
+    if (oFileBinary.OpenFile(L"test.jpg"))
+    {
+        long nFileSize = oFileBinary.GetFileSize();
+        pData = new BYTE[nFileSize];
+        if (oFileBinary.ReadFile(pData, nFileSize, nBytesCount))
+            oFileBinary.CloseFile();
+        else
+            RELEASEARRAYOBJECTS(pData);
+    }
+
     return 0;
 }

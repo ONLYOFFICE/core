e440f92c7e667e9b6767b067701923339fbe3fed
Ivan <ivan.morozov@onlyoffice.com>
2021-04-13 09:54:32 +0000

    fix audio crashes

--- ASCOfficePPTFile/PPTFormatLib/PPTXWriter/Animation.cpp
+++ ASCOfficePPTFile/PPTFormatLib/PPTXWriter/Animation.cpp
@@ -411,9 +411,11 @@ void Animation::FillAudio(CRecordClientVisualElementContainer *pCVEC,
         std::vector<CRecordSoundContainer*> soundCont;
         this->m_pSoundContainer->GetRecordsByType(
                     &soundCont, false);
-        oAudio.cMediaNode.tgtEl.name =
+        unsigned oldId = (pCVEC->m_oVisualShapeAtom.m_nOldIdRef - 1) % soundCont.size();
+        if (!soundCont.empty())
+            oAudio.cMediaNode.tgtEl.name =
                 static_cast<CRecordCString*>
-                (soundCont[pCVEC->m_oVisualShapeAtom.m_nOldIdRef -1]->m_arRecords[0])->m_strText;
+                (soundCont[oldId]->m_arRecords[0])->m_strText;
     }
 }
 

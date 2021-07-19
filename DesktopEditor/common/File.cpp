8cd53bc8a6d23519cb2a13c035b1b6414bd98c18
Teetoow <titou.github@ap2c.com>
2021-07-19 11:46:22 +0200

    This is a commit error: this code has been set twice

--- DesktopEditor/common/File.cpp
+++ DesktopEditor/common/File.cpp
@@ -1638,22 +1638,6 @@ namespace NSFile
         return sRet;
 #endif
 
-#if defined(__FreeBSD__)
-        int mib[4] = {CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, -1};
-        char buf[NS_FILE_MAX_PATH];
-        size_t size = NS_FILE_MAX_PATH;
-
-        memset(buf, 0, NS_FILE_MAX_PATH);
-        if (sysctl(mib, 4, &buf, &size, NULL, 0) != 0) {
-            size = readlink("/proc/curproc/file", buf, size - 1);
-            if (size < 0)
-            return L"";
-        }
-        std::string sUTF8(buf);
-        std::wstring sRet = CUtf8Converter::GetUnicodeStringFromUTF8((BYTE*)sUTF8.c_str(), sUTF8.length());
-        return sRet;
-#endif
-
         return L"";
     }
 

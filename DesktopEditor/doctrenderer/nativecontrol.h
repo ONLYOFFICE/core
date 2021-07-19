39763535807117b0c044fb41ecf11ed8e7e04559
Teetoow <titou.github@ap2c.com>
2021-07-19 11:38:07 +0200

    Bug correction: pData is a char*& argument so this line is a bug. I guess that you wanted to reset the pointer so using "*" is wrong. If you want to reset the data buffer content you might first check that pData is not equal to NULL and then do: "*pData = (char)0;". NULL is equivalent to a pointer (its cast is void*) not a byte nor a char.

--- DesktopEditor/doctrenderer/nativecontrol.h
+++ DesktopEditor/doctrenderer/nativecontrol.h
@@ -327,7 +327,7 @@ public:
 
             if (m_map_access_directories.end() == m_map_access_directories.find(NSFile::GetDirectoryName(strFile)))
             {
-                *pData = NULL;
+                pData = NULL;
                 dwLen = 0;
                 return;
             }

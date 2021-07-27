cab34310bf2a1f01b6fe761fc3894992ddecfa66
Maxim Selyanin <maxim.selyanin@onlyoffice.com>
2021-07-27 23:53:19 +0300

    tested v8-debug - it also works fine

--- DesktopEditor/doctrenderer/js_internal/v8/inspector/main.cpp
+++ DesktopEditor/doctrenderer/js_internal/v8/inspector/main.cpp
@@ -7,21 +7,7 @@ int main(int argc, char *argv[])
     JSSmart<NSJSBase::CJSContext> pJSContext = new NSJSBase::CJSContext();
     pJSContext->Initialize();
 
-//    v8::V8::InitializeICU();
-//    v8::Platform* v8Platform = v8::platform::CreateDefaultPlatform();
-//    v8::V8::InitializePlatform(v8Platform);
-//    v8::V8::Initialize();
-
-//    v8::Isolate::CreateParams create_params;
-//    create_params.array_buffer_allocator =
-//            v8::ArrayBuffer::Allocator::NewDefaultAllocator();
-//    v8::Isolate* isolate = v8::Isolate::New(create_params);
-
     {
-//        v8::Isolate::Scope isolate_scope(isolate);
-//        v8::HandleScope scope(isolate);
-//        v8::Handle<v8::Context> context = v8::Context::New(isolate);
-//        v8::Context::Scope context_scope(context);
         JSSmart<NSJSBase::CJSIsolateScope> isolate_scope = pJSContext->CreateIsolateScope();
         JSSmart<NSJSBase::CJSLocalScope> handle_scope = pJSContext->CreateLocalScope();
 
@@ -32,81 +18,42 @@ int main(int argc, char *argv[])
         JSSmart<NSJSBase::CJSTryCatch> try_catch = pJSContext->GetExceptions();
         JSSmart<NSJSBase::CJSObject> global_js = pJSContext->GetGlobal();
 
-//        NSJSBase::v8_debug::internal::CInspectorImpl i{
-//            context
-////                    , CV8Worker::getInitializer()->getPlatform()
-//                    , v8Platform
-//                    , {true, 1, ""}
-//            , 8080};
-//        i.prepareServer();
-//        std::cout << "server prepared\n";
+        //zero
+        pJSContext->
+                runScript(
+                                "var a = 1;\nvar b = 2;\nvar c = 3;\nvar d = 4;", try_catch
+                            );
+        try_catch->Check();
+        std::cout << "after zero\n";
 
-        //trial shit
-//        v8::Local<v8::Context> context = pJSContext->m_internal->m_context;
-//        v8::Isolate *isolate = context->GetIsolate();
-
-//        i.before();
-//        std::cout << "before called 1\n";
-//        const char *c1 = "function func1() {return 1 + 2;}func1();";
-//        v8::Script::Compile(context
-//                            , v8::String::NewFromOneByte(isolate, (uint8_t*)c1))
-//                .ToLocalChecked()->Run(context).ToLocalChecked();
 
         //one
         pJSContext->
-//        i.
                 runScript(
-//                            {
-                                "function function_1() { return 1 + 2; }function_1();", try_catch, L""
-//                            }
+                                "function function_1() \n { \n return 1 + 2;\n }", try_catch
                             );
         try_catch->Check();
         std::cout << "after one\n";
 
         //two
         pJSContext->
-//        i.
                 runScript(
-//                    {
-                        "function function_2() { return 100 + function_1(); }", try_catch, L""
-//                    }
+                        "function function_2()\n {\n return 100 + function_1(); \n}", try_catch
                     );
         try_catch->Check();
-//        i.before();
-//        std::cout << "before called 2\n";
-//        const char *c2 = "var val = func1();";
-//        v8::Script::Compile(context
-//                            , v8::String::NewFromOneByte(isolate, (uint8_t*)c2))
-//                .ToLocalChecked()->Run(context).ToLocalChecked();
         std::cout << "after two\n";
 
         //three
-
         JSSmart<NSJSBase::CJSValue> js_result =
-
-
-                global_js->call_func("function_2")
-
-
-//                i.callFunc({
-//                               static_cast<NSJSBase::CJSObjectV8*>(global_js.GetPointer())->value
-//                               , "function_2"
-//                               , 0
-//                               , nullptr
-//                           });
+                global_js->call_func("function_1")
                 ;
         std::cout << "after three\n";
 
         //four
-//        i.callFunc({
-//                       static_cast<NSJSBase::CJSObjectV8*>(global_js.GetPointer())->value
-//                       , "function_1"
-//                       , 0
-//                       , nullptr
-//                   });
-        global_js->call_func("function_1");
+        JSSmart<NSJSBase::CJSValue> js_result2 = global_js->call_func("function_2");
         std::cout << "after four\n";
 
+        std::cout << "function_1 = ";
         //check
         if (js_result.IsInit()){
             if (js_result->isNumber()) {
@@ -114,17 +61,23 @@ int main(int argc, char *argv[])
             } else {
                 std::cout << "no result\n";
             }
+        } else {
+            std::cout << "result not init\n";
         }
-        else
-        {
+
+
+        std::cout << "function_2 = ";
+        //check
+        if (js_result.IsInit()){
+            if (js_result2->isNumber()) {
+                std::cout << js_result2->toInt32() << std::endl;
+            } else {
+                std::cout << "no result\n";
+            }
+        } else {
             std::cout << "result not init\n";
         }
     }
-
-//    isolate->Dispose();
-//    v8::V8::Dispose();
-//    v8::V8::ShutdownPlatform();
-//    delete create_params.array_buffer_allocator;
 
     pJSContext->Dispose();
     NSJSBase::CJSContext::ExternalDispose();

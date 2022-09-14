78289248c80ad7f4962ab9efd5a300a2b462c3bb
Elena.Subbotina <Elena.Subbotina@onlyoffice.com>
2022-09-14 10:25:32 +0300

    .

--- ASCOfficePPTFile/PPTFormatLib/PPTXWriter/ShapeWriter.cpp
+++ ASCOfficePPTFile/PPTFormatLib/PPTXWriter/ShapeWriter.cpp
@@ -2054,7 +2054,7 @@ std::wstring PPT_FORMAT::CShapeWriter::ConvertImage()
         if (pImageElement->m_lpictureBrightness != 0)
         {
             int bright = std::round(3.051705 * pImageElement->m_lpictureBrightness); // -100000 - min, backgrpund. 50000 - usually. 100000 - max,white
-            if (bright < 100000 || bright > 100000)
+            if (bright < -100000 || bright > 100000)
                 bright = 50000;
 
             m_oWriter.WriteString(L" bright=\"" + std::to_wstring(bright) + L"\"");

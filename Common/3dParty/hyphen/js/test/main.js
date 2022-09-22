35cbbe2fb1d936e8e789c3a00c9d0c6f2abd38f6
Alexey <aleksei.nagaev@onlyoffice.com>
2022-09-22 21:53:34 +0300

    Add combobox in js test

--- Common/3dParty/hyphen/js/test/main.js
+++ Common/3dParty/hyphen/js/test/main.js
@@ -1,39 +1,38 @@
 (function (window, undefined){
+
     var textarea = document.getElementById("textarea");
     var form = document.querySelector("form");
-
-    var control = document.getElementById("dictionary-file");
+    var combobox = document.getElementById("combobox");
 
     var application = undefined;
-    var lang = undefined;
-
-    control.addEventListener("change", function(event) {
-        var file = control.files[0];
-        lang = file.name;
-        var reader = new FileReader();
-        reader.readAsText(file);
-    
-        if(application == undefined) {
-            application = hyphenCreateApplication(Module);
-        }
-    
-        reader.onload = function() {
-            hyphenLoadDictionary(Module, application, reader.result, lang);
-        };
-     
-    }, false);
     
     form.onsubmit = function(event) {
-        event.preventDefault();
-        var text = textarea.value.split("\n").join(" ").split(" ");
-    
+
+        if(combobox.value == "") {
+            return;
+        }
+
         if(application == undefined) {
             application = hyphenCreateApplication(Module);
         }
-    
-        for(var i = 0; i < text.length; i++) {
-            var hyphens = hyphenWord(Module, application, text[i].toLowerCase(), lang);
-			console.log(hyphens);
+
+        var lang = combobox.value;
+        var text = textarea.value.split("\n").join(" ").split(" ");
+     
+        var request = new XMLHttpRequest();
+        var path = '../../../../../../dictionaries/' + lang + '/' + 'hyph_' + lang  + '.dic';
+        request.open('GET', path, true);
+        request.send(null);
+
+        request.onload = function () {
+            var dict = request.responseText;
+            hyphenLoadDictionary(Module, application, dict, lang);
+
+            for(var i = 0; i < text.length; i++) {
+                var hyphens = hyphenWord(Module, application, text[i].toLowerCase(), lang);
+                console.log(hyphens);
+            }
         }
+        event.preventDefault();
     }
 })(self, undefined);

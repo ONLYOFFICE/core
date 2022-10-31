ed3a8f791610d6c28c3aec91b3ff62560d79eb66
Alexey <aleksei.nagaev@onlyoffice.com>
2022-10-31 17:44:16 +0300

    Indents fix

--- DesktopEditor/doctrenderer/docbuilder.com/test/dotnet/Program.cs
+++ DesktopEditor/doctrenderer/docbuilder.com/test/dotnet/Program.cs
@@ -10,42 +10,42 @@ using CContextScope = onlyofficedocbuilder.CONLYOFFICEDocBuilderContextScope;
 
 namespace Test
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            string resultPath = "result.docx";
-            Test(resultPath);
-        }
-        public static void Test(string resultPath)
-        {
-            var doctype = ".docx";
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			string resultPath = "result.docx";
+			Test(resultPath);
+		}
+		public static void Test(string resultPath)
+		{
+			var doctype = ".docx";
 			CDocBuilder oBuilder = new CDocBuilder();
 
-            oBuilder.Initialize();
-            
-            oBuilder.CreateFile(doctype);
+			oBuilder.Initialize();
+			
+			oBuilder.CreateFile(doctype);
 
-            CContext oContext = oBuilder.GetContext();
-            CContextScope oScope = oContext.CreateScope();
-          
-            CValue oGlobal = oContext.GetGlobal();
+			CContext oContext = oBuilder.GetContext();
+			CContextScope oScope = oContext.CreateScope();
+		  
+			CValue oGlobal = oContext.GetGlobal();
 
-            CValue oApi = oGlobal.GetProperty("Api");
-            CValue oDocument = oApi.Call("GetDocument");
-            CValue oParagraph = oApi.Call("CreateParagraph");
-            CValue oContent = oContext.CreateArray(1);
+			CValue oApi = oGlobal.GetProperty("Api");
+			CValue oDocument = oApi.Call("GetDocument");
+			CValue oParagraph = oApi.Call("CreateParagraph");
+			CValue oContent = oContext.CreateArray(1);
 
-            oParagraph.Call("SetSpacingAfter", 1000, false);
-            oParagraph.Call("AddText", "Hello from COM -> .net!");
-            oContent.Set(0, oParagraph);
-            oDocument.Call("InsertContent", oContent);
+			oParagraph.Call("SetSpacingAfter", 1000, false);
+			oParagraph.Call("AddText", "Hello from COM -> .net!");
+			oContent.Set(0, oParagraph);
+			oDocument.Call("InsertContent", oContent);
 
-            oBuilder.SaveFile(doctype, resultPath);
-            oBuilder.CloseFile();
+			oBuilder.SaveFile(doctype, resultPath);
+			oBuilder.CloseFile();
 
-            oBuilder.Dispose();
-        }
-    }
+			oBuilder.Dispose();
+		}
+	}
    
 }

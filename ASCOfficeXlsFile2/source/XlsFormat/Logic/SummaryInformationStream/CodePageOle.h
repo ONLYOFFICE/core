984e04cc34bcad5b0dbd0cc3ed523dc46a42b20b
Orca <orca.zhang@yahoo.com>
2022-11-24 11:31:04 +0800

    fix: relative path incorrect that cause building error

--- ASCOfficeXlsFile2/source/XlsFormat/Logic/SummaryInformationStream/CodePageOle.h
+++ ASCOfficeXlsFile2/source/XlsFormat/Logic/SummaryInformationStream/CodePageOle.h
@@ -35,7 +35,7 @@
 #include <string>
 #include <vector>
 #include <boost/smart_ptr/shared_array.hpp>
-#include "../../../../../../Common/DocxFormat/Source/Base/Types_32.h"
+#include "../../../../../Common/DocxFormat/Source/Base/Types_32.h"
 namespace OLEPS
 {
 

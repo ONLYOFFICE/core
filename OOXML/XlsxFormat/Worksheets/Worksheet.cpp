1b2a57a220dbb8c54663438f4e8ba351c898466a
Vikulov Dmitry <Vikulov.Dmitry@gmail.com>
2022-11-26 11:19:09 +0300

    minor

--- OOXML/XlsxFormat/Worksheets/Worksheet.cpp
+++ OOXML/XlsxFormat/Worksheets/Worksheet.cpp
@@ -98,7 +98,6 @@ namespace OOX
 		{
 			ClearItems();
 		}
-
         void CWorksheet::readBin(const CPath& oPath)
         {
             CXlsb* xlsb = dynamic_cast<CXlsb*>(File::m_pMainDocument);
@@ -229,7 +228,6 @@ namespace OOX
 
             }
         }
-
 		void CWorksheet::read(const CPath& oRootPath, const CPath& oPath)
 		{
 			m_oReadPath = oPath;
@@ -410,10 +408,343 @@ namespace OOX
 				m_oSheetData->AfterRead();
 			}
 		}
+		void CWorksheet::ReadWorksheetOptions(XmlUtils::CXmlLiteReader& oReader)
+		{
+			if ( oReader.IsEmptyNode() )
+				return;
 
+			CXlsxFlat* xlsx_flat = dynamic_cast<CXlsxFlat*>(WritingElement::m_pMainDocument);
 
-		//void CWorksheet::ReadWorksheetOptions(XmlUtils::CXmlLiteReader& oReader);//SheetData.cpp
+			if (!xlsx_flat) return;
 
+			if (false == m_oSheetViews.IsInit())
+			{
+				m_oSheetViews.Init();
+				m_oSheetViews->m_arrItems.push_back(new CSheetView());
+			}
+			nullable_int active_pane;
+			nullable_bool bFreeze;
+			nullable_int xSplit, ySplit;
+
+			nullable_int active_pane_number;
+			nullable_int left_column_visible;
+			nullable_int page_break_zoom;
+			std::map<int, nullable<CPane>> mapPanes;
+
+			nullable_string sDataHeader, sDataFooter;
+
+			int nDocumentDepth = oReader.GetDepth();
+			while ( oReader.ReadNextSiblingNode( nDocumentDepth ) )
+			{
+				std::wstring sName = XmlUtils::GetNameNoNS(oReader.GetName());
+
+				if (L"PageSetup" == sName)
+				{
+					if (false == m_oPageSetup.IsInit()) m_oPageSetup.Init();
+
+					int nDocumentDepth1 = oReader.GetDepth();
+					while (oReader.ReadNextSiblingNode(nDocumentDepth1))
+					{
+						std::wstring sName1 = XmlUtils::GetNameNoNS(oReader.GetName());
+
+						if (L"Header" == sName1)
+						{
+							if (false == m_oPageMargins.IsInit()) m_oPageMargins.Init();
+
+							WritingElement_ReadAttributes_Start_No_NS(oReader)
+								WritingElement_ReadAttributes_Read_if(oReader, L"Margin", m_oPageMargins->m_oHeader)
+								WritingElement_ReadAttributes_Read_else_if(oReader, L"Data", sDataHeader)
+							WritingElement_ReadAttributes_End_No_NS(oReader)
+						}
+						else if (L"Footer" == sName1)
+						{
+							if (false == m_oPageMargins.IsInit()) m_oPageMargins.Init();
+
+							WritingElement_ReadAttributes_Start_No_NS(oReader)
+								WritingElement_ReadAttributes_Read_if(oReader, L"Margin", m_oPageMargins->m_oFooter)
+								WritingElement_ReadAttributes_Read_else_if(oReader, L"Data", sDataFooter)
+							WritingElement_ReadAttributes_End_No_NS(oReader)
+						}
+						else if (L"Layout" == sName1)
+						{
+							WritingElement_ReadAttributes_Start_No_NS(oReader)
+								WritingElement_ReadAttributes_Read_if(oReader, L"x:Orientation", m_oPageSetup->m_oOrientation)
+							WritingElement_ReadAttributes_End_No_NS(oReader)
+						}
+						else if (L"PageMargins" == sName1)
+						{
+							if (false == m_oPageMargins.IsInit()) m_oPageMargins.Init();
+
+							WritingElement_ReadAttributes_Start_No_NS(oReader)
+								WritingElement_ReadAttributes_Read_if(oReader, L"Top", m_oPageMargins->m_oTop)
+								WritingElement_ReadAttributes_Read_else_if(oReader, L"Left", m_oPageMargins->m_oLeft)
+								WritingElement_ReadAttributes_Read_else_if(oReader, L"Right", m_oPageMargins->m_oRight)
+								WritingElement_ReadAttributes_Read_else_if(oReader, L"Bottom", m_oPageMargins->m_oBottom)
+							WritingElement_ReadAttributes_End_No_NS(oReader)
+						}
+					}
+				}
+				else if (L"Panes" == sName)
+				{
+					int nDocumentDepth1 = oReader.GetDepth();
+					while (oReader.ReadNextSiblingNode(nDocumentDepth1))
+					{
+						std::wstring sName1 = XmlUtils::GetNameNoNS(oReader.GetName());
+
+						if (L"Pane" == sName1)
+						{
+							nullable<CPane> pane; pane.Init();
+							nullable_int number;
+
+							int nDocumentDepth2 = oReader.GetDepth();
+							nullable_int col, row;
+							while (oReader.ReadNextSiblingNode(nDocumentDepth2))
+							{
+								std::wstring sName2 = XmlUtils::GetNameNoNS(oReader.GetName());
+								if (L"Number" == sName2)
+								{
+									number = oReader.GetText2();
+								}
+								else if (L"ActiveRow" == sName2)
+								{
+									row = oReader.GetText2();
+								}
+								else if (L"ActiveCol" == sName2)
+								{
+									col = oReader.GetText2();
+								}
+								else if (L"RangeSelection" == sName2)
+								{
+									r1c1_formula_convert::base_row = xlsx_flat->m_nLastReadRow;
+									r1c1_formula_convert::base_col = xlsx_flat->m_nLastReadCol;
+
+									r1c1_formula_convert convert;
+
+									std::wstring ref = convert.convert(oReader.GetText2());
+
+									m_oSheetViews->m_arrItems.back()->m_arrItems.push_back(new CSelection());
+									m_oSheetViews->m_arrItems.back()->m_arrItems.back()->m_oSqref = ref;
+
+									size_t pos_split = ref.find(L":");
+									m_oSheetViews->m_arrItems.back()->m_arrItems.back()->m_oActiveCell =
+										pos_split != std::wstring::npos ? ref.substr(0, pos_split) : ref;
+								}
+							}
+							if (col.IsInit() && row.IsInit())
+							{
+								if (m_oSheetViews->m_arrItems.back()->m_arrItems.empty())
+									m_oSheetViews->m_arrItems.back()->m_arrItems.push_back(new CSelection());
+
+								m_oSheetViews->m_arrItems.back()->m_arrItems.back()->m_oActiveCell = getCellAddress(*row + 1, *col + 1);
+
+								if (false == m_oSheetViews->m_arrItems.back()->m_arrItems.back()->m_oSqref.IsInit())
+								{
+									m_oSheetViews->m_arrItems.back()->m_arrItems.back()->m_oSqref = m_oSheetViews->m_arrItems.back()->m_arrItems.back()->m_oActiveCell;
+								}
+							}
+							if (number.IsInit())
+							{
+								mapPanes.insert(std::make_pair(*number, pane));
+							}
+						}
+					}
+				}
+				else if (L"SplitHorizontal" == sName)
+				{
+					ySplit = oReader.GetText2();
+				}
+				else if (L"SplitVertical" == sName)
+				{
+					xSplit = oReader.GetText2();
+				}
+				else if (L"DoNotDisplayGridlines" == sName)
+				{
+					m_oSheetViews->m_arrItems.back()->m_oShowGridLines.Init();
+					m_oSheetViews->m_arrItems.back()->m_oShowGridLines->FromBool(false);
+				}
+				else if (L"Selected" == sName)
+				{
+					m_oSheetViews->m_arrItems.back()->m_oTabSelected.Init();
+					m_oSheetViews->m_arrItems.back()->m_oTabSelected->FromBool(true);
+				}
+				else if (L"FreezePanes" == sName)
+				{
+					bFreeze = true;
+				}
+				else if (L"ActivePane" == sName)
+				{
+					active_pane_number = oReader.GetText2();
+				}
+				else if (L"Print" == sName)
+				{
+					if (false == m_oPageSetup.IsInit()) m_oPageSetup.Init();
+					if (false == m_oPrintOptions.IsInit()) m_oPrintOptions.Init();
+
+					int nDocumentDepth1 = oReader.GetDepth();
+					while (oReader.ReadNextSiblingNode(nDocumentDepth1))
+					{
+						std::wstring sName1 = XmlUtils::GetNameNoNS(oReader.GetName());
+
+						if (L"FitHeight" == sName1)
+						{
+							m_oPageSetup->m_oFitToHeight = oReader.GetText2();
+						}
+						else if (L"DraftQuality" == sName1)
+						{
+							m_oPageSetup->m_oDraft.Init();
+						}
+						else if (L"Gridlines" == sName1)
+						{
+							m_oPrintOptions->m_oGridLines = true;
+						}
+						else if (L"Scale" == sName1)
+						{
+							m_oPageSetup->m_oScale = oReader.GetText2();
+						}
+						else if (L"HorizontalResolution" == sName1)
+						{
+							m_oPageSetup->m_oHorizontalDpi = oReader.GetText2();
+						}
+						else if (L"VerticalResolution" == sName1)
+						{
+							m_oPageSetup->m_oVerticalDpi = oReader.GetText2();
+						}
+						else if (L"PaperSizeIndex" == sName1)
+						{
+							m_oPageSetup->m_oPaperSize = oReader.GetText2();
+						}
+					}
+				}
+				else if (L"FitToPage" == sName)
+				{
+					if (!m_oSheetPr.IsInit()) m_oSheetPr.Init();
+					if (!m_oSheetPr->m_oPageSetUpPr.IsInit()) m_oSheetPr->m_oPageSetUpPr.Init();
+
+					m_oSheetPr->m_oPageSetUpPr->m_oFitToPage.Init();
+					m_oSheetPr->m_oPageSetUpPr->m_oFitToPage->FromBool(true);
+				}
+				else if (L"ProtectObjects" == sName)
+				{
+
+				}
+				else if (L"ProtectScenarios" == sName)
+				{
+
+				}
+				else if (L"ProtectContents" == sName)
+				{
+
+				}
+				else if (L"LeftColumnVisible" == sName)
+				{
+					left_column_visible = oReader.GetText2();
+				}
+				else if (L"PageBreakZoom" == sName)
+				{
+					page_break_zoom = oReader.GetText2();
+				}
+				else if (L"DoNotDisplayColHeaders" == sName)
+				{
+
+				}
+				else if (L"ViewableRange" == sName)
+				{
+
+				}
+				else if (L"GridlineColor" == sName)
+				{
+
+				}
+				else if (L"Unsynced" == sName)
+				{
+
+				}
+				else if (L"DisplayPageBreak" == sName)
+				{
+
+				}
+				else if (L"ShowPageBreakZoom" == sName)
+				{
+					m_oSheetViews->m_arrItems.back()->m_oView = SimpleTypes::Spreadsheet::sheetviewPageBreakPreview;
+				}
+				else if (L"DefaultRowHeight" == sName)
+				{
+
+				}
+				else if (L"DefaultColumnWidth" == sName)
+				{
+
+				}
+				else if (L"Visible" == sName)
+				{
+
+				}
+				else if (L"DisplayRightToLeft" == sName)
+				{
+
+				}
+				else if (L"DisplayFormulas" == sName)
+				{
+					m_oSheetViews->m_arrItems.back()->m_oShowFormulas = true;
+				}
+				else if (L"ActiveRow" == sName)
+				{
+
+				}
+				else if (L"ActiveColumn" == sName)
+				{
+
+				}
+				else if (L"TabColorIndex" == sName)
+				{
+					if (false == m_oSheetPr.IsInit()) m_oSheetPr.Init();
+					m_oSheetPr->m_oTabColor.Init();  m_oSheetPr->m_oTabColor->m_oIndexed = oReader.GetText2();
+				}
+			}
+
+			if (m_oPageMargins.IsInit())
+			{
+				if (!m_oPageMargins->m_oFooter.IsInit())
+				{
+					m_oPageMargins->m_oFooter.Init();
+					m_oPageMargins->m_oFooter->SetValue(0.5);
+				}
+				if (!m_oPageMargins->m_oHeader.IsInit())
+				{
+					m_oPageMargins->m_oHeader.Init();
+					m_oPageMargins->m_oHeader->SetValue(0.5);
+				}
+			}
+
+			if (active_pane_number.IsInit())
+			{
+				std::map<int, nullable<CPane>>::iterator pFind = mapPanes.find(*active_pane_number);
+				if (pFind != mapPanes.end())
+				{
+					m_oSheetViews->m_arrItems.back()->m_oPane = pFind->second;
+				}
+			}
+			if (m_oSheetViews->m_arrItems.back()->m_oPane.IsInit())
+			{
+				m_oSheetViews->m_arrItems.back()->m_oPane->m_oActivePane.Init();
+				m_oSheetViews->m_arrItems.back()->m_oPane->m_oActivePane->SetValue(SimpleTypes::Spreadsheet::activepaneBottomLeft);
+				if (bFreeze.IsInit())
+				{
+					m_oSheetViews->m_arrItems.back()->m_oPane->m_oState.Init();
+					m_oSheetViews->m_arrItems.back()->m_oPane->m_oState->SetValue(SimpleTypes::Spreadsheet::panestateFrozen);
+				}
+				if (ySplit.IsInit())
+				{
+					m_oSheetViews->m_arrItems.back()->m_oPane->m_oYSplit.Init();
+					m_oSheetViews->m_arrItems.back()->m_oPane->m_oYSplit->SetValue(*ySplit);
+				}
+				if (xSplit.IsInit())
+				{
+					m_oSheetViews->m_arrItems.back()->m_oPane->m_oXSplit.Init();
+					m_oSheetViews->m_arrItems.back()->m_oPane->m_oXSplit->SetValue(*xSplit);
+				}
+			}
+		}
 		void CWorksheet::ReadAttributes(XmlUtils::CXmlLiteReader& oReader)
 		{
 			nullable_string sName;
@@ -465,7 +796,6 @@ namespace OOX
 				}
 			}
 		}
-
 		void CWorksheet::PrepareConditionalFormatting()
 		{
 			if (m_oExtLst.IsInit() == false) return;
@@ -886,7 +1216,6 @@ mc:Ignorable=\"x14ac\">");
 		{
 			writer.WriteString(L"</worksheet>");
 		}
-
 		const OOX::RId CWorksheet::AddHyperlink (std::wstring& sHref)
 		{
 			std::wstring sExistRId = IsExistHyperlink(sHref);
@@ -920,7 +1249,6 @@ mc:Ignorable=\"x14ac\">");
 			}
 			m_arrConditionalFormatting.clear();
 		}
-
 		smart_ptr<OOX::WritingElement> CWorksheet::FindVmlObject(const std::wstring &spid)
 		{
 			smart_ptr<OOX::WritingElement> oElement;
@@ -938,7 +1266,34 @@ mc:Ignorable=\"x14ac\">");
 			}
 			return oElement;
 		}
-//----------------------------------------------------------------------------------------------------------------------
-
+		void CWorksheet::read(const CPath& oPath)
+		{
+			//don't use this. instead use read(const CPath& oRootPath, const CPath& oFilePath)
+			CPath oRootPath;
+			read(oRootPath, oPath);
+		}
+		void CWorksheet::fromXML(XmlUtils::CXmlNode& node)
+		{
+		}
+		std::wstring CWorksheet::toXML() const
+		{
+			return _T("");
+		}
+		const OOX::FileType CWorksheet::type() const
+		{
+			return m_bIsChartSheet?OOX::Spreadsheet::FileTypes::Chartsheets:OOX::Spreadsheet::FileTypes::Worksheet;
+		}
+		const CPath CWorksheet::DefaultDirectory() const
+		{
+			return type().DefaultDirectory();
+		}
+		const CPath CWorksheet::DefaultFileName() const
+		{
+			return type().DefaultFileName();
+		}
+		const CPath& CWorksheet::GetReadPath() const
+		{
+			return m_oReadPath;
+		}
 	}
 }

43ebeec93c3cdca2ee5750837392f9adaecb4160
Elena.Subbotina <Elena.Subbotina@onlyoffice.com>
2023-01-23 20:32:21 +0300

    .

--- OdfFile/Writer/Format/odf_math_context.h
+++ OdfFile/Writer/Format/odf_math_context.h
@@ -43,13 +43,36 @@
 #define CREATE_MATH_TAG(tag)\
 	odf_writer::office_element_ptr elm;\
 	odf_writer::create_element(L"math", tag, elm, odf_context());\
-	//odf_context()->math_context()->debug_stream << tag << "\n";
+	odf_context()->math_context()->current_tag = tag;	
+//odf_context()->math_context()->debug_stream << tag << "\n";
 
 #define OPEN_MATH_TAG(elm)\
-	odf_context()->math_context()->tagFlag.push_back(odf_context()->math_context()->start_element(elm)); \
-	if(odf_context()->math_context()->tagFlag.back()) \
-		odf_context()->math_context()->counter++; \
-	//odf_context()->math_context()->debug_stream << L"open, counter is " << odf_context()->math_context()->counter << "\n";
+	odf_context()->math_context()->tagFlag.push_back(odf_context()->math_context()->start_element(elm));\
+	if (odf_context()->math_context()->current_tag == L"mn")\
+	{\
+		typedef odf_writer::math_mn* T;\
+		T tmp = dynamic_cast<T>(elm.get());\
+		odf_context()->math_context()->symbol_counter += tmp->text_.get().size();\
+	}\
+	if (odf_context()->math_context()->current_tag == L"mi")\
+	{\
+		typedef odf_writer::math_mi* T;\
+		T tmp = dynamic_cast<T>(elm.get());\
+		odf_context()->math_context()->symbol_counter += tmp->text_.get().size();\
+	}\
+	if (odf_context()->math_context()->current_tag == L"mo")\
+	{\
+		typedef odf_writer::math_mo* T;\
+		T tmp = dynamic_cast<T>(elm.get());\
+		odf_context()->math_context()->symbol_counter += tmp->text_.get().size();\
+	}\
+	if (odf_context()->math_context()->tagFlag.back())\
+	{\
+		odf_context()->math_context()->counter++;\
+	}\
+	odf_context()->math_context()->current_tag = L"";
+
+//odf_context()->math_context()->debug_stream << L"open, counter is " << odf_context()->math_context()->counter << "\n";
 
 #define CLOSE_MATH_TAG\
 	if(odf_context()->math_context()->tagFlag.back()) \
@@ -57,7 +80,7 @@
 		odf_context()->math_context()->end_element();\
 		odf_context()->math_context()->counter--; \
 	}\
-	odf_context()->math_context()->tagFlag.pop_back(); \
+	odf_context()->math_context()->tagFlag.pop_back(); 
 
 	//odf_context()->math_context()->debug_stream /*std::wcout*/ << L"close, counter is " << odf_context()->math_context()->counter << "\n";
 
@@ -72,18 +95,18 @@ namespace cpdoccore {
 		class odf_math_context
 		{
 		public:
-			odf_math_context(odf_conversion_context *odf_context);
+			odf_math_context(odf_conversion_context* odf_context);
 			~odf_math_context();
 
-			void set_styles_context(odf_style_context * style_context);
+			void set_styles_context(odf_style_context* style_context);
 
-			odf_drawing_context *drawing_context();
-			odf_text_context	*text_context();
+			odf_drawing_context* drawing_context();
+			odf_text_context* text_context();
 
-			void start_math(office_element_ptr & root);
-						
-			bool start_element(office_element_ptr & elm); // office_math_element TODO
-			
+			void start_math(office_element_ptr& root);
+
+			bool start_element(office_element_ptr& elm); // office_math_element TODO
+
 			void end_element();
 
 			std::vector<std::vector<std::wstring>> brackets;
@@ -91,10 +114,21 @@ namespace cpdoccore {
 			int lvl_of_me;
 			int matrix_row_counter;
 			std::vector<int> end_counter;
-			int counter; 
+			bool style_flag;
+			int counter;
+			std::wstring current_tag;
+			int symbol_counter;
+			double lvl_counter;
+			double lvl_up_counter;
+			double lvl_down_counter;
+			double lvl_max;
+			double lvl_min;
+			std::wstring font;
+			double size;
 			std::set<wchar_t> mo;
 			std::map<std::wstring, std::wstring> diak_symbols;
 			bool annotation_flag;
+			bool annotation_oper_flag;
 			std::map<std::wstring, std::wstring> annotation_operators;
 			std::map<std::wstring, std::wstring> annotation_brackets_begin;
 			std::map<std::wstring, std::wstring> annotation_brackets_end;

64cfee514f1fab5e40b5ba82d983def7870db9cf
RIMINMIR <viktor.andreev@onlyoffice.com>
2023-04-24 13:50:03 +0600

    updated license text

--- OOXML/Binary/Sheets/Reader/XMLReader/XMLReader.cpp
+++ OOXML/Binary/Sheets/Reader/XMLReader/XMLReader.cpp
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright Ascensio System SIA 2010-2019
+ * (c) Copyright Ascensio System SIA 2010-2023
  *
  * This program is a free software product. You can redistribute it and/or
  * modify it under the terms of the GNU Affero General Public License (AGPL)
@@ -12,7 +12,7 @@
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For
  * details, see the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
  *
- * You can contact Ascensio System SIA at 20A-12 Ernesta Birznieka-Upisha
+ * You can contact Ascensio System SIA at 20A-6 Ernesta Birznieka-Upish
  * street, Riga, Latvia, EU, LV-1050.
  *
  * The  interactive user interfaces in modified source and object code versions
@@ -36,17 +36,9 @@
 #include "../../../../DesktopEditor/common/File.h"
 #include "../../../Base/unicode_util.h"
 #include "../../../../Common/OfficeFileErrorDescription.h"
-#include "../../../../UnicodeConverter/UnicodeConverter.h"
-#include "../../../../UnicodeConverter/UnicodeConverter_Encodings.h"
 #include "../../../XlsxFormat/Workbook/Workbook.h"
-#include "../../../XlsxFormat/SharedStrings/SharedStrings.h"
-#include "../../../XlsxFormat/Styles/Styles.h"
-
-#include "../../../../DesktopEditor/xml/include/xmlutils.h"
 
 #include <memory>
-
-constexpr auto SheetName = L"Sheet ";
 
 _UINT32 XMLReader::Read(const std::wstring &sFileName, OOX::Spreadsheet::CXlsx &oXlsx)
 {
--- OOXML/Binary/Sheets/Reader/XMLReader/XMLReader.h
+++ OOXML/Binary/Sheets/Reader/XMLReader/XMLReader.h
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright Ascensio System SIA 2010-2019
+ * (c) Copyright Ascensio System SIA 2010-2023
  *
  * This program is a free software product. You can redistribute it and/or
  * modify it under the terms of the GNU Affero General Public License (AGPL)
@@ -12,7 +12,7 @@
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For
  * details, see the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
  *
- * You can contact Ascensio System SIA at 20A-12 Ernesta Birznieka-Upisha
+ * You can contact Ascensio System SIA at 20A-6 Ernesta Birznieka-Upish
  * street, Riga, Latvia, EU, LV-1050.
  *
  * The  interactive user interfaces in modified source and object code versions
@@ -43,4 +43,10 @@ public:
     /// @param oXlsx объект Xlsx таблицы в которую считается xml
     /// @return код выполнения операции
 	_UINT32 Read(const std::wstring &sFileName, OOX::Spreadsheet::CXlsx &oXlsx);
+
+    /// @brief Вторая версия считывающего метода
+    /// @param FileName имя xml файла который будет считан
+    /// @param oXlsx объект Xlsx таблицы в которую считается xml
+    /// @return код выполнения операции
+	_UINT32 Read2(const std::wstring &sFileName, OOX::Spreadsheet::CXlsx &oXlsx);
 };

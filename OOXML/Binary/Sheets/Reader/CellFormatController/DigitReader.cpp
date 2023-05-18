d3f75c50337b0dc72c660da4ccffcb6419af1eca
RIMINMIR <viktor.andreev@onlyoffice.com>
2023-05-18 23:19:33 +0600

    correct digit format

--- OOXML/Binary/Sheets/Reader/CellFormatController/DigitReader.cpp
+++ OOXML/Binary/Sheets/Reader/CellFormatController/DigitReader.cpp
@@ -98,7 +98,18 @@ bool DigitReader::ReadDigit(const std::wstring &value, std::wstring &digit, std:
 				{
 					digit = std::to_wstring(dValue);
 
-					data_format = createFractionFormat(value, pEndPtr);
+					 // Удаление лишних нулей после запятой
+					size_t dotPos = digit.find('.');
+					if (dotPos != std::wstring::npos)
+					{
+						size_t lastNonZeroPos = digit.find_last_not_of(L'0');
+                        if (lastNonZeroPos <= dotPos)
+						{
+							digit.erase(lastNonZeroPos + 3);
+						}
+					}
+
+                    data_format = createFractionFormat(digit, pEndPtr);
 					for (size_t i = 0; i < postfix.size(); ++i)
 					{
 						data_format += std::wstring(L"\\") + postfix[i];

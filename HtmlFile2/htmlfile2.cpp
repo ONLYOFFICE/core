01bfa49aba3e3b1179d1a767ba0fe66f8e6e7524
Kulikova Svetlana <Svetlana.Kulikova@onlyoffice.com>
2023-06-02 14:52:28 +0300

    fix epub EncodeXmlString

--- HtmlFile2/htmlfile2.cpp
+++ HtmlFile2/htmlfile2.cpp
@@ -72,6 +72,34 @@ struct CTextSettings
         bBdo(oTS.bBdo), bPre(oTS.bPre), nLi(oTS.nLi), sRStyle(oTS.sRStyle), sPStyle(oTS.sPStyle) {}
 };
 
+void replace_all(std::wstring& s, const std::wstring& s1, const std::wstring& s2)
+{
+    size_t pos = s.find(s1);
+    size_t l = s2.length();
+    while (pos != std::string::npos)
+    {
+        if (!(s1 == L"&" && s2 == L"&amp;" && s.length() > pos + 4 && s[pos] == L'&' && s[pos + 1] == L'a' && s[pos + 2] == L'm' && s[pos + 3] == L'p' && s[pos + 4] == L';'))
+            s.replace(pos, s1.length(), s2);
+        pos = s.find(s1, pos + l);
+    }
+}
+
+std::wstring EncodeXmlString(const std::wstring& s)
+{
+    std::wstring sRes = s;
+
+    replace_all(sRes, L"&", L"&amp;");
+    replace_all(sRes, L"<", L"&lt;");
+    replace_all(sRes, L">", L"&gt;");
+    replace_all(sRes, L"\"", L"&quot;");
+    replace_all(sRes, L"\'", L"&#39;");
+    replace_all(sRes, L"\n", L"&#xA;");
+    replace_all(sRes, L"\r", L"&#xD;");
+    replace_all(sRes, L"\t", L"&#x9;");
+
+    return sRes;
+}
+
 class CHtmlFile2_Private
 {
 public:
@@ -242,31 +270,31 @@ public:
             if(!oParams->m_sBookTitle.empty())
             {
                 sCore += L"<dc:title>";
-                sCore += oParams->m_sBookTitle;
+                sCore += EncodeXmlString(oParams->m_sBookTitle);
                 sCore += L"</dc:title>";
             }
             if(!oParams->m_sAuthors.empty())
             {
                 sCore += L"<dc:creator>";
-                sCore += oParams->m_sAuthors;
+                sCore += EncodeXmlString(oParams->m_sAuthors);
                 sCore += L"</dc:creator>";
             }
             if(!oParams->m_sGenres.empty())
             {
                 sCore += L"<dc:subject>";
-                sCore += oParams->m_sGenres;
+                sCore += EncodeXmlString(oParams->m_sGenres);
                 sCore += L"</dc:subject>";
             }
             if(!oParams->m_sDate.empty())
             {
                 sCore += L"<dcterms:created xsi:type=\"dcterms:W3CDTF\">";
-                sCore += oParams->m_sDate;
+                sCore += EncodeXmlString(oParams->m_sDate);
                 sCore += L"</dcterms:created>";
             }
             if(!oParams->m_sDescription.empty())
             {
                 sCore += L"<dc:description>";
-                sCore += oParams->m_sDescription;
+                sCore += EncodeXmlString(oParams->m_sDescription);
                 sCore += L"</dc:description>";
             }
         }

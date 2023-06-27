98eb8566415247b61a0d2b067a22548a32344ad8
RIMINMIR <viktor.andreev@onlyoffice.com>
2023-06-27 16:20:26 +0600

    add drawning conversion

--- OOXML/XlsxFormat/Drawing/Drawing.h
+++ OOXML/XlsxFormat/Drawing/Drawing.h
@@ -62,6 +62,7 @@ namespace OOX
 			virtual void fromXML(XmlUtils::CXmlLiteReader& oReader);
 
 			void fromBin(XLS::BaseObjectPtr& obj);
+			XLS::BaseObjectPtr toBin();
 			virtual EElementType getType () const;
 
 		private:

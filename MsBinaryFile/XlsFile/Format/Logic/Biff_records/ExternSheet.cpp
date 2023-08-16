f245a5b6a51bff1eb2638ea5fa82df493ce6b60a
Elena.Subbotina <Elena.Subbotina@onlyoffice.com>
2023-08-16 08:37:42 +0300

    fix bug #63681

--- MsBinaryFile/XlsFile/Format/Logic/Biff_records/ExternSheet.cpp
+++ MsBinaryFile/XlsFile/Format/Logic/Biff_records/ExternSheet.cpp
@@ -75,8 +75,10 @@ void ExternSheet::readFields(CFRecord& record)
 	{
         _UINT16 cXTI_2b;
         record >> cXTI_2b;
-        for(int i = 0; i < cXTI_2b; ++i)
+        for(_UINT16 i = 0; i < cXTI_2b; ++i)
 		{
+			if (record.getRdPtr() + 6 > record.getDataSize())
+				break;
 			XTIPtr xti(new XTI);
 			record >> *xti;
 			rgXTI.push_back(xti);
@@ -86,7 +88,7 @@ void ExternSheet::readFields(CFRecord& record)
     else
     {
         record >> cXTI;
-        for(int i = 0; i < cXTI; ++i)
+        for(_UINT32 i = 0; i < cXTI; ++i)
         {
             XTIPtr xti(new XTI);
             record >> *xti;

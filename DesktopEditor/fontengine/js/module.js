cd64b42c0347a4c7a10f14ce5179b7267ac09236
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2023-08-28 12:33:49 +0300

    Fixes for fonts module

--- DesktopEditor/fontengine/js/module.js
+++ DesktopEditor/fontengine/js/module.js
@@ -190,7 +190,7 @@ function onLoadFontsModule(window, undefined)
 		var c = READER.readInt();
 		while (c)
 		{
-			this.family_name += String.fromCharCode(c);
+			this.family_name += (c < 0x10000) ? String.fromCharCode(c) : (String.fromCharCode(0xD800 | (c >> 10)) + String.fromCharCode(0xDC00 | (c & 0x3FF)));
 			c = READER.readInt();
 		}
 
@@ -408,7 +408,7 @@ function onLoadFontsModule(window, undefined)
 		}
 	};
 
-	const FONTSIZE       = 72;
+	const FONTSIZE       = 576;
 	const STRING_MAX_LEN = 1024;
 	const COEF           = 25.4 / 72 / 64 / FONTSIZE;
 	let   STRING_POINTER = null;
@@ -422,6 +422,7 @@ function onLoadFontsModule(window, undefined)
 	AscFonts.MEASURE_FONTSIZE        = FONTSIZE;
 	AscFonts.GRAPHEME_STRING_MAX_LEN = STRING_MAX_LEN;
 	AscFonts.GRAPHEME_COEF           = COEF;
+	AscFonts.HB_STRING_MAX_LEN       = STRING_MAX_LEN;
 
 	function CCodePointsCalculator(codePointsBuffer, clusterBuffer)
 	{

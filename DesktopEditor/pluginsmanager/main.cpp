690665fa63ee9c3177a8c4f8d1cabb042a6fcb52
Vikulov Dmitry <dmitry.vikulov@onlyoffice.com>
2023-09-29 08:31:29 +0300

    minor

--- DesktopEditor/pluginsmanager/main.cpp
+++ DesktopEditor/pluginsmanager/main.cpp
@@ -807,34 +807,37 @@ private:
 					}
 
 					CPluginInfo* pPluginInfo = ReadPluginInfo(sConfigFile);
-					CPluginInfo* pInstalled = FindLocalPlugin(pPluginInfo->m_sGuid);
-
-					if ( pInstalled )
+					if ( pPluginInfo )
 					{
-						sPrintInfo = L"Already installed";
-						bResult = true;
-					}
-					else if ( pPluginInfo )
-					{
-						std::wstring sPluginDir = m_sPluginsDir + L"/" + (bDirGuid ? pPluginInfo->m_sGuid : pPluginInfo->m_sName);
+						CPluginInfo* pInstalled = FindLocalPlugin(pPluginInfo->m_sGuid);
 
-						// Check settings
-						// Can install if user hasn't deleted the plugin before
-						CPluginInfo* pRemoved = FindLocalPlugin(pPluginInfo->m_sGuid, Removed);
-						if ( !pRemoved )
+						if ( pInstalled )
 						{
-							if ( NSDirectory::Exists(sPluginDir) )
-								NSDirectory::DeleteDirectory(sPluginDir);
-							NSDirectory::CreateDirectory(sPluginDir);
-
-							NSDirectory::CopyDirectory(sTempDirExt, sPluginDir);
-
+							sPrintInfo = L"Already installed";
 							bResult = true;
 						}
-						else
+						else if ( pPluginInfo )
 						{
-							sPrintInfo = L"Installation cancelled. The plugin has been removed before.\n" \
-								L"Use --reset option to reset settings";
+							std::wstring sPluginDir = m_sPluginsDir + L"/" + (bDirGuid ? pPluginInfo->m_sGuid : pPluginInfo->m_sName);
+
+							// Check settings
+							// Can install if user hasn't deleted the plugin before
+							CPluginInfo* pRemoved = FindLocalPlugin(pPluginInfo->m_sGuid, Removed);
+							if ( !pRemoved )
+							{
+								if ( NSDirectory::Exists(sPluginDir) )
+									NSDirectory::DeleteDirectory(sPluginDir);
+								NSDirectory::CreateDirectory(sPluginDir);
+
+								NSDirectory::CopyDirectory(sTempDirExt, sPluginDir);
+
+								bResult = true;
+							}
+							else
+							{
+								sPrintInfo = L"Installation cancelled. The plugin has been removed before.\n" \
+									L"Use --reset option to reset settings";
+							}
 						}
 					}
 				}

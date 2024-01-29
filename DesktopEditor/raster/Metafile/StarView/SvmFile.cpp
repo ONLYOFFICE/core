e61999c5feec1b9e7931a69b04bb71870c87524b
Elena.Subbotina <Elena.Subbotina@onlyoffice.com>
2024-01-29 12:18:08 +0300

    fix bug #66192

--- DesktopEditor/raster/Metafile/StarView/SvmFile.cpp
+++ DesktopEditor/raster/Metafile/StarView/SvmFile.cpp
@@ -1070,7 +1070,7 @@ void CSvmFile::Read_META_BMP(TSvmBitmap & bitmap_info, BYTE** ppDstBuffer, unsig
         MetaFile::ReadImage((BYTE*)&bitmap_info, bitmap_info.nSize, destBuf, destSize, ppDstBuffer, pulWidth, pulHeight);
         delete []destBuf;
     }
-    else
+    else if (nHeaderSize >= bitmap_info.nSize)
     {
         BYTE *Header = new BYTE [ nHeaderSize];
         memcpy(Header, &bitmap_info, bitmap_info.nSize);

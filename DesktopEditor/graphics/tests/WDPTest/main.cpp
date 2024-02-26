dc5f25f162d8c3d8b671ee77b74ea39d7da53453
K1rillProkhorov <mr.kirill.prokhorov@list.ru>
2024-02-26 16:46:43 +0300

    Add test to WDP file format

---
+++ DesktopEditor/graphics/tests/WDPTest/main.cpp
@@ -0,0 +1,11 @@
+#include "../../Image.h"
+
+#include <codecvt>
+
+int main(int argc, char* argv[])
+{
+	Aggplus::CImage Cimg(std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(argv[1]));
+	Cimg.SaveFile(std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(argv[2]), 1);
+
+	return 0;
+}

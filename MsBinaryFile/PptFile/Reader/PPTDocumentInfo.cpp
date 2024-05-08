39748f9d9e3526425c4c9d86c41ccad5ea368962
Elena.Subbotina <Elena.Subbotina@onlyoffice.com>
2024-05-08 11:49:04 +0300

    fix bug #67895

--- MsBinaryFile/PptFile/Reader/PPTDocumentInfo.cpp
+++ MsBinaryFile/PptFile/Reader/PPTDocumentInfo.cpp
@@ -33,7 +33,7 @@
 
 using namespace PPT;
 
-CPPTDocumentInfo::CPPTDocumentInfo() : m_oCurrentUser(), m_bMacros(true), m_pStream(NULL)
+CPPTDocumentInfo::CPPTDocumentInfo() : m_oCurrentUser(), m_bMacroEnabled(true), m_pStream(NULL)
 {
 }
 
@@ -79,14 +79,13 @@ bool CPPTDocumentInfo::ReadFromStream(CRecordCurrentUserAtom *pCurrentUser, POLE
 
         pInfo->m_pDocumentInfo      = this;
 
-        pInfo->m_bEncrypt			= m_oCurrentUser.m_bIsEncrypt;
-        pInfo->m_strPassword		= m_strPassword;
-        pInfo->m_bMacros			= m_bMacros;
+        pInfo->m_bEncrypt = m_oCurrentUser.m_bIsEncrypt;
+        pInfo->m_strPassword = m_strPassword;
+        pInfo->m_bMacroEnabled = m_bMacroEnabled;
 
         bool bResult = pInfo->ReadFromStream(&oUserAtom, pStream);
 
-        m_bMacros					= pInfo->m_bMacros;
-        offsetToEdit				= pInfo->m_oUser.m_nOffsetLastEdit;
+        offsetToEdit = pInfo->m_oUser.m_nOffsetLastEdit;
         m_oCurrentUser.m_bIsEncrypt = pInfo->m_bEncrypt;
 
         if (bResult == false)
@@ -106,7 +105,6 @@ bool CPPTDocumentInfo::ReadFromStream(CRecordCurrentUserAtom *pCurrentUser, POLE
 
         pInfo = NULL;
     }
-
     return true;
 }
 std::wstring CPPTDocumentInfo::GetBinFromStg(const std::wstring& name, _UINT32 nRef)
--- MsBinaryFile/PptFile/Reader/PPTDocumentInfo.h
+++ MsBinaryFile/PptFile/Reader/PPTDocumentInfo.h
@@ -47,7 +47,7 @@ public:
 	std::vector<CPPTUserInfo*>		m_arUsers;
 	std::map<int, std::wstring>		m_mapStoreImageFile;
     std::wstring					m_strPassword;
-	bool							m_bMacros;
+	bool							m_bMacroEnabled;
 
 	std::wstring					m_app_xml;
 	std::wstring					m_core_xml;

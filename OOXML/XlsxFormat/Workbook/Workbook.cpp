a47e72cf2f3fca22e22eb09105e2282c5398f652
Elena Subbotina <Elena.Subbotina@onlyoffice.com>
2024-07-19 11:07:47 +0300

    fix bug #69311

--- OOXML/XlsxFormat/Workbook/Workbook.cpp
+++ OOXML/XlsxFormat/Workbook/Workbook.cpp
@@ -32,6 +32,8 @@
 #pragma once
 
 #include "Workbook.h"
+#include "../Worksheets/Worksheet.h"
+
 #include "../../XlsbFormat/Xlsb.h"
 
 #include "../../XlsbFormat/WorkBookStream.h"
@@ -381,10 +383,28 @@ namespace OOX
 			IFileContainer::Read(oRootPath, oPath); //в данном случае порядок считывания важен для xlsb
 
 			CXlsx* xlsx = dynamic_cast<CXlsx*>(File::m_pMainDocument);
-			if ((xlsx) && (xlsx->m_pVbaProject))
+			if (xlsx)
 			{
-				m_bMacroEnabled = true;
+				if (xlsx->m_pVbaProject)
+				{
+					m_bMacroEnabled = true;
+				}
+				//дубли листов
+				for (auto elm : this->m_mapContainer)
+				{
+					if (elm.second->type() == OOX::Spreadsheet::FileTypes::Chartsheets || elm.second->type() == OOX::Spreadsheet::FileTypes::Worksheet)
+					{
+						if (xlsx->m_mapWorksheets.end() == xlsx->m_mapWorksheets.find(elm.first))
+						{
+							CWorksheet* sheet = dynamic_cast<CWorksheet*>(elm.second.GetPointer());
+							xlsx->m_arWorksheets.push_back(sheet);
+							xlsx->m_mapWorksheets.insert(std::make_pair(elm.first, sheet));
+						}
+
+					}
+				}
 			}
+
 		}
 		void CWorkbook::toXML(NSStringUtils::CStringBuilder& writer) const
 		{

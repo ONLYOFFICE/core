a6365c23c688858a174046ecde1332d3d6a2244c
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2024-08-06 10:52:06 +0300

    Refactoring

--- DesktopEditor/graphics/tests/booleanPaths/mainwindow.cpp
+++ DesktopEditor/graphics/tests/booleanPaths/mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "../../BooleanOperations.h"
+#include "../../GraphicsPath.h"
 
 MainWindow::MainWindow(QWidget *parent)
 	: QMainWindow(parent)

56088e9eff081e35f82731b8e5a3851d76dbcc10
Mikhail Lobotskiy <Mikhail.Lobotskiy@onlyoffice.com>
2024-08-13 20:44:46 +0400

    Add support for JAVA_HOME environment variable

--- DesktopEditor/doctrenderer/docbuilder.java/make.py
+++ DesktopEditor/doctrenderer/docbuilder.java/make.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import re
 import platform
+import subprocess
 
 # NOTE: In JDK 8 and earlier, `javac` does not create the directories specified in the -d option if they do not already exist
 #       So we need to create them manually
@@ -12,14 +13,14 @@ def makedirs(dir):
 
 # return all files with extension `ext` in directory `dir` as string
 def getFilesInDir(dir, ext):
-    files = '';
+    files = [];
     for file in os.listdir(dir):
         if file.endswith(ext):
             # for non-windows systems '$'-symbol in file names should be escaped
             if platform.system().lower() != 'windows':
                 file = re.sub(r'\$', r'\\$', file)
-            files += ' ' + dir + '/' + file
-    
+            files.append(os.path.join(dir, file))
+
     return files
 
 if __name__ == "__main__":
@@ -34,12 +35,25 @@ if __name__ == "__main__":
     java_files = getFilesInDir('docbuilder', '.java')
     java_files += getFilesInDir('docbuilder/utils', '.java')
 
+    # INITIALIZE JAVA TOOLS
+    javac = 'javac'
+    jar = 'jar'
+    ext = '.exe' if platform.system().lower() == 'windows' else ''
+    java_home = os.environ.get('JAVA_HOME')
+    if java_home:
+        javac = os.path.join(java_home, 'bin', 'javac' + ext)
+        jar = os.path.join(java_home, 'bin', 'jar' + ext)
+
+    if not os.path.exists(javac):
+        print('Error: Cannot find: ' + javac)
+        exit()
+
     # BUILD
     classes_dir = file_dir + '/build/classes'
     makedirs(classes_dir + '/docbuilder/utils')
     headers_dir = file_dir + '/src/jni'
     # build all Java classes
-    os.system('javac -d ' + classes_dir + (' -h ' + headers_dir if args.headers else '') + java_files)
+    subprocess.call([javac, '-d', classes_dir] + java_files, cwd=os.getcwd())
 
     # PACKING TO JAR
     if not args.no_jar:
@@ -47,4 +61,4 @@ if __name__ == "__main__":
         class_files = getFilesInDir('docbuilder', '.class')
         class_files += getFilesInDir('docbuilder/utils', '.class')
         makedirs('../libs')
-        os.system('jar -cvf ../libs/docbuilder.jar ' + class_files)
+        subprocess.call([jar, '-cvf', '../libs/docbuilder.jar'] + class_files, cwd=os.getcwd())

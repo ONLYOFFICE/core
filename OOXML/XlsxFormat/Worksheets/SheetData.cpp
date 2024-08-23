4384ca2274590a83e9d62dca4949681130a05660
Viktor Andreev <viktor.andreev@onlyoffice.com>
2024-08-23 15:12:25 +0600

    Fix number value check

--- OOXML/XlsxFormat/Worksheets/SheetData.cpp
+++ OOXML/XlsxFormat/Worksheets/SheetData.cpp
@@ -1957,7 +1957,7 @@ namespace OOX
 							wchar_t *tail;
 							long double tempVal = std::wcstold(m_oValue->m_sText.c_str(), &tail);
 							if(*tail == L'\0')
-								if(tempVal <= DBL_MAX && tempVal >= DBL_MIN)
+                                if(tempVal <= DBL_MAX && tempVal >= -DBL_MAX)
 								{
 									m_oType->SetValue(SimpleTypes::Spreadsheet::celltypeNumber);
 									realCache = tempVal;

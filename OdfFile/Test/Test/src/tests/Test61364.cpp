7e7186f06899fb5aaace62522bb8c567f17e2737
Kamil Kerimov <kamil.kerimov@onlyoffice.com>
2024-08-29 21:39:58 +0500

    Test odt to docx tab size conversion

--- OdfFile/Test/Test/src/tests/Test61364.cpp
+++ OdfFile/Test/Test/src/tests/Test61364.cpp
@@ -1,5 +1,7 @@
 #include "Test61364.h" 
 
+#include "../../../../../OOXML/DocxFormat/Settings/Settings.h"
+
 TestEnv61364* g_TestEnv61364 = (TestEnv61364*)testing::AddGlobalTestEnvironment(new TestEnv61364);
 
 TestEnv61364::TestEnv61364()
@@ -19,4 +21,22 @@ TEST_F(Test61364, Docx)
 TEST_F(Test61364, document)
 {
 	EXPECT_TRUE(Docx->m_oMain.document);
+}
+
+TEST_F(Test61364, settings)
+{
+	EXPECT_TRUE(Docx->m_oMain.settings);
+}
+
+TEST_F(Test61364, settings_default_tab_stop)
+{
+	ASSERT_TRUE(Docx->m_oMain.settings->m_oDefaultTabStop.IsInit());
+	const ComplexTypes::Word::CTwipsMeasure& tabStop = Docx->m_oMain.settings->m_oDefaultTabStop.get();
+
+	ASSERT_TRUE(tabStop.m_oVal.IsInit());
+	const SimpleTypes::CTwipsMeasure& tabStopValue = tabStop.m_oVal.get();
+
+	const int expected_tab_stop_in_twips = 282;
+
+	EXPECT_EQ(tabStopValue.ToTwips(), expected_tab_stop_in_twips);
 }

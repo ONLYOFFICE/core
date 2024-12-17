48d86cb7ceacc1375067d0575156431e89250a5c
Alexey Nagaev <aleksei.nagaev@onlyoffice.com>
2024-12-17 15:39:50 +0300

    Add base classes

--- DocxRenderer/src/logic/elements/Table.cpp
+++ DocxRenderer/src/logic/elements/Table.cpp
@@ -2,5 +2,54 @@
 
 namespace NSDocxRenderer
 {
+    void CTable::CCell::Clear()
+	{
+
+	}
+	void CTable::CCell::ToXml(NSStringUtils::CStringBuilder& oWriter) const
+	{
+
+	}
+	void CTable::CCell::ToXmlPptx(NSStringUtils::CStringBuilder& oWriter) const
+	{
+
+	}
+
+	void CTable::CRow::Clear()
+	{
+
+	}
+	void CTable::CRow::ToXml(NSStringUtils::CStringBuilder& oWriter) const
+	{
+
+	}
+	void CTable::CRow::ToXmlPptx(NSStringUtils::CStringBuilder& oWriter) const
+	{
+
+	}
+
+	void CTable::Clear()
+	{
+
+	}
+	void CTable::ToXml(NSStringUtils::CStringBuilder& oWriter) const
+	{
+		oWriter.WriteString(L"<w:tbl>");
+
+		oWriter.WriteString(L"<w:tblPr>");
+		oWriter.WriteString(L"</w:tblPr>");
+
+		oWriter.WriteString(L"<w:tblGrid>");
+		oWriter.WriteString(L"</w:tblGrid>");
+
+		oWriter.WriteString(L"<w:tr>");
+		oWriter.WriteString(L"</w:tr>");
+
+		oWriter.WriteString(L"</w:tbl>");
+	}
+	void CTable::ToXmlPptx(NSStringUtils::CStringBuilder& oWriter) const
+	{
+
+	}
 
 } // namespace NSDocxRenderer
--- DocxRenderer/src/logic/elements/Table.h
+++ DocxRenderer/src/logic/elements/Table.h
@@ -6,8 +6,32 @@ namespace NSDocxRenderer
 {
     class CTable : public CBaseItem
 	{
+	public:
+		class CRow : CBaseItem
+		{
+		public:
+			CRow() = default;
+			virtual ~CRow() = default;
+			virtual void Clear() override final;
+			virtual void ToXml(NSStringUtils::CStringBuilder& oWriter) const override final;
+			virtual void ToXmlPptx(NSStringUtils::CStringBuilder& oWriter) const override final;
+		};
+
+		class CCell : CBaseItem
+		{
+		public:
+			CCell() = default;
+			virtual ~CCell() = default;
+			virtual void Clear() override final;
+			virtual void ToXml(NSStringUtils::CStringBuilder& oWriter) const override final;
+			virtual void ToXmlPptx(NSStringUtils::CStringBuilder& oWriter) const override final;
+		};
+
 		CTable() = default;
 		virtual ~CTable() = default;
+		virtual void Clear() override final;
+		virtual void ToXml(NSStringUtils::CStringBuilder& oWriter) const override final;
+		virtual void ToXmlPptx(NSStringUtils::CStringBuilder& oWriter) const override final;
 	};
 } // namespace NSDocxRenderer
 

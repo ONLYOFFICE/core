ddd7628bbe46d60d25bc82a7a3a80e1aa39156e7
ElenaSubbotina <Elena.Subbotina@onlyoffice.com>
2025-02-13 17:48:33 +0300

    fix bug #73097

--- OdfFile/Reader/Format/draw_frame_pptx.cpp
+++ OdfFile/Reader/Format/draw_frame_pptx.cpp
@@ -172,12 +172,13 @@ void draw_frame::pptx_convert(oox::pptx_conversion_context & Context)
 		}
 		oox::_oox_fill fill;
 		
-		graphic_format_properties_ptr properties = calc_graphic_properties_content(instances);
+		graphic_format_properties_ptr properties = calc_graphic_properties_content(instances, is_object_);
 		if (properties)
 		{
 			properties->apply_to(Context.get_slide_context().get_properties());
 
 			Compute_GraphicFill(properties->common_draw_fill_attlist_, properties->style_background_image_, Context.root(), fill);
+
 			if (properties->fo_clip_)
 			{
 				std::wstring strRectClip = properties->fo_clip_.get();
@@ -403,7 +404,7 @@ void draw_text_box::pptx_convert(oox::pptx_conversion_context & Context)
 }
 void draw_object::pptx_convert(oox::pptx_conversion_context & Context)
 {
-	if (draw_frame_ptr)
+	if (draw_frame_ptr) 
 	{
 		draw_frame *frame = dynamic_cast<draw_frame *>(draw_frame_ptr.get());
 		if (frame)

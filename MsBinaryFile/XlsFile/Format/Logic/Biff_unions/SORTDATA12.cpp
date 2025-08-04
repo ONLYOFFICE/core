9fc3fcabcb67e1e78064f60d114e8ac4743df4d3
Viktor Andreev <viktor.andreev@onlyoffice.com>
2025-08-04 19:01:27 +0600

    Fix sort data record writing

--- MsBinaryFile/XlsFile/Format/Logic/Biff_unions/SORTDATA12.cpp
+++ MsBinaryFile/XlsFile/Format/Logic/Biff_unions/SORTDATA12.cpp
@@ -73,6 +73,16 @@ const bool SORTDATA12::saveContent(BinProcessor& proc)
     if(m_SortData == nullptr)
         return false;
     proc.mandatory(*m_SortData);
+	auto castedPtr = static_cast<SortData*>(m_SortData.get());
+	for(auto i : castedPtr->sortCond12Array)
+	{
+		CFRecord binDataRec(rt_ContinueFrt12, proc.getGlobalWorkbookInfo());
+		i->save(binDataRec);
+		ContinueFrt12 tempRecord;
+		tempRecord.rgb.resize(binDataRec.getRdPtr());
+		memcpy(tempRecord.rgb.data(), binDataRec.getCurStaticData<char>(), binDataRec.getRdPtr());
+		proc.mandatory(tempRecord);
+	}
     return true;
 }
 

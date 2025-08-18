3e0a88a48782a85e35381ece35f830f6af964bcd
Timofey Derevyankin <timofey.derevyankin@onlyoffice.com>
2025-08-18 13:36:58 +0300

    fix/bug-with-break-columns
    update comments

--- OdfFile/Reader/Converter/docx_conversion_context.cpp
+++ OdfFile/Reader/Converter/docx_conversion_context.cpp
@@ -166,12 +166,12 @@ docx_conversion_context::~docx_conversion_context()
 {
 }
 
-void docx_conversion_context::set_implicit_end( bool _flag ) // fix bug with convert from docx to odt with break columns
+void docx_conversion_context::set_implicit_end( bool _flag ) // fix bug with convert from docx to odt. Bug with break columns
 {
     flag_implicit_end = _flag;
 }
 
-bool docx_conversion_context::get_implicit_end() const // fix bug with convert from docx to odt with break columns
+bool docx_conversion_context::get_implicit_end() const // fix bug with convert from docx to odt. Bug with break columns
 {
     return flag_implicit_end;
 }

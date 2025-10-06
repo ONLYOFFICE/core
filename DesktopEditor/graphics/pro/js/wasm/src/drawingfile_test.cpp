526e53e465a84e4b7233dedb0cf0fe8235db866a
Svetlana Kulikova <Svetlana.Kulikova@onlyoffice.com>
2025-10-06 13:02:07 +0300

    Fix test Split&Merge

--- DesktopEditor/graphics/pro/js/wasm/src/drawingfile_test.cpp
+++ DesktopEditor/graphics/pro/js/wasm/src/drawingfile_test.cpp
@@ -1051,7 +1051,7 @@ int main(int argc, char* argv[])
 		int nBufferLen = NULL;
 		BYTE* pBuffer = NULL;
 
-		if (GetFromBase64(NSFile::GetProcessDirectory() + L"/split1.txt", &pBuffer, &nBufferLen))
+		if (true && GetFromBase64(NSFile::GetProcessDirectory() + L"/split.txt", &pBuffer, &nBufferLen))
 		{
 			std::vector<int> arrPages = { 0 };
 			BYTE* pSplitPages = SplitPages(pGrFile, arrPages.data(), arrPages.size(), pBuffer, nBufferLen);
@@ -1060,40 +1060,28 @@ int main(int argc, char* argv[])
 			if (nLength > 4)
 			{
 				NSFile::CFileBinary oFile;
-				if (oFile.CreateFileW(NSFile::GetProcessDirectory() + L"/split1.pdf"))
+				if (oFile.CreateFileW(NSFile::GetProcessDirectory() + L"/split.pdf"))
 					oFile.WriteFile(pSplitPages + 4, nLength - 4);
 				oFile.CloseFile();
-
-				BYTE* pMallocData = (BYTE*)malloc(nLength - 4);
-				memcpy(pMallocData, pSplitPages + 4, nLength - 4);
-
-				MergePages(pGrFile, pMallocData, nLength - 4, 0, "merge1");
 			}
 			RELEASEARRAYOBJECTS(pSplitPages);
 		}
 		RELEASEARRAYOBJECTS(pBuffer);
 
-		if (GetFromBase64(NSFile::GetProcessDirectory() + L"/split2.txt", &pBuffer, &nBufferLen))
+		if (true)
 		{
-			std::vector<int> arrPages = { 0 };
-			BYTE* pSplitPages = SplitPages(pGrFile, arrPages.data(), arrPages.size(), pBuffer, nBufferLen);
-			int nLength = READ_INT(pSplitPages);
-
-			if (nLength > 4)
+			NSFile::CFileBinary oFile;
+			if (oFile.OpenFile(NSFile::GetProcessDirectory() + L"/split.pdf"))
 			{
-				NSFile::CFileBinary oFile;
-				if (oFile.CreateFileW(NSFile::GetProcessDirectory() + L"/split2.pdf"))
-					oFile.WriteFile(pSplitPages + 4, nLength - 4);
-				oFile.CloseFile();
+				DWORD dwFileSize = oFile.GetFileSize();
+				BYTE* pFileContent = (BYTE*)malloc(dwFileSize);
 
-				BYTE* pMallocData = (BYTE*)malloc(nLength - 4);
-				memcpy(pMallocData, pSplitPages + 4, nLength - 4);
-
-				MergePages(pGrFile, pMallocData, nLength - 4, 0, "merge2");
+				DWORD dwReaded;
+				if (oFile.ReadFile(pFileContent, dwFileSize, dwReaded))
+					MergePages(pGrFile, pFileContent, dwReaded, 0, "merge1");
 			}
-			RELEASEARRAYOBJECTS(pSplitPages);
+			oFile.CloseFile();
 		}
-		RELEASEARRAYOBJECTS(pBuffer);
 	}
 
 	// INFO

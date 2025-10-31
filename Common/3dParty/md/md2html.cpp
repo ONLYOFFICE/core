aa24268a2ee5b028ea19b05d2af4d0fe0f25c70d
Kirill Polyakov <Kirill.Poljakov@onlyoffice.com>
2025-10-31 16:19:42 +0300

    Fix bug #74743

--- Common/3dParty/md/md2html.cpp
+++ Common/3dParty/md/md2html.cpp
@@ -87,7 +87,7 @@ bool ConvertMdFileToHtml(const std::wstring& wsPathToMdFile, const std::wstring&
 	bool bResult = true;
 
 	if (0 != md_html(sMdData.c_str(), sMdData.length(), ToHtmlFile, &oFile,
-					 MD_DIALECT_GITHUB | MD_FLAG_NOINDENTEDCODEBLOCKS | MD_HTML_FLAG_SKIP_UTF8_BOM,
+					 MD_DIALECT_GITHUB | MD_FLAG_NOINDENTEDCODEBLOCKS | MD_HTML_FLAG_SKIP_UTF8_BOM | MD_FLAG_HARD_SOFT_BREAKS,
 					 0))
 		bResult = false;
 

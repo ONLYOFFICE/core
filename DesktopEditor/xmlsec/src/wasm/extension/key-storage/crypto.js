3f2ad2dfd77830ae39bb4c30bff6e065377a9f3c
Vladimir Privezenov <vladimir.privezenov@onlyoffice.com>
2025-12-10 20:33:08 +0300

    Develop crypto library

---
+++ DesktopEditor/xmlsec/src/wasm/extension/key-storage/crypto.js
@@ -0,0 +1,148 @@
+import {Key, KeyPair, KeyUsages, PrivateKey, PublicKey, SymmetricKey} from "./keys/keys.ts";
+import {
+	digestTypes,
+	exportKeyFormats,
+} from "./keys/key-types.ts";
+import {AesGcmGenParams} from "./keys/params.ts";
+
+
+
+
+const pbkdf2Parameters = {
+	iterations: 150000,
+	hash: digestTypes.SHA256,
+	saltLength: 16
+};
+
+function CCryptoBase() {
+
+}
+CCryptoBase.prototype.sign = function(key, data) {};
+CCryptoBase.prototype.digest = function(algorithm, data) {};
+CCryptoBase.prototype.verify = function(key, signature, data) {};
+CCryptoBase.prototype.decrypt = function(key, data) {};
+CCryptoBase.prototype.encrypt = function(key, data) {};
+CCryptoBase.prototype.generateKey = function(params, keyUsage) {};
+CCryptoBase.prototype.wrapKey = function(format, key, masterPassword, salt, aesParams, keyUsage) {};
+CCryptoBase.prototype.unwrapKey = function(format, key, masterPassword, salt, aesParams, keyParams, keyUsage) {};
+CCryptoBase.prototype.getRandomValues = function(length) {};
+CCryptoBase.prototype.randomUUID = function() {};
+
+function CWebCrypto() {
+	CCryptoBase.call(this);
+	this.crypto = self.crypto;
+	this.subtle = this.crypto.subtle;
+}
+CWebCrypto.prototype = Object.create(CCryptoBase.prototype);
+CWebCrypto.prototype.constructor = CWebCrypto;
+CWebCrypto.prototype.getRandomValues = function(length) {
+	const ui = new Uint8Array(length);
+	return this.crypto.getRandomValues(ui);
+}
+CWebCrypto.prototype.getAesCryptoKey = function(masterPassword, salt) {
+	const oThis = this;
+	return this.subtle.importKey(
+		'raw',
+		masterPassword,
+		{ name: 'PBKDF2' },
+		false,
+		['deriveKey']
+	).then(function(pwKey) {
+		return oThis.subtle.deriveKey(
+			{
+				name: 'PBKDF2',
+				salt: salt,
+				iterations: pbkdf2Parameters.iterations,
+				hash: pbkdf2Parameters.hash
+			},
+			pwKey,
+			new AesGcmGenParams(),
+			false,
+			['wrapKey', 'unwrapKey']
+		);
+	});
+};
+
+CWebCrypto.prototype.wrapKey = function (format, key, masterPassword, salt, aesParams, keyUsage) {
+	const oThis = this;
+	return Promise.all([this.getAesCryptoKey(masterPassword, salt),  this.getCryptoKeyFromWrapper(key, keyUsage)]).then(function(cryptoKeys) {
+		return oThis.subtle.wrapKey(format, cryptoKeys[1], cryptoKeys[0], aesParams);
+	});
+	
+}
+CWebCrypto.prototype.unwrapKey = function(format, key, masterPassword, salt, aesParams, keyParams, keyUsages) {
+	const oThis = this;
+	return this.getAesCryptoKey(masterPassword, salt).then(function(cryptoAesKey) {
+		return oThis.subtle.unwrapKey(format, key, cryptoAesKey, aesParams, keyParams, true, /*this.getKeyUsages(keyUsages)*/["sign"]);
+	}).then(function(cryptoKey) {
+		return oThis.subtle.exportKey(format, cryptoKey);
+	});
+}
+CWebCrypto.prototype.getCryptoKeyFromWrapper = function(key, keyUsages) {
+	return this.subtle.importKey(key.exportFormat, key.key, key.params, true, keyUsages);
+}
+CWebCrypto.prototype.sign = function(key, data) {
+	const oThis = this;
+	const cryptoKey = key.getCryptoKey();
+	const params = key.getCryptoParams();
+	return oThis.subtle.sign(cryptoKey, cryptoKey, data);
+	return this.getCryptoKeyFromWrapper(key, new KeyUsages(false, true)).then(function(cryptoKey) {
+		return oThis.subtle.sign(key.params, cryptoKey, data);
+	});
+}
+CWebCrypto.prototype.digest = function(algorithm, data) {
+	return this.subtle.digest(algorithm, data);
+}
+CWebCrypto.prototype.verify = function(key, signature, data) {
+	const oThis = this;
+	return this.getCryptoKeyFromWrapper(key, new KeyUsages(false, true)).then(function(cryptoKey) {
+		return oThis.subtle.verify(key.params, cryptoKey, signature, data);
+	});
+}
+CWebCrypto.prototype.decrypt = function(key, data) {
+	const oThis = this;
+	const cryptoKey = key.getCryptoKey();
+	const algorithm = key.getCryptoAlgrotihm();
+	return oThis.subtle.decrypt(algorithm, cryptoKey, data);
+}
+CWebCrypto.prototype.encrypt = function(key, data) {
+		const cryptoKey = key.getCryptoKey();
+		const algorithm = key.getCryptoAlgorithm();
+		return this.subtle.encrypt(algorithm, cryptoKey, data);
+}
+CWebCrypto.prototype.exportKey = function(key) {
+	const cryptoKey = key.getCryptoKey();
+	const format = key.getCryptoFormat();
+	return this.subtle.exportKey(format, cryptoKey);
+}
+CWebCrypto.prototype.generateKey = function(params, keyUsages) {
+	const oThis = this;
+	return this.subtle.generateKey(params, true, this.getKeyUsages(keyUsage)).then(function(cryptoKey) {
+		if (cryptoKey.privateKey && cryptoKey.publicKey) {
+			return Promise.all([oThis.subtle.exportKey(exportKeyFormats.spki, cryptoKey.publicKey), oThis.subtle.exportKey(exportKeyFormats.pkcs8, cryptoKey.privateKey)]);
+			const publicKeyBuffer = await this.subtle.exportKey(exportKeyFormats.spki, cryptoKey.publicKey);
+			const publicKey = new PublicKey(publicKeyBuffer, importParams);
+			const privateKeyBuffer = await this.subtle.exportKey(exportKeyFormats.pkcs8, cryptoKey.privateKey);
+			const privateKey = new PrivateKey(privateKeyBuffer, importParams, this.getRandomValues(pbkdf2Parameters.saltLength));
+			return new KeyPair(publicKey, privateKey, keyUsage);
+		}
+		return oThis.subtle.exportKey(exportKeyFormats.raw, cryptoKey);
+		return new SymmetricKey(keyBuffer, importParams, keyUsage);
+	}).then(function(exportedKeys) {
+		if (exportedKeys.length === 2) {
+			//todo
+		} else {
+			//todo
+		}
+	});
+	const importParams = params.getImportParams();
+
+};
+
+CWebCrypto.prototype.randomUUID = function() {
+	return this.crypto.randomUUID();
+}
+
+function getCrypto() {
+	return new CWebCrypto();
+}
--- DesktopEditor/xmlsec/src/wasm/extension/key-storage/keys.js
+++ DesktopEditor/xmlsec/src/wasm/extension/key-storage/keys.js
@@ -177,10 +177,13 @@ function writeBuffer() {
 
 }
 
-const c_oAscCipherKeyVersion = {
-	KeyBaseNoVersion: 0,
-	WebKeyPairV1: 1,
-	WebSymmetricKeyV1: 2
+const c_oAscCipherKeyType = {
+	NoType: 0,
+	WebSymmetricKey: 1,
+	WebSignKeyPair: 2,
+	WebEncryptKeyPair: 3,
+	WebPublicKey: 4,
+	WebPrivateKey: 5,
 };
 
 
@@ -188,7 +191,8 @@ function CryptoKeyBase() {
 	this.date = null;
 	this.isValid = false;
 	this.guid = null;
-	this.version = c_oAscCipherKeyVersion.KeyBaseNoVersion;
+	this.version = 1;
+	this.type = c_oAscCipherKeyType.NoType;
 }
 CryptoKeyBase.import = function(reader, version, symmetricKey) {
 
@@ -212,7 +216,10 @@ CryptoKeyBase.prototype.setGUID = function(guid) {
 	this.guid = guid;
 };
 CryptoKeyBase.prototype.setVersion = function(version) {
-	this.guid = version;
+	this.version = version;
+};
+CryptoKeyBase.prototype.setType = function(type) {
+	this.type = type;
 };
 
 
@@ -220,30 +227,177 @@ function WebKeyPair() {
 	CryptoKeyBase.call(this);
 	this.privateKey = null;
 	this.publicKey = null;
-	this.version = c_oAscCipherKeyVersion.WebKeyPairV1;
+	this.version = 1;
 }
 WebKeyPair.prototype = Object.create(CryptoKeyBase.prototype);
 WebKeyPair.prototype.constructor = WebKeyPair;
+WebKeyPair.prototype.setPublicKey = function(publicKey) {
+	this.publicKey = publicKey;
+};
+WebKeyPair.prototype.setPrivateKey = function(privateKey) {
+	this.privateKey = privateKey;
+};
+
+WebKeyPair.import = function(reader) {
+	const keyPair = new this();
+	keyPair.setVersion(readLong(reader));
+	switch (keyPair.version) {
+		case 1: {
+			keyPair.setDate(readString(reader));
+			keyPair.setGUID(readString(reader));
+			keyPair.setIsValid(readBool(reader));
+			keyPair.setPublicKey(readObject(reader));
+			keyPair.setPrivateKey(readObject(reader));
+			break;
+		}
+		default: {
+			return null;
+		}
+	}
+};
+WebKeyPair.prototype.export = function() {
+	writeLong(writer, this.version);
+	switch (this.version) {
+		case 1: {
+			writeString(writer, this.date.toISOString());
+			writeString(writer, this.guid);
+			writeBool(writer, this.isValid);
+			writeObject(writer, this.publicKey);
+			writeObject(writer, this.privateKey);
+			break;
+		}
+		default: {
+			break;
+		}
+	}
+};
+
+
+function WebSignKeyPair() {
+	WebKeyPair.call(this);
+	this.type = c_oAscCipherKeyType.WebSignKeyPair;
+}
+WebSignKeyPair.prototype = Object.create(WebKeyPair.prototype);
+WebSignKeyPair.prototype.constructor = WebSignKeyPair;
+WebSignKeyPair.import = WebKeyPair.import;
+
+function WebEncryptSignPair() {
+	WebKeyPair.call(this);
+	this.privateKey = null;
+	this.publicKey = null;
+	this.type = c_oAscCipherKeyType.WebEncryptKeyPair;
+}
+WebEncryptSignPair.prototype = Object.create(WebKeyPair.prototype);
+WebEncryptSignPair.prototype.constructor = WebEncryptSignPair;
+WebEncryptSignPair.import = WebKeyPair.import;
+
+function AsymmetricKey() {
+	this.binaryKey = null;
+	this.cryptoKey = null;
+	this.version = 1;
+	this.type = c_oAscCipherKeyType.NoType;
+}
+AsymmetricKey.prototype.setBinaryKey = function(binaryKey) {
+	this.binaryKey = binaryKey;
+};
+AsymmetricKey.prototype.setCryptoKey = function(cryptoKey) {
+	this.cryptoKey = cryptoKey;
+};
+AsymmetricKey.prototype.setVersion = function(version) {
+	this.version = version;
+};
+AsymmetricKey.prototype.changeMasterPassword = function(oldMasterPassword, newMasterPassword) {};
 
 function WebPrivateKey() {
+	AsymmetricKey.call(this);
+	this.type = c_oAscCipherKeyType.WebPrivateKey;
+	this.salt = null;
+}
+WebPrivateKey.prototype = Object.create(AsymmetricKey.prototype);
+WebPrivateKey.prototype.constructor = WebPrivateKey;
+
+
+WebPrivateKey.import = function(reader) {
+	const key = new WebPrivateKey();
+	key.setVersion(readLong(reader));
+	switch (key.version) {
+		case 1: {
+			key.setBinaryKey(readBuffer(reader));
+			key.setSalt(readBuffer(reader));
+			break;
+		}
+		default:
+			return null;
+	}
+	return key;
+};
+WebPrivateKey.prototype.export = function(writer) {
+	writeLong(writer, this.version);
+	switch (this.version) {
+		case 1: {
+			writeBuffer(writer, this.binaryKey);
+			writeBuffer(writer, this.salt);
+			break;
+		}
+		default: {
+			break;
+		}
+	}
+};
+WebPrivateKey.prototype.setSalt = function(salt) {
+	this.salt = salt;
+};
+WebPrivateKey.prototype.changeMasterPassword = function(oldMasterPassword, newMasterPassword) {
+	const oldPasswordKey = WebSymmetricKey.getFromPassword(oldMasterPassword, this.salt);
 
 }
+
+
 
 function WebPublicKey() {
-	
+	AsymmetricKey.call(this);
+	this.binaryKey = null;
+	this.cryptoKey = null;
 }
+WebPublicKey.prototype = Object.create(AsymmetricKey.prototype);
+WebPublicKey.prototype.constructor = WebPublicKey;
+WebPublicKey.import = function(reader) {
+	const key = new WebPublicKey();
+	key.setVersion(readLong(reader));
+	switch (key.version) {
+		case 1: {
+			key.setBinaryKey(readBuffer(reader));
+			break;
+		}
+		default:
+			return null;
+	}
+	return key;
+};
+WebPublicKey.prototype.export = function(writer) {
+	writeLong(writer, this.version);
+	switch (this.version) {
+		case 1: {
+			writeBuffer(writer, this.binaryKey);
+			break;
+		}
+		default: {
+			break;
+		}
+	}
+};
 
 function WebSymmetricKey() {
 	CryptoKeyBase.call(this);
-	this.version = c_oAscCipherKeyVersion.WebSymmetricKeyV1;
+	this.version = 1;
+	this.type = c_oAscCipherKeyType.WebSymmetricKey;
 	this.cryptoKey = null;
 	this.binaryKey = null;
 }
-
 WebSymmetricKey.prototype = Object.create(CryptoKeyBase.prototype);
 WebSymmetricKey.prototype.constructor = WebSymmetricKey;
-WebSymmetricKey.prototype.setBinaryKey = function() {
-
+WebSymmetricKey.prototype.setBinaryKey = function(buffer) {
+	this.binaryKey = buffer;
 };
 WebSymmetricKey.prototype.getCryptoKey = function() {
 	if (!this.cryptoKey && this.binaryKey) {
@@ -252,27 +406,36 @@ WebSymmetricKey.prototype.getCryptoKey = function() {
 	return this.cryptoKey;
 };
 
-WebSymmetricKey.import = function(reader, version) {
-	switch (version) {
-		case c_oAscCipherKeyVersion.WebSymmetricKeyV1: {
-			const symmetricKey = new WebSymmetricKey();
-			symmetricKey.setVersion(readByte(reader));
+WebSymmetricKey.import = function(reader) {
+	const symmetricKey = new WebSymmetricKey();
+	symmetricKey.setVersion(readLong(reader));
+	switch (symmetricKey.version) {
+		case 1: {
 			symmetricKey.setDate(readString(reader));
 			symmetricKey.setGUID(readString(reader));
 			symmetricKey.setIsValid(readBool(reader));
-			symmetricKey.setBinaryKey();
-			return symmetricKey;
+			symmetricKey.setBinaryKey(readBuffer(reader));
+			break;
 		}
 		default:
 			return null;
 	}
+	return symmetricKey;
 };
 WebSymmetricKey.prototype.export = function(writer) {
-	writeByte(writer, this.version);
-	writeString(writer, this.date.toISOString());
-	writeString(writer, this.guid);
-	writeBool(this.isValid);
-	writeBuffer(this.binaryKey);
+	writeLong(writer, this.version);
+	switch (this.version) {
+		case 1: {
+			writeString(writer, this.date.toISOString());
+			writeString(writer, this.guid);
+			writeBool(writer, this.isValid);
+			writeBuffer(writer, this.binaryKey);
+			break;
+		}
+		default: {
+			break;
+		}
+	}
 };
 
 
